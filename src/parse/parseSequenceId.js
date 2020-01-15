cc65ff7d45ece6534b57a20f0f8fe3d5e06937a0
unknown <p.zhao.johnson@gmail.com>
2020-01-15 11:29:10 -0500

    Added parseSequenceId function

---
+++ src/parse/parseSequenceId.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} unparsed What the user input.
+ * 
+ * @returns {string} The parsed sequence ID.
+ */
+function parseSequenceId(unparsed) {
+  return unparsed.trim();
+}
+
+export default parseSequenceId;
---
+++ src/parse/parseSequenceId.test.js
@@ -0,0 +1,23 @@
+import parseSequenceId from './parseSequenceId';
+import { parse } from '@babel/core';
+
+it('parseSequenceId', () => {
+
+  // no whitespace
+  expect(parseSequenceId('asdf')).toBe('asdf');
+
+  // leading whitespace
+  expect(parseSequenceId(' \tasdf')).toBe('asdf');
+
+  // trailing whitespace
+  expect(parseSequenceId('asdf \t')).toBe('asdf');
+
+  // intervening whitespace
+  expect(parseSequenceId('as\t df')).toBe('as\t df');
+
+  // whitespace everywhere
+  expect(parseSequenceId('\t as  df\t\n')).toBe('as  df');
+
+  // only whitespace
+  expect(parseSequenceId(' \n\t ')).toBe('');
+});
