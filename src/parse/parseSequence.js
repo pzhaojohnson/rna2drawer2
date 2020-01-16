68ba433b00d28fef2254ddb122ce00810e1c9431
unknown <p.zhao.johnson@gmail.com>
2020-01-16 13:24:33 -0500

    Finished parseSequence function and filled out its test suite

--- src/parse/parseSequence.js
+++ src/parse/parseSequence.js
@@ -1,26 +1,48 @@
-let _defaultOptions = {
-  toUpperCase: true,
-  toLowerCase: false,
+function _checkForContradictoryOptions(options) {
+  if (options.toUpperCase && options.toLowerCase) {
+    throw new Error('toUpperCase and toLowerCase options cannot both be true.');
+  }
 
-  t2u: true,
-  u2t: false,
-
-  ignoreNumbers: true,
-  ignoreNonBases: true,
-  ignoreNonAlphanumerics: true
-};
-
-/**
- * @param {object} options 
- * 
- * @returns {boolean} True if the options object is contradictory.
- */
-function _contradictoryOptions(options) {
-  return (options.toUpperCase && options.toLowerCase)
-    || (options.t2u && u2t);
+  if (options.t2u && options.u2t) {
+    throw new Error('t2u and u2t options cannot both be true.');
+  }
 }
 
 /**
+ * @param {string} c A character.
+ * 
+ * @returns {boolean} True if the character is a whitespace character.
+ */
+function _isWhitespace(c) {
+  return c.trim().length === 0;
+}
+
+function _isNumber(c) {
+  return c.match(/[0-9]/);
+}
+
+function _isLetter(c) {
+  return c.match(/[A-Z|a-z]/)
+}
+
+function _isAUGCT(c) {
+  return c.match(/[A|a|U|u|G|g|C|c|T|t]/);
+}
+
+function _isNonAlphanumeric(c) {
+  return !_isNumber(c) && !_isLetter(c);
+}
+
+/**
+ * Whitespace characters are always ignored.
+ * 
+ * By default:
+ *  The cases of letters are not changed.
+ *  Neither Ts nor Us are converted to each other.
+ *  Numbers are not ignored.
+ *  Non-AUGCT letters are not ignored.
+ *  Non-alphanumeric letters are not ignored.
+ * 
  * @param {string} unparsed What the user input.
  * @param {object} options 
  * @param {boolean} options.toUpperCase True if letters are to be converted to upper case.
@@ -28,21 +50,57 @@ function _contradictoryOptions(options) {
  * @param {boolean} options.t2u True if Ts (both lower and upper case) are to be converted to Us (of the same case).
  * @param {boolean} options.u2t True if Us (both lower and upper case) are to be converted to Ts (of the same case).
  * @param {boolean} options.ignoreNumbers True if numbers are to be ignored.
- * @param {boolean} options.ignoreNonBases True if letters that are not [A|a|U|u|G|g|C|c|T|t] are to be ignored.
+ * @param {boolean} options.ignoreNonAUGCTLetters True if letters that are not [A|a|U|u|G|g|C|c|T|t] are to be ignored.
  * @param {boolean} options.ignoreNonAlphanumerics True if characters that are not numbers or letters are to be ignored.
  * 
  * @returns {string} The parsed sequence.
  * 
- * @throws {Error} If contradictory options are given. Potentially contradictory options include
- *  toUpperCase and toLowerCase, and t2u and u2t.
+ * @throws {Error} If options.toUpperCase and options.toLowerCase are both true.
+ * @throws {Error} If options.t2u and options.u2t are both true.
  */
-function parseSequence(unparsed, options=_defaultOptions) {
+function parseSequence(unparsed, options={}) {
+  _checkForContradictoryOptions(options);
+  
   let parsed = '';
 
   for (let i = 0; i < unparsed.length; i++) {
     let c = unparsed.charAt(i);
 
+    if (options.toUpperCase) {
+      c = c.toUpperCase();
+    } else if (options.toLowerCase) {
+      c = c.toLowerCase();
+    }
 
+    if (options.t2u) {
+      if (c === 'T') {
+        c = 'U';
+      } else if (c === 't') {
+        c = 'u';
+      }
+    } else if (options.u2t) {
+      if (c === 'U') {
+        c = 'T';
+      } else if (c === 'u') {
+        c = 't';
+      }
+    }
+
+    let toBeIgnored = false;
+
+    if (_isWhitespace(c)) {
+      toBeIgnored = true;
+    } else if (_isNumber(c) && options.ignoreNumbers) {
+      toBeIgnored = true;
+    } else if (_isLetter(c) && !_isAUGCT(c) && options.ignoreNonAUGCTLetters) {
+      toBeIgnored = true;
+    } else if (_isNonAlphanumeric(c) && options.ignoreNonAlphanumerics) {
+      toBeIgnored = true;
+    }
+
+    if (!toBeIgnored) {
+      parsed += c;
+    }
   }
 
   return parsed;
--- src/parse/parseSequence.test.js
+++ src/parse/parseSequence.test.js
@@ -1,1 +1,133 @@
 import parseSequence from './parseSequence';
+
+it('contradictory options', () => {
+  
+  // case conversion
+  expect(() => parseSequence('asdf', { toUpperCase: true, toLowerCase: true })).toThrow();
+  expect(() => parseSequence('asdf', { toUpperCase: true, toLowerCase: false })).not.toThrow();
+  expect(() => parseSequence('asdf', { toUpperCase: false, toLowerCase: true })).not.toThrow();
+  expect(() => parseSequence('asdf', { toUpperCase: false, toLowerCase: false })).not.toThrow();
+
+  // T and U conversion
+  expect(() => parseSequence('asdf', { t2u: true, u2t: true })).toThrow();
+  expect(() => parseSequence('asdf', { t2u: true, u2t: false })).not.toThrow();
+  expect(() => parseSequence('asdf', { t2u: false, u2t: true })).not.toThrow();
+  expect(() => parseSequence('asdf', { t2u: false, u2t: false })).not.toThrow();
+});
+
+it('empty input', () => {
+
+  // empty string
+  expect(parseSequence('')).toBe('');
+
+  // only whitespace
+  expect(parseSequence('  \n\n\t\t\r \r\n ')).toBe('');
+})
+
+it('ignore whitespace', () => {
+
+  // no whitespace
+  expect(parseSequence('AAA')).toBe('AAA');
+
+  // whitespace in lots of places
+  expect(parseSequence(
+    ' \n\t  \t AU  \t \n\r \r\r\r   \n\n\n \t\t\t GC  \t\t \n\r\n \t'
+  )).toBe('AUGC');
+});
+
+it('toUpperCase', () => {
+
+  // non-AUGCT letters are turned to upper case as well
+  expect(parseSequence(
+    'AaUUuuuggCCcIiNNn',
+    { toUpperCase: true }
+  )).toBe('AAUUUUUGGCCCIINNN');
+});
+
+it('toLowerCase', () => {
+
+  // non-AUGCT letters are turned to lower case as well
+  expect(parseSequence(
+    'AaUUuuuggCCcIiNNn',
+    { toLowerCase: true }
+  )).toBe('aauuuuuggccciinnn');
+});
+
+it('t2u', () => {
+
+  // must handle upper and lower case Ts (and account for their case)
+
+  expect(parseSequence(
+    'AATtUGCt',
+    { t2u: true }
+  )).toBe('AAUuUGCu');
+  
+  expect(parseSequence(
+    'AATtUGCt',
+    { t2u: true, toUpperCase: true }
+  )).toBe('AAUUUGCU');
+
+  expect(parseSequence(
+    'AATtUGCt',
+    { t2u: true, toLowerCase: true }
+  )).toBe('aauuugcu');
+});
+
+it('u2t', () => {
+
+  // must handle upper and lower case Us (and account for their case)
+
+  expect(parseSequence(
+    'AAUuTGCu',
+    { u2t: true }
+  )).toBe('AATtTGCt');
+  
+  expect(parseSequence(
+    'AAUuTGCu',
+    { u2t: true, toUpperCase: true }
+  )).toBe('AATTTGCT');
+
+  expect(parseSequence(
+    'AAUuTGCu',
+    { u2t: true, toLowerCase: true }
+  )).toBe('aatttgct');
+});
+
+it('ignoreNumbers', () => {
+  expect(parseSequence(
+    '1au234ggc0056ac789tgc',
+    { ignoreNumbers: true }
+  )).toBe('auggcactgc');
+
+  expect(parseSequence(
+    '1au234ggc0056ac789tgc',
+    { ignoreNumbers: false }
+  )).toBe('1au234ggc0056ac789tgc');
+});
+
+it('ignoreNonAUGCTLetters', () => {
+
+  // must handle upper and lower case versions of letters
+
+  expect(parseSequence(
+    'iIaAnNgGkkCcopUTtuzx',
+    { ignoreNonAUGCTLetters: true }
+  )).toBe('aAgGCcUTtu');
+
+  expect(parseSequence(
+    'iIaAnNgGkkCcopUTtuzx',
+    { ignoreNonAUGCTLetters: false }
+  )).toBe('iIaAnNgGkkCcopUTtuzx');
+});
+
+it('ignoreNonAlphanumerics', () => {
+  expect(parseSequence(
+    '<>!AaGGGwer()...CCc',
+    { ignoreNonAlphanumerics: true }
+  )).toBe('AaGGGwerCCc');
+
+  expect(parseSequence(
+    '<>!AaGGGwer()...CCc',
+    { ignoreNonAlphanumerics: false }
+  )).toBe('<>!AaGGGwer()...CCc');
+});
