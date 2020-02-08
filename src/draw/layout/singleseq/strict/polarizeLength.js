3fd782f9957cb31ab68a03495636d19834102202
unknown <p.zhao.johnson@gmail.com>
2020-02-08 13:12:19 -0500

    Refactored length argument to straightLength

--- src/draw/layout/singleseq/strict/polarizeLength.js
+++ src/draw/layout/singleseq/strict/polarizeLength.js
@@ -2,12 +2,12 @@
  * This is not perfectly precise, as polar length really depends on the size of a given circle,
  * but this seems to work well enough in practice.
  * 
- * @param {number} length 
+ * @param {number} straightLength 
  * 
  * @returns {number} The polar length of a straight length.
  */
-function polarizeLength(length) {
-  return 1.15 * length;
+function polarizeLength(straightLength) {
+  return 1.15 * straightLength;
 }
 
 export default polarizeLength;
