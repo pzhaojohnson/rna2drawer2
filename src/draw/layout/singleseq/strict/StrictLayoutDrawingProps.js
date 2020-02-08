7098568593852c4bb827d039903c2c7dc429d044
unknown <p.zhao.johnson@gmail.com>
2020-02-08 14:02:57 -0500

    Added documentation comment for StrictLayoutDrawingProps class

--- src/draw/layout/singleseq/strict/StrictLayoutDrawingProps.js
+++ src/draw/layout/singleseq/strict/StrictLayoutDrawingProps.js
@@ -1,3 +1,10 @@
+/**
+ * Since instances of this class have no object properties,
+ * they can be deep copied using the spread operator.
+ * 
+ * Instances of this class can also be converted to and parsed from
+ * a JSON string via the JSON.stringify and JSON.parse methods, respectively.
+ */
 class StrictLayoutDrawingProps {
   constructor() {
     this.flatOutermostLoop = false;
