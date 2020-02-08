82b89ebac336c42b87237c484faf02686bde4988
unknown <p.zhao.johnson@gmail.com>
2020-02-08 14:01:01 -0500

    Added test for ability to convert to and parse from a JSON string

--- src/draw/layout/singleseq/strict/StrictLayoutDrawingProps.test.js
+++ src/draw/layout/singleseq/strict/StrictLayoutDrawingProps.test.js
@@ -18,3 +18,21 @@ it('can be copied using the spread operator', () => {
     expect(dps1[k]).toBe(dps0[k]);
   });
 });
+
+it('can be converted to and parsed from a JSON string', () => {
+  let dps0 = new StrictLayoutDrawingProps();
+  dps0.flatOutermostLoop = !dps0.flatOutermostLoop;
+  dps0.rotation = 0.5;
+  dps0.watsonCrickBondLength = 2;
+  dps0.terminiGap = 9;
+  dps0.maxTriangleLoopAngle = 0.3;
+
+  let s = JSON.stringify(dps0);
+  let dps1 = JSON.parse(s);
+
+  expect(Object.keys(dps1).length).toBe(Object.keys(dps0).length);
+
+  Object.keys(dps0).forEach(k => {
+    expect(dps1[k]).toBe(dps0[k]);
+  });
+});
