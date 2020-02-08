08a8c1a0a08436ef294e98556bc5a8b0d733fbbc
unknown <p.zhao.johnson@gmail.com>
2020-02-08 13:54:33 -0500

    Added test for ability to convert to and parse from a JSON string

--- src/draw/layout/singleseq/strict/StrictLayoutBaseProps.test.js
+++ src/draw/layout/singleseq/strict/StrictLayoutBaseProps.test.js
@@ -17,3 +17,20 @@ it('can be copied using the spread operator', () => {
     expect(bps1[k]).toBe(bps0[k]);
   });
 });
+
+it('can be converted to a JSON string and parsed from a JSON string', () => {
+  let bps0 = new StrictLayoutBaseProps();
+  bps0.stretch3 = 1;
+  bps0.flatOutermostLoopAngle3 = 0.5;
+  bps0.flipStem = !bps0.flipStem;
+  bps0.loopShape = 'triangle';
+
+  let s = JSON.stringify(bps0);
+  let bps1 = JSON.parse(s);
+
+  expect(Object.keys(bps1).length).toBe(Object.keys(bps0).length);
+
+  Object.keys(bps0).forEach(k => {
+    expect(bps1[k]).toBe(bps0[k]);
+  });
+});
