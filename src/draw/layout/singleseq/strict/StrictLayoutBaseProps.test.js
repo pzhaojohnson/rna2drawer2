a617cb953054b95cae51fddffce3f45a566b8e2a
unknown <p.zhao.johnson@gmail.com>
2020-02-08 13:16:39 -0500

    Added test suite for StrictLayoutBaseProps class

---
+++ src/draw/layout/singleseq/strict/StrictLayoutBaseProps.test.js
@@ -0,0 +1,19 @@
+import StrictLayoutBaseProps from './StrictLayoutBaseProps';
+
+it('can instantiate', () => {
+  let bps = new StrictLayoutBaseProps();
+});
+
+it('can be copied using the spread operator', () => {
+  let bps0 = new StrictLayoutBaseProps();
+  bps0.stretch3 = 1;
+  bps0.flatOutermostLoopAngle3 = 0.5;
+  bps0.flipStem = !bps0.flipStem;
+  bps0.loopShape = 'triangle';
+
+  let bps1 = {...bps0};
+
+  Object.keys(bps0).forEach(k => {
+    expect(bps1[k]).toBe(bps0[k]);
+  });
+});
