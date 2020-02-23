518ac618ec802601ff16dd7fc306ce7caa53ae4a
Philip Johnson <p.zhao.johnson@gmail.com>
2020-02-23 13:04:56 -0500

    Worked on StraightBond class

--- src/draw/StraightBond.js
+++ src/draw/StraightBond.js
@@ -4,319 +4,223 @@ import createUUIDforSVG from './createUUIDforSVG';
 class StraightBond {
 
   /**
+   * @typedef {Object} StraightBond~Coordinates 
+   * @property {number} x1 
+   * @property {number} y1 
+   * @property {number} x2 
+   * @property {number} y2 
+   */
+
+  /**
    * @param {Base} b1 
    * @param {Base} b2 
    * @param {numbering} padding1 
    * @param {numbering} padding2 
    * 
-   * @returns {object} The coordinates for the SVG line object of a straight bond
-   *  given the two bases it connects and their paddings.
+   * @returns {StraightBond~Coordinates} The coordinates of the line of a straight bond.
    */
   static coordinates(b1, b2, padding1, padding2) {
-    let angle1 = b1.angleBetweenCenters(b2);
-    let x1 = b1.xCenter + (padding1 * Math.cos(angle1));
-    let y1 = b1.yCenter + (padding1 * Math.sin(angle1));
-
-    let angle2 = b2.angleBetweenCenters(b1);
-    let x2 = b2.xCenter + (padding2 * Math.cos(angle2));
-    let y2 = b2.yCenter + (padding2 * Math.sin(angle2));
-
-    return { x1: x1, y1: y1, x2: x2, y2: y2 };
-  }
-
-  /**
-   * @param {Base} b1 
-   * @param {Base} b2 
-   * @param {number} padding1 
-   * @param {number} padding2 
-   * 
-   * @returns {number} The opacity for a straight bond given its bases and paddings.
-   */
-  static opacity(b1, b2, padding1, padding2) {
     if (padding1 + padding2 >= b1.distanceBetweenCenters(b2)) {
-      return 0;
+      let x = (b1.xCenter + b2.xCenter) / 2;
+      let y = (b1.yCenter + b2.yCenter) / 2;
+
+      return {
+        x1: x,
+        y1: y,
+        x2: x,
+        y2: y,
+      };
     } else {
-      return 1;
+      let angle = b1.angleBetweenCenters(b2);
+
+      return {
+        x1: b1.xCenter + (padding1 * Math.cos(angle)),
+        y1: b1.yCenter + (padding1 * Math.sin(angle)),
+        x2: b2.xCenter - (padding2 * Math.cos(angle)),
+        y2: b2.yCenter - (padding2 * Math.sin(angle)),
+      };
     }
   }
 
   /**
-   * Creates a straight bond styled as a strand bond.
-   * 
-   * @param {SVG.Doc} svg The SVG document to draw the straight bond on.
+   * @param {SVG.Doc} svg 
    * @param {Base} b1 
    * @param {Base} b2 
-   * @param {object} drawingDefaults The defaults property of the drawing that the straight bond is part of.
    * 
-   * @returns {StraightBond} The newly created straight bond.
+   * @returns {StraightBond} 
    */
-  static createStrand(svg, b1, b2, drawingDefaults) {
-    let styles = {
-      padding1: drawingDefaults.strandBondPadding,
-      padding2: drawingDefaults.strandBondPadding,
-      stroke: drawingDefaults.strandBondStroke,
-      strokeWidth: drawingDefaults.strandBondStrokeWidth
-    };
-
-    return StraightBond.create(svg, b1, b2, styles);
-  };
-
-  /**
-   * Creates a straight bond styled as a Watson Crick bond.
-   * 
-   * @param {SVG.Doc} svg The SVG document to draw the straight bond on.
-   * @param {Base} b1 
-   * @param {Base} b2 
-   * @param {object} drawingDefaults The defaults property of the drawing that the straight bond is part of.
-   * 
-   * @returns {StraightBond} The newly created straight bond.
-   */
-  static createWatsonCrick(svg, b1, b2, drawingDefaults) {
-    let letters = b1.letter.toUpperCase() + b2.letter.toUpperCase();
-    let stroke;
-
-    if (['AU', 'UA', 'AT', 'TA'].includes(letters)) {
-      stroke = drawingDefaults.watsonCrickAUTBondStroke;
-    } else if (['GC', 'CG'].includes(letters)) {
-      stroke = drawingDefaults.watsonCrickGCBondStroke;
-    } else if (['GU', 'UG', 'GT', 'TG'].includes(letters)) {
-      stroke = drawingDefaults.watsonCrickGUTBondStroke;
-    } else {
-      stroke = drawingDefaults.watsonCrickOtherBondStroke;
-    }
-
-    let styles = {
-      padding1: drawingDefaults.watsonCrickBondPadding,
-      padding2: drawingDefaults.watsonCrickBondPadding,
-      stroke: stroke,
-      strokeWidth: drawingDefaults.watsonCrickBondStrokeWidth
-    };
-
-    return StraightBond.create(svg, b1, b2, styles);
-  };
-
-  /**
-   * The padding1, padding2, stroke, strokeWidth, and opacity properties can be
-   * specified via the optional styles argument.
-   * 
-   * @param {SVG.Doc} svg The SVG document to draw the straight bond on.
-   * @param {Base} b1 
-   * @param {Base} b2 
-   * @param {object} [styles={}] Optional stylings of the straight bond.
-   * 
-   * @returns {StraightBond} The newly created straight bond.
-   */
-  static create(svg, b1, b2, styles={}) {
-    let padding1 = styles.padding1 === undefined ? 0 : styles.padding1;
-    let padding2 = styles.padding2 === undefined ? 0 : styles.padding2;
-    let cs = StraightBond.coordinates(b1, b2, padding1, padding2);
+  static create(svg, b1, b2) {
+    let cs = StraightBond.coordinates(b1, b2, 8, 8);
     let line = svg.line(cs.x1, cs.y1, cs.x2, cs.y2);
-    
-    let stroke = styles.stroke === undefined ? '#000000' : styles.stroke;
-    let strokeWidth = styles.strokeWidth === undefined ? 1 : styles.strokeWidth;
-    let opacity = StraightBond.opacity(b1, b2, padding1, padding2);
-    
-    line.attr({
-      'id': createUUIDforSVG(),
-      'stroke': stroke,
-      'stroke-width': strokeWidth,
-      'opacity': opacity
-    });
-    
+    line.id(createUUIDforSVG());
     return new StraightBond(line, b1, b2);
   }
 
   /**
-   * The end of the SVG line object (defined by the x1 and y1 properties)
-   * is attached to base b1, and the other end (defined by the x2 and y2
-   * properties) is attached to base b2.
-   * 
-   * @param {SVG.Line} line The SVG line object of the straight bond.
+   * @param {SVG.Line} line 
    * @param {Base} b1 One base of the straight bond.
    * @param {Base} b2 The other base of the straight bond.
    */
   constructor(line, b1, b2) {
     this._base1 = b1;
     this._base2 = b2;
-    this._validateBases();
     
     this._line = line;
     this._validateLine();
+
     this._storePaddings();
   }
 
   /**
-   * @throws {Error} If a single base was input to be both bases of this bond.
-   */
-  _validateBases() {
-    if (Object.is(this._base1, this._base2)) {
-      throw new Error('A base cannot have a bond with itself.');
-    }
-  }
-
-  /**
-   * Validates the SVG line object of this straight bond.
-   * 
-   * @throws {Error} If the id property is not defined.
+   * @throws {Error} If the ID of the line is not a string or is an empty string.
    */
   _validateLine() {
-
-    // just checks that the id property is defined
-    if (typeof(this._line.attr('id')) !== 'string') {
-      throw new Error('The SVG line object of a straight bond must have a unique ID.');
+    if (typeof(this._line.id()) !== 'string' || this._line.id().length === 0) {
+      throw new Error('Invalid line ID.');
     }
   }
 
   /**
-   * @returns {Base} The base in this bond referred to as base1.
+   * @returns {Base} 
    */
   get base1() {
     return this._base1;
   }
 
   /**
-   * @returns {Base} The base in this bond referred to as base2.
+   * @returns {Base} 
    */
   get base2() {
     return this._base2;
   }
 
   /**
-   * @param {Base} b 
-   * 
-   * @returns {boolean} True if the given base is in this bond.
+   * @returns {string} 
    */
-  inBond(b) {
-    return Object.is(this.base1, b) || Object.is(this.base2, b);
+  get id() {
+    return this._line.attr('id');
   }
 
   /**
-   * @param {Base} b One of the bases in this bond.
-   * 
-   * @returns {Base} The other base in this bond.
-   * 
-   * @throws {Error} If the given base is not in this bond.
-   */
-  otherBase(b) {
-    if (!this.inBond(b)) {
-      throw new Error("The given base is not in this bond.");
-    } else if (Object.is(this.base1, b)) {
-      return this.base2;
-    } else {
-      return this.base1;
-    }
-  }
-  
-  /**
-   * Initializes the _padding1 and _padding2 properties, which store the paddings
-   * for bases 1 and 2, respectively.
-   * 
-   * The _padding1 and _padding2 properties are necessary for the reposition method
-   * to work.
+   * Sets the _padding1 and _padding2 properties based on the current positions
+   * of the bases and line of this straight bond.
    */
   _storePaddings() {
     this._padding1 = distanceBetween(
       this.base1.xCenter,
       this.base1.yCenter,
       this._line.attr('x1'),
-      this._line.attr('y1')
+      this._line.attr('y1'),
     );
 
     this._padding2 = distanceBetween(
       this.base2.xCenter,
       this.base2.yCenter,
       this._line.attr('x2'),
-      this._line.attr('y2')
+      this._line.attr('y2'),
     );
   }
 
-  get id() {
-    return this._line.attr('id');
-  }
-
   /**
-   * @returns {number} The distance between the center of base 1 and its end of the line.
+   * @returns {number} 
    */
   get padding1() {
     return this._padding1;
   }
 
   /**
-   * @param {number} pdg The new padding.
+   * @param {number} p 
    */
-  set padding1(pdg) {
-    this._padding1 = pdg;
-    this.reposition();
+  set padding1(p) {
+    this._reposition(p, this.padding2);
   }
 
   /**
-   * @returns {number} The distance between the center of base 2 and its end of the line.
+   * @returns {number} 
    */
   get padding2() {
     return this._padding2;
   }
 
   /**
-   * @param {number} pdg The new padding.
+   * @param {number} p 
    */
-  set padding2(pdg) {
-    this._padding2 = pdg;
-    this.reposition();
+  set padding2(p) {
+    this._reposition(this.padding1, p);
   }
 
   /**
-   * Repositions the SVG line object of this straight bond based on
-   * the current positions of the two bases of this bond and the padding
-   * properties of this bond.
+   * Repositions the line of this straight bond based on the current positions
+   * of the bases of this straight bond.
    */
   reposition() {
-    let cs = StraightBond.coordinates(this.base1, this.base2, this.padding1, this.padding2);
-    let opacity = StraightBond.opacity(this.base1, this.base2, this.padding1, this.padding2);
+    this._reposition(this.padding1, this.padding2);
+  }
+
+  /**
+   * Repositions the line of this straight bond based on the current positions
+   * of the bases of this straight bond and the given paddings.
+   * 
+   * @param {number} padding1 
+   * @param {number} padding2 
+   */
+  _reposition(padding1, padding2) {
+    let cs = StraightBond.coordinates(this.base1, this.base2, padding1, padding2);
     
     this._line.attr({
       'x1': cs.x1,
       'y1': cs.y1,
       'x2': cs.x2,
       'y2': cs.y2,
-      'opacity': opacity
     });
+
+    this._storePaddings();
   }
 
+  /**
+   * Inserts the line of this straight bond before the given element.
+   * 
+   * @param {SVG.Element} ele 
+   */
   insertBefore(ele) {
     this._line.insertBefore(ele);
   }
 
+  /**
+   * Inserts the line of this straight bond after the given element.
+   * 
+   * @param {SVG.Element} ele 
+   */
   insertAfter(ele) {
     this._line.insertAfter(ele);
   }
 
   /**
-   * @returns {string} The stroke color of the SVG line object of this straight bond.
+   * @returns {string} 
    */
   get stroke() {
     return this._line.attr('stroke');
   }
 
+  /**
+   * @param {string} s 
+   */
   set stroke(s) {
     this._line.attr({ 'stroke': s });
   }
 
   /**
-   * @returns {number} The stroke width of the SVG line object of this straight bond.
+   * @returns {number} 
    */
   get strokeWidth() {
     return this._line.attr('stroke-width');
   }
 
+  /**
+   * @param {number} sw 
+   */
   set strokeWidth(sw) {
     this._line.attr({ 'stroke-width': sw });
-  }
-
-  /**
-   * @returns {number} The opacity of the SVG line object of this straight bond.
-   */
-  get opacity() {
-    return this._line.attr('opacity');
   }
 }
 
--- src/draw/StraightBond.test.js
+++ src/draw/StraightBond.test.js
@@ -2,172 +2,81 @@ import StraightBond from './StraightBond';
 import createNodeSVG from './createNodeSVG';
 import createUUIDforSVG from './createUUIDforSVG';
 import Base from './Base';
-import Drawing from './Drawing';
 
-it('coordinates', () => {
+function checkCoordinates(cs, ecs) {
+  expect(cs.x1).toBeCloseTo(ecs.x1, 6);
+  expect(cs.y1).toBeCloseTo(ecs.y1, 6);
+  expect(cs.x2).toBeCloseTo(ecs.x2, 6);
+  expect(cs.y2).toBeCloseTo(ecs.y2, 6);
+}
+
+it('coordinates static method', () => {
   let svg = createNodeSVG();
   let b1 = Base.create(svg, 'A', 1, 2);
   let b2 = Base.create(svg, 'U', 5, 6);
   
-  function checkCoordinates(cs, ex1, ey1, ex2, ey2) {
-    expect(cs.x1).toBeCloseTo(ex1, 6);
-    expect(cs.y1).toBeCloseTo(ey1, 6);
-    expect(cs.x2).toBeCloseTo(ex2, 6);
-    expect(cs.y2).toBeCloseTo(ey2, 6);
-  }
+  // basic test
+  checkCoordinates(
+    StraightBond.coordinates(b1, b2, 1, 2),
+    {
+      x1: 1 + (2 ** -0.5),
+      y1: 2 + (2 ** -0.5),
+      x2: 5 - (2 ** 0.5),
+      y2: 6 - (2 ** 0.5),
+    },
+  );
 
-  let cs0 = StraightBond.coordinates(b1, b2, 1, 2);
+  // paddings of zero
+  checkCoordinates(
+    StraightBond.coordinates(b1, b2, 0, 0),
+    {
+      x1: 1,
+      y1: 2,
+      x2: 5,
+      y2: 6,
+    },
+  );
+
+  // negative base coordinates
+  b1.move(-2, -1);
+  b2.move(-10.5, -100);
   
   checkCoordinates(
-    cs0,
-    1 + (2 ** -0.5),
-    2 + (2 ** -0.5),
-    5 - (2 ** 0.5),
-    6 - (2 ** 0.5)
+    StraightBond.coordinates(b1, b2, 2.5, 1.111),
+    {
+      x1: -2.2138596577358562,
+      y1: -3.49083601362938,
+      x2: -10.404960768102185,
+      y2: -98.89307247554311,
+    },
   );
 
-  // zero paddings
-  let cs1 = StraightBond.coordinates(b1, b2, 0, 0);
-  checkCoordinates(cs1, 1, 2, 5, 6);
-
-  // negative coordinates
-  b1.move(-2, -1);
-  let cs2 = StraightBond.coordinates(b1, b2, 2, 1);
-
+  // paddings greater than distance between bases
   checkCoordinates(
-    cs2,
-    -2 + (2 ** 0.5),
-    -1 + (2 ** 0.5),
-    5 - (2 ** -0.5),
-    6 - (2 ** -0.5)
+    StraightBond.coordinates(b1, b2, 60, 60),
+    {
+      x1: -6.25,
+      y1: -50.5,
+      x2: -6.25,
+      y2: -50.5,
+    },
   );
 });
 
-it('opacity static', () => {
-  let svg = createNodeSVG();
-  let b1 = Base.create(svg, 'A', 1, 2);
-  let b2 = Base.create(svg, 'U', 4, 6);
-  expect(b1.distanceBetweenCenters(b2)).toBeCloseTo(5);
-
-  expect(StraightBond.opacity(b1, b2, 1, 2)).toEqual(1);
-  expect(StraightBond.opacity(b1, b2, 0, 0)).toEqual(1);
-  expect(StraightBond.opacity(b1, b2, 4, 3)).toEqual(0);
-});
-
-it('createStrand', () => {
-  let svg = createNodeSVG();
-  let drawing = new Drawing(svg);
-  let b1 = Base.create(drawing._svg, 'A', 1, 2);
-  let b2 = Base.create(drawing._svg, 'A', 2, 2);
-
-  let strandBond = StraightBond.createStrand(drawing._svg, b1, b2, drawing.defaults);
-  expect(strandBond.padding1).toBeCloseTo(drawing.defaults.strandBondPadding, 6);
-  expect(strandBond.padding2).toBeCloseTo(drawing.defaults.strandBondPadding, 6);
-  expect(strandBond.stroke).toEqual(drawing.defaults.strandBondStroke);
-  expect(strandBond.strokeWidth).toBeCloseTo(drawing.defaults.strandBondStrokeWidth, 6);
-});
-
-it('createWatsonCrick', () => {
-  let svg = createNodeSVG();
-  let drawing = new Drawing(svg);
-
-  let ba = Base.create(drawing._svg, 'A', 1, 2);
-  let bu = Base.create(drawing._svg, 'u', 4, 5);
-  let bg = Base.create(drawing._svg, 'g', 9, 9);
-  let bc = Base.create(drawing._svg, 'C', 6, 8);
-  let bt = Base.create(drawing._svg, 't', -2, -3);
-
-  let auBond = StraightBond.createWatsonCrick(drawing._svg, ba, bu, drawing.defaults);
-  expect(auBond.padding1).toBeCloseTo(drawing.defaults.watsonCrickBondPadding, 6);
-  expect(auBond.padding2).toBeCloseTo(drawing.defaults.watsonCrickBondPadding, 6);
-  expect(auBond.stroke).toEqual(drawing.defaults.watsonCrickAUTBondStroke);
-  expect(auBond.strokeWidth).toBeCloseTo(drawing.defaults.watsonCrickBondStrokeWidth, 6);
-
-  let gcBond = StraightBond.createWatsonCrick(drawing._svg, bg, bc, drawing.defaults);
-  expect(gcBond.stroke).toEqual(drawing.defaults.watsonCrickGCBondStroke);
-
-  let taBond = StraightBond.createWatsonCrick(drawing._svg, bt, ba, drawing.defaults);
-  expect(taBond.stroke).toEqual(drawing.defaults.watsonCrickGCBondStroke);
-
-  let tgBond = StraightBond.createWatsonCrick(drawing._svg, bt, bg, drawing.defaults);
-  expect(tgBond.stroke).toEqual(drawing.defaults.watsonCrickGUTBondStroke);
-});
-
-it('create', () => {
+it('basic test of create static method', () => {
   let svg = createNodeSVG();
   let b1 = Base.create(svg, 'A', 1, 1);
   let b2 = Base.create(svg, 'U', 4, 4);
-
-  // no styles argument
-  let sb0 = StraightBond.create(svg, b1, b2);
-
-  let styles1 = {
-    padding1: 1,
-    padding2: 2 ** 0.5,
-    stroke: '#00ff00',
-    strokeWidth: 3
-  };
-
-  let sb1 = StraightBond.create(svg, b1, b2, styles1);
-  expect(sb1.padding1).toBeCloseTo(1, 6);
-  expect(sb1.padding2).toBeCloseTo(2 ** 0.5, 6);
-  expect(sb1.stroke).toEqual('#00ff00');
-  expect(sb1.strokeWidth).toBeCloseTo(3, 6);
-  expect(sb1.opacity).toEqual(1);
-
-  // should result in opacity of zero
-  let styles2 = {
-    padding1: 3,
-    padding2: 3
-  };
-
-  let sb2 = StraightBond.create(svg, b1, b2, styles2);
-  expect(sb2.padding1).toBeCloseTo(3, 6);
-  expect(sb2.padding2).toBeCloseTo(3, 6);
-  expect(sb2.opacity).toEqual(0);
+  expect(() => StraightBond.create(svg, b1, b2)).not.toThrow();
 });
 
-it('_validateBases', () => {
+it('basic test of constructor', () => {
   let svg = createNodeSVG();
-  let line = svg.line(1, 1, 2, 2);
-  line.attr({ 'id': createUUIDforSVG() });
-
-  let b1 = Base.create(svg, 'A', 1, 2);
-  let b2 = Base.create(svg, 'U', 2, 3);
+  let line = svg.line(0, 0.22, 2.45, -1);
+  line.id(createUUIDforSVG());
+  let b1 = Base.create(svg, 'G', -1, -2);
+  let b2 = Base.create(svg, 'C', 10, 0.002);
   expect(() => new StraightBond(line, b1, b2)).not.toThrow();
-
-  let b3 = Base.create(svg, 'G', 4, 5);
-  expect(() => new StraightBond(line, b3, b3)).toThrow();
-});
-
-it('inBond', () => {
-  let svg = createNodeSVG();
-  let line = svg.line(1, 1, 2, 2);
-  line.attr({ 'id': createUUIDforSVG() });
-
-  let b1 = Base.create(svg, 'A', 1, 2);
-  let b2 = Base.create(svg, 'U', 3, 1);
-  let bond = new StraightBond(line, b1, b2);
-  expect(bond.inBond(b1)).toBeTruthy();
-  expect(bond.inBond(b2)).toBeTruthy();
-
-  let b3 = Base.create(svg, 'G', 4, 5);
-  expect(bond.inBond(b3)).toBeFalsy();
-});
-
-it('otherBase', () => {
-  let svg = createNodeSVG();
-  let line = svg.line(1, 1, 2, 2);
-  line.attr({ 'id': createUUIDforSVG() });
-
-  let b1 = Base.create(svg, 'G', 3, 4);
-  let b2 = Base.create(svg, 'C', 6, 8);
-  let bond = new StraightBond(line, b1, b2);
-  expect(bond.otherBase(b1)).toBe(b2);
-  expect(bond.otherBase(b2)).toBe(b1);
-
-  let b3 = Base.create(svg, 'A', 1, 3);
-  expect(() => bond.otherBase(b3)).toThrow();
 });
 
 it('_validateLine', () => {
@@ -176,14 +85,28 @@ it('_validateLine', () => {
   let b1 = Base.create(svg, 'U', 2, 3);
 
   let line0 = svg.line(1, 1, 2, 2);
-  line0.attr({ 'id': createUUIDforSVG() });
+  line0.id(createUUIDforSVG());
   expect(() => new StraightBond(line0, b0, b1)).not.toThrow();
 
-  let line1 = svg.line(2, 2, 3, 3);
-  expect(() => new StraightBond(line1, b0, b1)).toThrow();
+  /* It does seems possible using the SVG.js framework to create
+  an element with no ID. */
+
+  // line with ID that is not a string
+  let line2 = svg.line(-1, -2, 0, 0);
+  line2.id(22);
+  expect(() => new StraightBond(line2, b0, b1)).toThrow();
 });
 
-it('id', () => {
+it('base1 and base2 getters', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'A', 4.5, 6);
+  let b2 = Base.create(svg, 'u', -10, -4);
+  let sb = StraightBond.create(svg, b1, b2);
+  expect(sb.base1).toBe(b1);
+  expect(sb.base2).toBe(b2);
+});
+
+it('id getter', () => {
   let svg = createNodeSVG();
   let b0 = Base.create(svg, 'A', 1, 2);
   let b1 = Base.create(svg, 'U', 2, 3);
@@ -192,52 +115,195 @@ it('id', () => {
   let id = createUUIDforSVG();
   line.attr({ 'id': id });
   let sb = new StraightBond(line, b0, b1);
-  expect(sb.id).toEqual(id);
+  
+  // check getter
+  expect(sb.id).toBe(id);
+
+  // check actual value
+  expect(sb._line.id()).toBe(id);
 });
 
-it('padding', () => {
+it('padding1 getter and setter', () => {
   let svg = createNodeSVG();
-  let line = svg.line(1, 1, 2, 2);
-  line.attr({ 'id': createUUIDforSVG() });
-  let b1 = Base.create(svg, 'A', 0, 0);
-  let b2 = Base.create(svg, 'U', 3, 3);
-  let sb = new StraightBond(line, b1, b2);
-  expect(sb.padding1).toBeCloseTo(2 ** 0.5, 6);
-  expect(sb.padding2).toBeCloseTo(2 ** 0.5, 6);
+  let b1 = Base.create(svg, 'a', 40.3, 4.9);
+  let b2 = Base.create(svg, 'T', 1, 4);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  let x2 = sb._line.attr('x2');
+  let y2 = sb._line.attr('y2');
+
+  sb.padding1 = 0.25;
+
+  // check getter
+  expect(sb.padding1).toBeCloseTo(0.25, 6);
+
+  // check actual line coordinates
+  checkCoordinates(
+    {
+      x1: sb._line.attr('x1'),
+      y1: sb._line.attr('y1'),
+      x2: sb._line.attr('x2'),
+      y2: sb._line.attr('y2'),
+    },
+    {
+      x1: 40.05006552984633,
+      y1: 4.89427630984381,
+      x2: x2,
+      y2: y2,
+    },
+  );
 });
 
-it('stroke', () => {
+it('padding2 getter and setter', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'a', 0.2, -3);
+  let b2 = Base.create(svg, 'T', 120.5, 8);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  let x1 = sb._line.attr('x1');
+  let y1 = sb._line.attr('y1');
+
+  sb.padding2 = 0.5;
+
+  // check getter
+  expect(sb.padding2).toBeCloseTo(0.5, 6);
+
+  // check actual line coordinates
+  checkCoordinates(
+    {
+      x1: sb._line.attr('x1'),
+      y1: sb._line.attr('y1'),
+      x2: sb._line.attr('x2'),
+      y2: sb._line.attr('y2'),
+    },
+    {
+      x1: x1,
+      y1: y1,
+      x2: 120.00207721370248,
+      y2: 7.954470900671049,
+    },
+  );
+});
+
+it('reposition', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'a', 0.2, -3);
+  let b2 = Base.create(svg, 'T', 120.5, 8);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  sb.padding1 = 5;
+  sb.padding2 = 0.99;
+
+  b1.move(-10, 0.5);
+  b2.move(1000, 980.2);
+
+  sb.reposition();
+
+  checkCoordinates(
+    {
+      x1: sb._line.attr('x1'),
+      y1: sb._line.attr('y1'),
+      x2: sb._line.attr('x2'),
+      y2: sb._line.attr('y2'),
+    },
+    {
+      x1: -6.411041600732329,
+      y1: 3.981289647289641,
+      x2: 999.289386236945,
+      y2: 979.5107046498367,
+    },
+  );
+});
+
+it('_reposition', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'a', 0.2, -3);
+  let b2 = Base.create(svg, 'T', 120.5, 8);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  b1.move(-10, 0.5);
+  b2.move(1000, 980.2);
+
+  sb._reposition(5, 0.99);
+
+  checkCoordinates(
+    {
+      x1: sb._line.attr('x1'),
+      y1: sb._line.attr('y1'),
+      x2: sb._line.attr('x2'),
+      y2: sb._line.attr('y2'),
+    },
+    {
+      x1: -6.411041600732329,
+      y1: 3.981289647289641,
+      x2: 999.289386236945,
+      y2: 979.5107046498367,
+    },
+  );
+});
+
+it('insertBefore', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'a', -0.445, 0.56);
+  let b2 = Base.create(svg, 'T', 1, 2);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  let circle = svg.circle(100);
+  let rect = svg.rect(2);
+
+  expect(sb._line.position()).toBeLessThan(circle.position());
+  expect(sb._line.position()).toBeLessThan(rect.position());
+
+  sb.insertBefore(rect);
+
+  expect(sb._line.position()).toBeGreaterThan(circle.position());
+  expect(sb._line.position()).toBeLessThan(rect.position());
+});
+
+it('insertAfter', () => {
+  let svg = createNodeSVG();
+  let b1 = Base.create(svg, 'a', -0.445, 0.56);
+  let b2 = Base.create(svg, 'T', 1, 2);
+  let sb = StraightBond.create(svg, b1, b2);
+
+  let circle = svg.circle(100);
+  let rect = svg.rect(2);
+
+  expect(sb._line.position()).toBeLessThan(circle.position());
+  expect(sb._line.position()).toBeLessThan(rect.position());
+
+  sb.insertAfter(circle);
+
+  expect(sb._line.position()).toBeGreaterThan(circle.position());
+  expect(sb._line.position()).toBeLessThan(rect.position());
+});
+
+it('stroke getter and setter', () => {
   let svg = createNodeSVG();
   let b1 = Base.create(svg, 'A', 1, 1);
   let b2 = Base.create(svg, 'U', 4, 4);
+  let sb = StraightBond.create(svg, b1, b2);
 
-  let line = svg.line(2, 2, 3, 3);
-  line.attr({ 'id': createUUIDforSVG(), 'stroke': '#00ff00' });
-  let sb = new StraightBond(line, b1, b2);
-  expect(sb.stroke).toEqual('#00ff00');
+  sb.stroke = '#456abc';
+  
+  // check getter
+  expect(sb.stroke).toBe('#456abc');
+
+  // check actual value
+  expect(sb._line.attr('stroke')).toBe('#456abc');
 });
 
-it('strokeWidth', () => {
+it('strokeWidth getter and setter', () => {
   let svg = createNodeSVG();
   let b1 = Base.create(svg, 'A', 1, 1);
   let b2 = Base.create(svg, 'U', 4, 4);
+  let sb = StraightBond.create(svg, b1, b2);
 
-  let line = svg.line(2, 2, 3, 3);
-  line.attr({ 'id': createUUIDforSVG(), 'stroke-width': 2 });
-  let sb = new StraightBond(line, b1, b2);
-  expect(sb.strokeWidth).toEqual(2);
-});
+  sb.strokeWidth = 2.3;
 
-it('opacity getter', () => {
-  let svg = createNodeSVG();
-  let b1 = Base.create(svg, 'A', 1, 2);
-  let b2 = Base.create(svg, 'U', 4, 5);
+  // check getter
+  expect(sb.strokeWidth).toBeCloseTo(2.3, 6);
 
-  let styles0 = { padding1: 1, padding2: 1 };
-  let sb0 = StraightBond.create(svg, b1, b2, styles0);
-  expect(sb0.opacity).toEqual(1);
-
-  let styles1 = { padding1: 3, padding2: 3 };
-  let sb1 = StraightBond.create(svg, b1, b2, styles1);
-  expect(sb1.opacity).toEqual(0);
+  // check actual value
+  expect(sb._line.attr('stroke-width')).toBeCloseTo(2.3, 6);
 });
