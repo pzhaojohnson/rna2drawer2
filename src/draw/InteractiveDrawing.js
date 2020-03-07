adca236b84cd0afd419bcec479516731132826b8
Philip Johnson <p.zhao.johnson@gmail.com>
2020-03-07 17:43:58 -0500

    Wrote pseudocode for InteractiveDrawing class

--- src/draw/InteractiveDrawing.js
+++ src/draw/InteractiveDrawing.js
@@ -1,5 +1,11 @@
 import Drawing from './Drawing';
 
+const _MODES = {
+  FOLDING: 'FOLDING',
+  PIVOTING: 'PIVOTING',
+  SELECTING_BASES: 'SELECTING_BASES',
+};
+
 class InteractiveDrawing {
 
   /**
@@ -7,6 +13,9 @@ class InteractiveDrawing {
    */
   constructor(container) {
     this._drawing = new Drawing(container);
+
+    this._layoutType = 'strict';
+    this._mode = _MODES.FOLDING;
   }
 
   /**
@@ -23,6 +32,62 @@ class InteractiveDrawing {
   applyStrictLayout() {
     this._drawing.applyStrictLayout();
   }
+
+  handleMousemove(e) {
+    if (this._mode === _MODES.PIVOTING) {
+      this._handleMousemovePivoting(e);
+    } else if (this._mode === _MODES.SELECTING_BASES) {
+      this._handleMousemoveSelectingBases(e);
+    }
+  }
+
+  handleMouseup(e) {
+    if (this._mode === _MODES.PIVOTING) {
+      this._handleMouseupPivoting(e);
+    } else if (this._mode === _MODES.SELECTING_BASES) {
+      this._handleMouseupSelectingBases(e);
+    }
+  }
+
+  /**
+   * @param {Base} b 
+   */
+  handleMouseoverBase(b) {
+    if (this._mode === _MODES.FOLDING) {
+      this._handleMouseoverBaseFolding(b);
+    }
+  }
+
+  _handleMouseoverBaseFolding(b) {
+    let p = this._drawing.strictLayoutPositionOfBase(b);
+    
+    let startingState = {
+      hovered: p,
+      firstSelected: this._firstSelected,
+      lastSelected: this._lastSelected,
+      partners: this._drawing.strictLayoutPartners(),
+      baseProps: this._strictLayoutProps.base,
+    };
+
+    let endingState = FoldingMode.handleMouseover(startingState);
+
+    if (partnersDifferent || basePropsDifferent) {
+      this.pushUndo();
+    }
+
+    let layout = new StrictLayout(endingState.partners, generalProps, baseProps);
+    this._drawing.applyStrictLayout(layout);
+    this.baseProps = baseProps;
+    this.applyHighlighting();
+  }
+
+  handleMouseoutBase(e) {}
+
+  handleMousedownBase(e) {}
+
+  handleDblclickBase(e) {}
+
+  handleDblclickTertiaryBond(e) {}
 }
 
 export default InteractiveDrawing;
