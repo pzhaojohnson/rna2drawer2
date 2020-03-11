ce144302516bb52173480bce3e4aed9117c02099
Philip Johnson <p.zhao.johnson@gmail.com>
2020-03-11 11:53:03 -0400

    Began adding test suite for circleCenter function

---
+++ src/draw/layout/singleseq/strict/circleCenter.test.js
@@ -0,0 +1,47 @@
+import circleCenter from './circleCenter';
+
+it('', () => {});
+
+// more than semicircle
+
+// normal case
+
+// distance between points is zero
+
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
+// clockwise polar distance is normal
+
+// distance between points is very small
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
+// clockwise polar distance is normal
+
+// distance between points is normal
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
+
+// less than semicircle
+
+// normal case
+
+// distance between points is zero
+
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
+// clockwise polar distance is normal
+
+// distance between points is very small
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
+// clockwise polar distance is normal
+
+// distance between points is normal
+// clockwise polar distance is zero
+// clockwise polar distance is negative
+// clockwise polar distance is very small
