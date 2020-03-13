b5d46f8d6eca7e454665ab1607ff723aa9660e79
unknown <p.zhao.johnson@gmail.com>
2020-03-13 09:59:57 -0400

    baseCoordinatesHairpin function now uses circleCenter function

--- src/draw/layout/singleseq/strict/UnpairedRegionHairpin.js
+++ src/draw/layout/singleseq/strict/UnpairedRegionHairpin.js
@@ -1,5 +1,8 @@
 import VirtualBaseCoordinates from '../../VirtualBaseCoordinates';
-import { polarizeLength } from './circle';
+import { polarizeLength, circleCenter } from './circle';
+import distanceBetween from '../../../distanceBetween';
+import angleBetween from '../../../angleBetween';
+import normalizeAngle from '../../../normalizeAngle';
 
 /**
  * @param {UnpairedRegion} ur 
@@ -9,38 +12,36 @@ import { polarizeLength } from './circle';
  */
 function baseCoordinatesHairpin(ur) {
   let coordinates = [];
+
+  let st = ur.boundingStem5;
+
+  let cc = circleCenter(
+    st.xTopLeft,
+    st.yTopLeft,
+    st.xTopRight,
+    st.yTopRight,
+    ur.polarLength,
+  );
+
+  let xCenter = cc.x;
+  let yCenter = cc.y;
+
+  let bcb5 = ur.baseCoordinatesBounding5();
+  let bcb3 = ur.baseCoordinatesBounding3();
+  let radius = distanceBetween(xCenter, yCenter, bcb5.xCenter, bcb5.yCenter);
+  let angle5 = angleBetween(xCenter, yCenter, bcb5.xCenter, bcb5.yCenter);
+  let angle3 = angleBetween(xCenter, yCenter, bcb3.xCenter, bcb3.yCenter);
+  angle3 = normalizeAngle(angle3, angle5);
+
+  let aincr = (angle3 - angle5) / (ur.size + 1);
+  let a = angle5 + aincr;
   
-  let circumference = ur.polarLength + ur.boundingStem5.polarWidth;
-  let radius = circumference / (2 * Math.PI);
-  
-  let cbp5 = ur.baseCoordinatesBounding5();
-  let cbp3 = ur.baseCoordinatesBounding3();
-  let o = cbp5.distanceBetweenCenters(cbp3) / 2;
-  let angle = ur.boundingStem5.angle + Math.asin(o / radius);
-
-  let xCenter;
-  let yCenter;
-
-  // the width of the stem is too big for the radius of the hairpin
-  if (isNaN(angle)) {
-    xCenter = (cbp5.xCenter + cbp3.xCenter) / 2;
-    yCenter = (cbp5.yCenter + cbp3.yCenter) / 2;
-  } else {
-    xCenter = cbp5.xCenter + (radius * Math.cos(angle));
-    yCenter = cbp5.yBottom + (radius * Math.sin(angle));
-  }
-
-  angle = ur.boundingStem5.angle + Math.PI;
-  angle += (2 * Math.PI) * ((ur.boundingStem5.polarWidth / 2) / circumference);
-  angle += (2 * Math.PI) * (polarizeLength(0.5) / circumference);
-
   for (let p = ur.boundingPosition5 + 1; p < ur.boundingPosition3; p++) {
     coordinates.push(new VirtualBaseCoordinates(
       xCenter + (radius * Math.cos(angle)) - 0.5,
-      yCenter + (radius * Math.sin(angle)) - 1
+      yCenter + (radius * Math.sin(angle)) - 0.5,
     ));
-
-    angle += (2 * Math.PI) * (polarizeLength(1) / circumference);
+    a += aincr;
   }
   
   return coordinates;
