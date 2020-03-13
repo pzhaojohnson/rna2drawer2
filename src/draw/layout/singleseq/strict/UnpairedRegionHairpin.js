21c82958d50ffc25815c24a0b2f6acec16a55875
unknown <p.zhao.johnson@gmail.com>
2020-03-13 10:08:15 -0400

    Small Hairpin loops no longer overlap with the top base pair of the stem

--- src/draw/layout/singleseq/strict/UnpairedRegionHairpin.js
+++ src/draw/layout/singleseq/strict/UnpairedRegionHairpin.js
@@ -28,13 +28,22 @@ function baseCoordinatesHairpin(ur) {
 
   let bcb5 = ur.baseCoordinatesBounding5();
   let bcb3 = ur.baseCoordinatesBounding3();
+  
   let radius = distanceBetween(xCenter, yCenter, bcb5.xCenter, bcb5.yCenter);
+
   let angle5 = angleBetween(xCenter, yCenter, bcb5.xCenter, bcb5.yCenter);
   let angle3 = angleBetween(xCenter, yCenter, bcb3.xCenter, bcb3.yCenter);
   angle3 = normalizeAngle(angle3, angle5);
 
   let aincr = (angle3 - angle5) / (ur.size + 1);
   let a = angle5 + aincr;
+
+  let polarLength = (angle3 - angle5) * radius;
+  let semicircleLength = Math.PI * bcb5.distanceBetweenCenters(bcb3) / 2;
+
+  if (polarLength < semicircleLength) {
+    radius += 1 - (polarLength / semicircleLength);
+  }
   
   for (let p = ur.boundingPosition5 + 1; p < ur.boundingPosition3; p++) {
     coordinates.push(new VirtualBaseCoordinates(
