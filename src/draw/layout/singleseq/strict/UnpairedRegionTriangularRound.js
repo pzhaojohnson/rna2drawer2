abd01500c86c7eec3fb7635e957decce53bcea22
Philip Johnson <p.zhao.johnson@gmail.com>
2020-03-14 18:30:31 -0400

    Worked on baseCoordinatesTriangularRound function and tests for it

--- src/draw/layout/singleseq/strict/UnpairedRegionTriangularRound.js
+++ src/draw/layout/singleseq/strict/UnpairedRegionTriangularRound.js
@@ -14,12 +14,7 @@ function baseCoordinatesTriangularRound(ur) {
   let coordinates = new Array(ur.size);
   let p = ur.boundingPosition5 + 1;
   let q = ur.boundingPosition3 - 1;
-  let boundingAngle5 = ur.boundingStem5.angle + (Math.PI / 2);
-
-  function done() {
-    return p > q;
-  }
-
+  
   function baseCoordinates(position) {
     if (position < ur.boundingPosition5 || position > ur.boundingPosition3) {
       throw new Error("Position is out of range.");
@@ -36,127 +31,103 @@ function baseCoordinatesTriangularRound(ur) {
     coordinates[position - ur.boundingPosition5 - 1] = new VirtualBaseCoordinates(xLeft, yTop);
   }
 
-  function straightDistance() {
-    return baseCoordinates(p - 1).distanceBetweenCenters(baseCoordinates(q + 1));
-  }
+  function circlePairs() {
+    let angle5 = ur.boundingStem5.angle;
+    let angle3 = ur.boundingStem3.angle;
+    angle3 = normalizeAngle(angle3, angle5);
+    let bisectingAngle = (angle5 + angle3) / 2;
 
-  function polarLength() {
-    return polarizeLength(p - q + 2);
-  }
-  
-  function bisectingAngle() {
-    let bstoa5 = ur.boundingStem5.angle;
-    let bstoa3 = normalizeAngle(ur.boundingStem3.angle, bstoa5);
-    return (bstoa5 + bstoa3) / 2;
-  }
+    let bcb5 = ur.baseCoordinatesBounding5();
+    let bcb3 = ur.baseCoordinatesBounding3();
+    let o = bcb5.distanceBetweenCenters(bcb3) / 2;
+    let radius = o / Math.sin(bisectingAngle - angle5);
 
-  function boundingAngle3() {
-    let bsa = bisectingAngle();
-    let ba5 = normalizeAngle(boundingAngle5, bsa);
-    return bsa - (ba5 - bsa);
-  }
+    let xCenter = bcb5.xLeft + (radius * Math.cos(angle5 + Math.PI));
+    let yCenter = bcb5.yTop + (radius * Math.sin(angle5 + Math.PI));
 
-  function angleSpan() {
-    let a5 = boundingAngle5 - (Math.PI / 2);
-    let a3 = normalizeAngle(boundingAngle3() + (Math.PI / 2), a5);
-    return a3 - a5;
-  }
-  
-  function boundingRadius() {
-    return (straightDistance() / 2) / Math.sin(angleSpan() / 2);
-  }
+    let circumference = 2 * Math.PI * radius;
+    let a5 = angle5 + ((2 * Math.PI) * ((ur.boundingStem5.polarWidth / 2) / circumference));
+    let a3 = angle3 - ((2 * Math.PI) * ((ur.boundingStem3.polarWidth / 2) / circumference));
+    let aincr = (2 * Math.PI) * (polarizeLength(1) / circumference);
+    a5 += aincr;
+    a3 += aincr;
 
-  function boundingPolarLength() {
-    return boundingRadius() * angleSpan();
-  }
-
-  function tooLong() {
-    return polarLength() > boundingPolarLength();
-  }
-
-  function boundingPair() {
-    let r = boundingRadius();
-    let xCenter = baseCoordinates(p - 1).xLeft + (r * Math.cos(boundingAngle5 + (Math.PI / 2)));
-    let yCenter = baseCoordinates(p - 1).yTop + (r * Math.sin(boundingAngle5 + (Math.PI / 2)));
-    
-    let aincr = angleSpan() * (polarizeLength(1) / boundingPolarLength());
-    let a = boundingAngle5 - (Math.PI / 2) + aincr;
-    
-    setBaseCoordinates(
-      p,
-      xCenter + (r * Math.cos(a)),
-      yCenter + (r * Math.sin(a))
-    );
-    
-    a = boundingAngle3() + (Math.PI / 2) - aincr;
-    
-    setBaseCoordinates(
-      q,
-      xCenter + (r * Math.cos(a)),
-      yCenter + (r * Math.sin(a))
-    );
-    
-    p++;
-    q--;
-    boundingAngle5 += aincr;
-  }
-
-  function triangularizeBoundingAngle5() {
-    let hyp = Math.floor((q - p + 1) / 2);
-    let obc = baseCoordinates(p - 1);
-    let rbc = baseCoordinates(q + 1);
-    let adj = (obc.distanceBetweenCenters(rbc) - 1) / 2;
-    let a;
-    
-    // in case adj >= hyp due to imprecision in floating point arithmetic
-    if (adj < hyp) {
-      a = Math.acos(adj / hyp);
-    } else {
-      a = 0;
+    function done() {
+      return p > q
+        || a3 - a5 <= Math.PI
+        || polarizeLength(q - p + 2) <= (a3 - a5) * radius;
     }
-    
-    boundingAngle5 = bisectingAngle() + ((Math.PI / 2) - a);
-  }
 
-  function triangularPair() {
-    if (p === q) {
-      let obc = baseCoordinates(p - 1);
-      let rbc = baseCoordinates(p + 1);
-      
+    while (!done()) {
       setBaseCoordinates(
         p,
-        ((obc.xLeft + rbc.xLeft) / 2) + Math.cos(bisectingAngle()),
-        ((obc.yTop + rbc.yTop) / 2) + Math.sin(bisectingAngle())
+        xCenter + (radius * Math.cos(a5)),
+        yCenter + (radius * Math.sin(a5)),
       );
-    } else {
-      let obc = baseCoordinates(p - 1);
-      
-      setBaseCoordinates(
-        p,
-        obc.xLeft + Math.cos(boundingAngle5),
-        obc.yTop + Math.sin(boundingAngle5)
-      );
-      
-      let rbc = baseCoordinates(q + 1);
-      
       setBaseCoordinates(
         q,
-        rbc.xLeft + Math.cos(boundingAngle3()),
-        rbc.yTop + Math.sin(boundingAngle3())
+        xCenter + (radius * Math.cos(a3)),
+        yCenter + (radius * Math.sin(a3)),
       );
+      p++;
+      q--;
+      a5 += aincr;
+      a3 -= aincr;
     }
-    
-    p++;
-    q--;
   }
 
-  function round() {
+  function trianglePairs() {
+    let obc = baseCoordinates(p - 1);
+    let rbc = baseCoordinates(q + 1);
+    
+    let hyp;
+    let opp;
+    if (p - q + 1 % 2 === 0) {
+      hyp = (p - q + 1) / 2;
+      opp = obc.distanceBetweenCenters(rbc) - 1;
+    } else {
+      hyp = (p - q + 2) / 2;
+      opp = obc.distanceBetweenCenters(rbc);
+    }
+    opp = Math.min(opp, hyp - 0.001);
+    
+    let bisectingAngle = obc.angleBetweenCenters(rbc) - (Math.PI / 2);
+    let a = Math.asin(opp / hyp);
+    let a5 = bisectingAngle + a;
+    let a3 = bisectingAngle - a;
+
+    function done() {
+      obc = baseCoordinates(p - 1);
+      rbc = baseCoordinates(q + 1);
+      return p > q
+        || polarizeLength(p - q + 2) <= (a5 - a3) * (obc.distanceBetweenCenters(rbc) / 2);
+    }
+
+    while (!done()) {
+      obc = baseCoordinates(p - 1);
+      setBaseCoordinates(
+        p,
+        obc.xLeft + Math.cos(a5),
+        obc.yTop + Math.sin(a5)
+      );
+      rbc = baseCoordinates(q + 1);
+      setBaseCoordinates(
+        q,
+        rbc.xLeft + Math.cos(a3),
+        rbc.yTop + Math.sin(a3)
+      );
+      p++;
+      q--;
+    }
+  }
+
+  function smushedRoundPairs() {
     let cc = circleCenter(
       baseCoordinates(p - 1).xLeft,
       baseCoordinates(p - 1).yTop,
       baseCoordinates(q + 1).xLeft,
       baseCoordinates(q + 1).yTop,
-      polarLength(),
+      polarizeLength(p - q + 2),
     );
     let xCenter = cc.x;
     let yCenter = cc.y;
@@ -180,7 +151,7 @@ function baseCoordinatesTriangularRound(ur) {
       baseCoordinates(q + 1).xLeft,
       baseCoordinates(q + 1).yTop,
     );
-    angle5 = normalizeAngle(angle5, angle3);
+    angle3 = normalizeAngle(angle3, angle5);
 
     let aincr = (angle3 - angle5) / (p - q + 2);
     let a = angle5 + aincr;
@@ -195,26 +166,9 @@ function baseCoordinatesTriangularRound(ur) {
     }
   }
 
-  if (!done() && tooLong()) {
-    boundingPair();
-  }
-
-  while (!done() && tooLong() && angleSpan() > Math.PI) {
-    boundingPair();
-  }
-
-  if (!done() && tooLong()) {
-    triangularizeBoundingAngle5();
-
-    while (!done() && tooLong()) {
-      triangularPair();
-    }
-  }
-
-  if (!done()) {
-    round();
-  }
-
+  circlePairs();
+  trianglePairs();
+  smushedRoundPairs();
   return coordinates;
 }
 
--- src/draw/layout/singleseq/strict/UnpairedRegionTriangularRound.test.js
+++ src/draw/layout/singleseq/strict/UnpairedRegionTriangularRound.test.js
@@ -1,6 +1,62 @@
 import baseCoordinatesTriangularRound from './UnpairedRegionTriangularRound';
+import StrictLayoutGeneralProps from './StrictLayoutGeneralProps';
+import StrictLayoutBaseProps from './StrictLayoutBaseProps';
+import Stem from './Stem';
+import { RoundLoop } from './StemLayout';
 
-it('', () => {});
+function zeroStretch3(length) {
+  let bps = [];
+  for (let i = 0; i < length; i++) {
+    bps.push(new StrictLayoutBaseProps());
+    bps[i].stretch3 = 0;
+  }
+  return bps;
+}
+
+it('empty unpaired region', () => {
+  let partners = [3, null, 1, 6, null, 4];
+  let gps = new StrictLayoutGeneralProps();
+  let bps = zeroStretch3(partners.length);
+
+  let st = new Stem(0, partners, gps, bps);
+  RoundLoop.setCoordinatesAndAngles(st, gps, bps);
+
+  let it = st.loopIterator();
+  it.next();
+  it.next();
+  let ur = it.next().value;
+
+  let coords = baseCoordinatesTriangularRound(ur);
+  expect(coords.length).toBe(0);
+});
+
+it('unpaired region of length one', () => {
+  let partners = [3, null, 1, null, 7, null, 5];
+  let gps = new StrictLayoutGeneralProps();
+  let bps = zeroStretch3(partners.length);
+  
+  let st = new Stem(0, partners, gps, bps);
+  RoundLoop.setCoordinatesAndAngles(st, gps, bps);
+
+  let it = st.loopIterator();
+  it.next();
+  it.next();
+  let ur = it.next().value;
+
+  let coords = baseCoordinatesTriangularRound(ur);
+  let xs = '';
+  let ys = '';
+  xs += ur.baseCoordinatesBounding5().xCenter + '\n';
+  ys += ur.baseCoordinatesBounding5().yCenter + '\n';
+  coords.forEach(vbc => {
+    xs += vbc.xCenter + '\n';
+    ys += vbc.yCenter + '\n';
+  });
+  xs += ur.baseCoordinatesBounding3().xCenter;
+  ys += ur.baseCoordinatesBounding3().yCenter;
+  console.log(xs);
+  console.log(ys);
+});
 
 // less than semicircle
 
