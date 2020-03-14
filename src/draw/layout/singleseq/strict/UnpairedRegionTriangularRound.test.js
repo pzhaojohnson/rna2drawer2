698953dadeff4923643e41e8e5a20ef2c74d8d9b
unknown <p.zhao.johnson@gmail.com>
2020-03-13 21:09:34 -0400

    Started working on test suite for baseCoordinatesTriangularRound function

---
+++ src/draw/layout/singleseq/strict/UnpairedRegionTriangularRound.test.js
@@ -0,0 +1,21 @@
+import baseCoordinatesTriangularRound from './UnpairedRegionTriangularRound';
+
+it('', () => {});
+
+// less than semicircle
+
+// exactly semicircle
+
+// more than semicircle
+
+// much more than semicircle
+
+// empty unpaired region
+
+// length one
+
+// distance between bounding stems is zero
+
+// bounding stems have same angle
+
+// bounding stems completely overlap
