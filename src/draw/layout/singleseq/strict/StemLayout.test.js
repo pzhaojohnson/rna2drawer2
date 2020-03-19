17a84b51568a29622ae7ed0217d9ffef66f22cf8
unknown <p.zhao.johnson@gmail.com>
2020-03-18 21:38:34 -0400

    Added tests for traverseUnpairedRegion53 static method of FlatOutermostLoop class

--- src/draw/layout/singleseq/strict/StemLayout.test.js
+++ src/draw/layout/singleseq/strict/StemLayout.test.js
@@ -43,30 +43,85 @@ it('FlatoutermostLoop traverseUnpairedRegion53 - size of one', () => {
   let partners = [null, 4, null, 2, null];
   let gps = new StrictLayoutGeneralProps();
   gps.flatOutermostLoop = true;
-  gps.basePairBondLength = 1.78;
   let bps = defaultBaseProps(partners.length);
-  bps[0].stretch3 = 2.2;
-  bps[0].flatOutermostLoopAngle3 = Math.PI / 7;
+  bps[3].stretch3 = 2.2;
+  bps[3].flatOutermostLoopAngle3 = Math.PI / 7;
 
   let outermostStem = new Stem(0, partners, gps, bps);
   let it = outermostStem.loopIterator();
+  it.next();
+  let st5 = it.next().value;
   let ur = it.next().value;
-  let st3 = it.next().value;
+
+  st5.angle = Math.PI / 3;
+  st5.xBottomCenter = 1.2;
+  st5.yBottomCenter = 3.77;
   
-  st3.angle = Math.PI / 6;
-  st3.xBottomCenter = 1.2;
-  st3.yBottomCenter = 2.3;
-
-  console.log(st3.baseCoordinates5());
-
-  /*
   checkCoords(
-    FlatOutermostLoop.traverseUnpairedRegion35(ur, bps),
+    FlatOutermostLoop.traverseUnpairedRegion53(ur, bps),
     [
-      [3.428012, -1.9250572921102043],
+      [-3.1936791509797766, 4.492148299476559],
     ],
   );
-  */
 });
 
-it('FlatOutermostLoop traverseUnpairedRegion53 - size of four', () => {});
+it('FlatOutermostLoop traverseUnpairedRegion53 - size of four', () => {
+  let partners = [null, 4, null, 2, null, null, null, null];
+  let gps = new StrictLayoutGeneralProps();
+  gps.flatOutermostLoop = true;
+  let bps = defaultBaseProps(partners.length);
+  bps[3].stretch3 = 2;
+  bps[3].flatOutermostLoopAngle3 = Math.PI / 8;
+  bps[4].stretch3 = -5;
+  bps[4].flatOutermostLoopAngle3 = 0;
+  bps[5].stretch3 = 5;
+  bps[5].flatOutermostLoopAngle3 = -Math.PI / 8;
+
+  let outermostStem = new Stem(0, partners, gps, bps);
+  let it = outermostStem.loopIterator();
+  it.next();
+  let st5 = it.next().value;
+  let ur = it.next().value;
+
+  st5.angle = -Math.PI / 3;
+  st5.xBottomCenter = 1;
+  st5.yBottomCenter = 1.4;
+
+  checkCoords(
+    FlatOutermostLoop.traverseUnpairedRegion53(ur, bps),
+    [
+      [3.528912231189045, 3.3970473189814863],
+      [4.137673660197765, 4.190400659272721],
+      [9.333826082904398, 7.190400659272721],
+      [10.199851486688837, 7.690400659272721],
+    ],
+  );
+});
+
+it("FlatOutermost traverseUnpairedRegion53 - 5' dangling unpaired region", () => {
+  let partners = [null, null, null, 6, null, 4, null];
+  let gps = new StrictLayoutGeneralProps();
+  gps.flatOutermostLoop = true;
+  let bps = defaultBaseProps(partners.length);
+  bps[0].stretch3 = 2;
+  bps[0].flatOutermostLoopAngle3 = Math.PI / 3;
+  bps[1].stretch3 = 10;
+  bps[1].flatOutermostLoopAngle3 = -Math.PI / 2;
+  
+  let outermostStem = new Stem(0, partners, gps, bps);
+  let it = outermostStem.loopIterator();
+  let ur = it.next().value;
+  
+  outermostStem.angle = (Math.PI / 6) - (Math.PI / 2);
+  outermostStem.xBottomCenter = -1;
+  outermostStem.yBottomCenter = -5;
+
+  checkCoords(
+    FlatOutermostLoop.traverseUnpairedRegion53(ur, bps),
+    [
+      [-1.6830127018922192, -6.18301270189222],
+      [-1.6830127018922183, -3.1830127018922196],
+      [9.316987298107781, -3.1830127018922223],
+    ],
+  );
+});
