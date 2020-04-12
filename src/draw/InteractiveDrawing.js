d3cc742d826cbf8ad9c288fd43d7987b03e39796
Philip Johnson <p.zhao.johnson@gmail.com>
2020-04-12 13:18:07 -0400

    Started adding functionality for showing a static drawing

--- src/draw/InteractiveDrawing.js
+++ src/draw/InteractiveDrawing.js
@@ -1,4 +1,6 @@
 import Drawing from './Drawing';
+import StrictLayoutGeneralProps from './layout/singleseq/strict/StrictLayoutGeneralProps';
+import StrictLayoutBaseProps from './layout/singleseq/strict/StrictLayoutBaseProps';
 
 class InteractiveDrawing {
 
@@ -19,10 +21,61 @@ class InteractiveDrawing {
   }
 
   /**
+   * @callback InteractiveDrawing~SVG 
+   * 
+   * @returns {SVG.Svg} 
+   */
+
+  /**
+   * @param {Node} container 
+   * @param {InteractiveDrawing~SVG} SVG 
+   */
+  addTo(container, SVG) {
+    this._drawing.addTo(container, SVG);
+  }
+
+  /**
    * @returns {boolean} True if this drawing is empty.
    */
   isEmpty() {
     return this._drawing.isEmpty();
+  }
+
+  /**
+   * Returns null if no sequence is added.
+   * 
+   * @param {string} id 
+   * @param {string} letters 
+   * 
+   * @returns {Sequence|null} 
+   */
+  _appendSequenceOutOfView(id, letters) {
+    let seq = this._drawing.appendSequenceOutOfView(id, letters);
+    if (seq) {
+      seq.forEachBase(b => {
+        this._strictLayoutProps.base.push(new StrictLayoutBaseProps());
+      });
+    }
+    return seq;
+  }
+
+  /**
+   * @param {string} id 
+   * @param {string} letters 
+   * @param {Array<number|null>} secondaryPartners 
+   * @param {Array<number|null>} tertiaryPartners 
+   */
+  appendStructure(id, letters, secondaryPartners, tertiaryPartners) {
+    let strictLayoutPartners = this._drawing.strictLayoutPartners();
+    let seq = this._appendSequenceOutOfView(id, letters);
+    if (!seq) {
+      return;
+    }
+    this._drawing.addStrandBondsForSequence(seq);
+    this._drawing.applyStrictLayoutPartners(
+      strictLayoutPartners.concat(secondaryPartners),
+    );
+    this._drawing.addTertiaryPairsForSequence(seq, tertiaryPartners);
   }
 }
 
