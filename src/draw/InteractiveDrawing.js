5063067188be52a6bd1a5f0214ddbac2b528a015
Philip Johnson <p.zhao.johnson@gmail.com>
2020-04-12 12:35:40 -0400

    Removed cruft from InteractiveDrawing class

--- src/draw/InteractiveDrawing.js
+++ src/draw/InteractiveDrawing.js
@@ -1,11 +1,5 @@
 import Drawing from './Drawing';
 
-const _MODES = {
-  FOLDING: 'FOLDING',
-  PIVOTING: 'PIVOTING',
-  SELECTING_BASES: 'SELECTING_BASES',
-};
-
 class InteractiveDrawing {
 
   /**
@@ -22,7 +16,6 @@ class InteractiveDrawing {
     };
 
     this._layoutType = 'strict';
-    this._mode = _MODES.FOLDING;
   }
 
   /**
@@ -31,70 +24,6 @@ class InteractiveDrawing {
   isEmpty() {
     return this._drawing.isEmpty();
   }
-
-  addStructure(id, sequence, partners) {
-    this._drawing.addStructure(id, sequence, partners);
-  }
-
-  applyStrictLayout() {
-    this._drawing.applyStrictLayout();
-  }
-
-  handleMousemove(e) {
-    if (this._mode === _MODES.PIVOTING) {
-      this._handleMousemovePivoting(e);
-    } else if (this._mode === _MODES.SELECTING_BASES) {
-      this._handleMousemoveSelectingBases(e);
-    }
-  }
-
-  handleMouseup(e) {
-    if (this._mode === _MODES.PIVOTING) {
-      this._handleMouseupPivoting(e);
-    } else if (this._mode === _MODES.SELECTING_BASES) {
-      this._handleMouseupSelectingBases(e);
-    }
-  }
-
-  /**
-   * @param {Base} b 
-   */
-  handleMouseoverBase(b) {
-    if (this._mode === _MODES.FOLDING) {
-      this._handleMouseoverBaseFolding(b);
-    }
-  }
-
-  _handleMouseoverBaseFolding(b) {
-    let p = this._drawing.strictLayoutPositionOfBase(b);
-    
-    let startingState = {
-      hovered: p,
-      firstSelected: this._firstSelected,
-      lastSelected: this._lastSelected,
-      partners: this._drawing.strictLayoutPartners(),
-      baseProps: this._strictLayoutProps.base,
-    };
-
-    let endingState = FoldingMode.handleMouseover(startingState);
-
-    if (partnersDifferent || basePropsDifferent) {
-      this.pushUndo();
-    }
-
-    let layout = new StrictLayout(endingState.partners, generalProps, baseProps);
-    this._drawing.applyStrictLayout(layout);
-    this.baseProps = baseProps;
-    this.applyHighlighting();
-  }
-
-  handleMouseoutBase(e) {}
-
-  handleMousedownBase(e) {}
-
-  handleDblclickBase(e) {}
-
-  handleDblclickTertiaryBond(e) {}
 }
 
 export default InteractiveDrawing;
