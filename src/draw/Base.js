c2c433ec14c8546be2986530bdcbdac22109fff6
Philip Johnson <p.zhao.johnson@gmail.com>
2020-04-23 10:05:41 -0400

    Removed extra blank lines

--- src/draw/Base.js
+++ src/draw/Base.js
@@ -52,22 +52,17 @@ class Base {
     if (savedState.className !== 'Base') {
       throw new Error('Saved state is not for a base.');
     }
-
     let text = svg.findOne('#' + savedState.text);
     let b = new Base(text);
-
     if (savedState.highlighting) {
       b.addCircleHighlightingFromSavedState(savedState.highlighting, svg, clockwiseNormalAngle);
     }
-
     if (savedState.outline) {
       b.addCircleOutlineFromSavedState(savedState.outline, svg, clockwiseNormalAngle);
     }
-
     if (savedState.numbering) {
       b.addNumberingFromSavedState(savedState.numbering, svg);
     }
-
     Base._copyPropsToMostRecent(b);
     return b;
   }
@@ -105,14 +100,12 @@ class Base {
   static create(svg, character, xCenter, yCenter) {
     let text = svg.text((add) => add.tspan(character));
     text.id();
-
     text.attr({
       'x': xCenter,
       'y': yCenter,
       'text-anchor': 'middle',
       'dominant-baseline': 'middle',
     });
-
     let b = new Base(text);
     Base._applyMostRecentProps(b);
     return b;
@@ -145,19 +138,15 @@ class Base {
    * @throws {Error} If the dy property is not 0.4em.
    */
   _validateText() {
-
     if (typeof(this._text.id()) !== 'string' || this._text.id().length === 0) {
       throw new Error('Invalid ID.');
     }
-
     if (this._text.text().length !== 1) {
       throw new Error('The text content must be a single character.');
     }
-
     if (this._text.attr('text-anchor') !== 'middle') {
       throw new Error('The text-anchor property must be middle.');
     }
-
     if (this._text.attr('dominant-baseline') !== 'middle') {
       throw new Error('dominant-baseline must be middle.');
     }
@@ -186,7 +175,6 @@ class Base {
     if (c.length !== 1) {
       throw new Error('The given string has multiple characters.');
     }
-
     this._text.clear();
     this._text.tspan(c);
   }
@@ -216,15 +204,12 @@ class Base {
       'x': xCenter,
       'y': yCenter
     });
-
     if (this._highlighting !== null) {
       this._highlighting.reposition(xCenter, yCenter, clockwiseNormalAngle);
     }
-
     if (this._outline !== null) {
       this._outline.reposition(xCenter, yCenter, clockwiseNormalAngle);
     }
-
     if (this._numbering !== null) {
       this._numbering.reposition(xCenter, yCenter, outerNormalAngle);
     }
@@ -381,11 +366,9 @@ class Base {
    */
   addCircleHighlighting(svg) {
     this.removeHighlighting();
-
     this._highlighting = CircleBaseAnnotation.createNondisplaced(
       svg, this.xCenter, this.yCenter
     );
-
     return this._highlighting;
   }
 
@@ -398,7 +381,6 @@ class Base {
    */
   addCircleHighlightingFromSavedState(savedState, svg, clockwiseNormalAngle) {
     this.removeHighlighting();
-
     this._highlighting = CircleBaseAnnotation.fromSavedState(
       savedState,
       svg,
@@ -406,7 +388,6 @@ class Base {
       this.yCenter,
       clockwiseNormalAngle,
     );
-
     return this._highlighting;
   }
 
@@ -441,11 +422,9 @@ class Base {
    */
   addCircleOutline(svg) {
     this.removeOutline();
-
     this._outline = CircleBaseAnnotation.createNondisplaced(
       svg, this.xCenter, this.yCenter
     );
-
     return this._outline;
   }
 
@@ -458,7 +437,6 @@ class Base {
    */
   addCircleOutlineFromSavedState(savedState, svg, clockwiseNormalAngle) {
     this.removeOutline();
-
     this._outline = CircleBaseAnnotation.fromSavedState(
       savedState,
       svg,
@@ -466,7 +444,6 @@ class Base {
       this.yCenter,
       clockwiseNormalAngle,
     );
-
     return this._outline;
   }
 
@@ -503,11 +480,9 @@ class Base {
    */
   addNumbering(svg, number, outerNormalAngle) {
     this.removeNumbering();
-
     this._numbering = Numbering.create(
       svg, number, this.xCenter, this.yCenter, outerNormalAngle
     );
-
     return this._numbering;
   }
 
@@ -519,14 +494,12 @@ class Base {
    */
   addNumberingFromSavedState(savedState, svg) {
     this.removeNumbering();
-
     this._numbering = Numbering.fromSavedState(
       savedState,
       svg,
       this.xCenter,
       this.yCenter,
     );
-
     return this._numbering;
   }
 
@@ -558,7 +531,6 @@ class Base {
     this.removeHighlighting();
     this.removeOutline();
     this.removeNumbering();
-    
     this._text.remove();
   }
 
@@ -579,19 +551,15 @@ class Base {
       className: 'Base',
       text: this._text.id(),
     };
-
     if (this.hasHighlighting()) {
       savableState.highlighting = this._highlighting.savableState();
     }
-
     if (this.hasOutline()) {
       savableState.outline = this._outline.savableState();
     }
-
     if (this.hasNumbering()) {
       savableState.numbering = this._numbering.savableState();
     }
-
     return savableState;
   }
 }
