8edbc5d036c5ccb1c90a833250d09f06e235dbaa
Philip Johnson <p.zhao.johnson@gmail.com>
2020-05-02 13:51:08 -0400

    Added tests for InteractiveDrawing class

--- src/draw/InteractiveDrawing.test.js
+++ src/draw/InteractiveDrawing.test.js
@@ -2,6 +2,39 @@ import InteractiveDrawing from './InteractiveDrawing';
 import createNodeSVG from './createNodeSVG';
 
 describe('InteractiveDrawing class', () => {
+  describe('constructor', () => {
+    it('initializes drawing', () => {
+      let idrawing = new InteractiveDrawing();
+      expect(idrawing._drawing).toBeTruthy();
+    });
+
+    it('initializes strict layout properties', () => {
+      let idrawing = new InteractiveDrawing();
+      expect(idrawing._strictLayoutProps.general).toBeTruthy();
+      expect(idrawing._strictLayoutProps.base).toBeTruthy();
+      expect(idrawing._strictLayoutProps.baseWidth).toBeTruthy();
+      expect(idrawing._strictLayoutProps.baseHeight).toBeTruthy();
+    });
+
+    it('initializes layout type', () => {
+      let idrawing = new InteractiveDrawing();
+      expect(idrawing.layoutType).toBeTruthy();
+    });
+  });
+
+  it('addTo method', () => {
+    let idrawing = new InteractiveDrawing();
+    let n = document.body.childNodes.length;
+    idrawing.addTo(document.body, () => createNodeSVG());
+    expect(document.body.childNodes.length).toBe(n + 1);
+  });
+
+  it('layoutType getter', () => {
+    let idrawing = new InteractiveDrawing();
+    idrawing._layoutType = 'freeform';
+    expect(idrawing.layoutType).toBe('freeform');
+  });
+
   describe('_overallSecondaryPartners method', () => {
     it('multiple sequences', () => {
       let id = new InteractiveDrawing();
