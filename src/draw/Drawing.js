ddba15360d5c140285f75371a56e250bafb1840f
Philip Johnson <p.zhao.johnson@gmail.com>
2020-05-02 12:24:57 -0400

    Tidied up adjustNumberingLineAngles method and lowered thresholds

--- src/draw/Drawing.js
+++ src/draw/Drawing.js
@@ -417,16 +417,17 @@ class Drawing {
     this.forEachSecondaryBond(sb => {
       let b1 = sb.base1;
       let b2 = sb.base2;
-      let ba = b1.angleBetweenCenters(b2);
+      let ba12 = b1.angleBetweenCenters(b2);
+      let ba21 = b2.angleBetweenCenters(b1);
       if (b1.hasNumbering()) {
-        let la = normalizeAngle(b1.numbering.lineAngle, ba) - ba;
-        if (la < Math.PI / 2 || la > 3 * Math.PI / 2) {
+        let la = normalizeAngle(b1.numbering.lineAngle, ba12) - ba12;
+        if (la < Math.PI / 4 || la > 7 * Math.PI / 4) {
           b1.numbering.lineAngle = b1.numbering.lineAngle + Math.PI;
         }
       }
       if (b2.hasNumbering()) {
-        let la = normalizeAngle(b2.numbering.lineAngle + Math.PI, ba) - ba;
-        if (la < Math.PI / 2 || la > 3 * Math.PI / 2) {
+        let la = normalizeAngle(b2.numbering.lineAngle, ba21) - ba21;
+        if (la < Math.PI / 4 || la > 7 * Math.PI / 4) {
           b2.numbering.lineAngle = b2.numbering.lineAngle + Math.PI;
         }
       }
