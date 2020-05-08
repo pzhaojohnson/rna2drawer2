e4586cd8eabb5c3afa6d4be9a6bfd55bdaa06895
Philip Johnson <p.zhao.johnson@gmail.com>
2020-05-08 18:44:42 -0400

    Tidied up ExportPptx component

--- src/forms/ExportPptx.js
+++ src/forms/ExportPptx.js
@@ -24,6 +24,7 @@ class ExportPptx extends React.Component {
       <div
         style={{
           position: 'relative',
+          width: '400px',
           height: '100%',
           backgroundColor: '#fefefe',
           borderWidth: '0px 0px 0px thin',
@@ -31,147 +32,221 @@ class ExportPptx extends React.Component {
           borderColor: '#bfbfbf',
         }}
       >
-        <CloseButton
-          position={'absolute'}
-          top={'0px'}
-          right={'0px'}
-          onClick={() => this.close()}
-        />
-        <div
-          style={{
-            width: '400px',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          {this._title()}
-          {this._baseFontSizeSection()}
-          {this._errorMessageSection()}
-          {this._exportButton()}
-        </div>
+        {this.closeButton()}
+        {this.titleAndContent()}
       </div>
     );
   }
 
-  _title() {
+  closeButton() {
     return (
-      <div>
-        <p
-          className={'unselectable-text'}
-          style={{
-            margin: '16px 32px 0px 32px',
-            fontSize: '24px',
-          }}
-        >
-          Export PPTX
-        </p>
-        <div
-          style={{
-            height: '0px',
-            borderWidth: '0px 0px thin 0px',
-            borderStyle: 'solid',
-            borderColor: '#bfbfbf',
-            margin: '8px 16px 0px 16px',
-          }}
-        ></div>
-      </div>
+      <CloseButton
+        position={'absolute'}
+        top={'0px'}
+        right={'0px'}
+        onClick={() => this.close()}
+      />
     );
   }
 
-  _baseFontSizeSection() {
+  titleAndContent() {
     return (
       <div
-        style={{ margin: '24px 40px 18px 40px' }}
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
       >
-        <p
-          className={'unselectable-text'}
-          style={{ fontSize: '12px' }}
-        >
-          Scale the exported drawing by setting the font size of bases.
-        </p>
-        <div
-          style={{
-            margin: '8px 0px 0px 8px',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}
-        >
-          <p
-            className={'unselectable-text'}
-            style={{
-              fontSize: '12px',
-              display: 'inline-block',
-              marginRight: '8px',
-            }}
-          >
-            Font size of bases:
-          </p>
-          <input
-            type={'text'}
-            value={this.state.baseFontSize}
-            onChange={event => this._onBaseFontSizeInputChange(event)}
-            spellCheck={'false'}
-            style={{
-              flexGrow: '1',
-              fontSize: '12px',
-              textAlign: 'right',
-            }}
-          />
-        </div>
+        {this.titleSection()}
+        {this.baseFontSizeSection()}
+        {this.errorMessageSection()}
+        {this.exportSection()}
       </div>
+    );
+  }
+
+  titleSection() {
+    return (
+      <div>
+        {this.titleText()}
+        {this.titleUnderline()}
+      </div>
+    );
+  }
+
+  titleText() {
+    return (
+      <p
+        className={'unselectable-text'}
+        style={{
+          margin: '16px 32px 0px 32px',
+          fontSize: '24px',
+        }}
+      >
+        Export PPTX
+      </p>
+    );
+  }
+
+  titleUnderline() {
+    return (
+      <div
+        style={{
+          height: '0px',
+          borderWidth: '0px 0px thin 0px',
+          borderStyle: 'solid',
+          borderColor: '#bfbfbf',
+          margin: '8px 16px 0px 16px',
+        }}
+      ></div>
+    );
+  }
+
+  baseFontSizeSection() {
+    return (
+      <div
+        style={{
+          margin: '24px 40px 18px 40px',
+        }}
+      >
+        {this.baseFontSizeDescription()}
+        {this.baseFontSizeField()}
+      </div>
+    );
+  }
+
+  baseFontSizeDescription() {
+    return (
+      <p
+        className={'unselectable-text'}
+        style={{
+          fontSize: '12px',
+        }}
+      >
+        Scale the exported drawing by setting the font size of bases.
+      </p>
+    );
+  }
+
+  baseFontSizeField() {
+    return (
+      <div
+        style={{
+          margin: '8px 0px 0px 8px',
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+        {this.baseFontSizeLabel()}
+        {this.baseFontSizeInput()}
+      </div>
+    );
+  }
+
+  baseFontSizeLabel() {
+    return (
+      <p
+        className={'unselectable-text'}
+        style={{
+          fontSize: '12px',
+          display: 'inline-block',
+          marginRight: '8px',
+        }}
+      >
+        Font size of bases:
+      </p>
+    );
+  }
+
+  baseFontSizeInput() {
+    return (
+      <input
+        type={'text'}
+        value={this.state.baseFontSize}
+        onChange={event => this.onBaseFontSizeInputChange(event)}
+        spellCheck={'false'}
+        style={{
+          flexGrow: '1',
+          fontSize: '12px',
+          textAlign: 'right',
+        }}
+      />
     );
   }
   
-  _onBaseFontSizeInputChange(event) {
+  onBaseFontSizeInputChange(event) {
     this.setState({
       baseFontSize: event.target.value,
     });
   }
 
-  _errorMessageSection() {
+  errorMessageSection() {
     if (!this.state.errorMessage) {
-      return (
-        <div key={this.state.errorMessageKey} id={this.state.errorMessageKey}></div>
-      );
+      return this.emptyErrorMessageSection();
     }
     return (
-      <div key={this.state.errorMessageKey} id={this.state.errorMessageKey} >
-        <p
-          className={'unselectable-text'}
-          style={{
-            margin: '0px 40px 0px 40px',
-            fontSize: '14px',
-            color: 'red',
-            animationName: 'fadein',
-            animationDuration: '0.75s',
-          }}
-        >
-          <b>{this.state.errorMessage}</b>
-        </p>
+      <div
+        key={this.state.errorMessageKey}
+        id={this.state.errorMessageKey}
+      >
+        {this.errorMessageText()}
       </div>
     );
   }
 
-  _exportButton() {
+  emptyErrorMessageSection() {
+    return (
+      <div
+        key={this.state.errorMessageKey}
+        id={this.state.errorMessageKey}
+      ></div>
+    );
+  }
+
+  errorMessageText() {
+    return (
+      <p
+        className={'unselectable-text'}
+        style={{
+          margin: '0px 40px 0px 40px',
+          fontSize: '14px',
+          color: 'red',
+          animationName: 'fadein',
+          animationDuration: '0.75s',
+        }}
+      >
+        <b>{this.state.errorMessage}</b>
+      </p>
+    );
+  }
+
+  exportSection() {
     return (
       <div
         style={{
           margin: '6px 40px 0px 40px',
         }}
       >
-        <button
-          onClick={() => this.export()}
-          style={{
-            padding: '4px 32px 4px 32px',
-            fontSize: '12px',
-            borderRadius: '2px',
-          }}
-        >
-          Export
-        </button>
+        {this.exportButton()}
       </div>
+    );
+  }
+
+  exportButton() {
+    return (
+      <button
+        onClick={() => this.export()}
+        style={{
+          padding: '4px 32px 4px 32px',
+          fontSize: '12px',
+          borderRadius: '2px',
+        }}
+      >
+        Export
+      </button>
     );
   }
 
@@ -180,31 +255,52 @@ class ExportPptx extends React.Component {
     if (!bfs) {
       return;
     }
-    this.setState({ errorMessage: '', errorMessageKey: uuidv1() });
+    this.setState({
+      errorMessage: '',
+      errorMessageKey: uuidv1(),
+    });
     let scaling = bfs / Base.mostRecentProps().fontSize;
     let pres = this.createPptx(scaling);
-    pres.writeFile('Drawing.pptx');
+    if (pres) {
+      pres.writeFile('Drawing.pptx');
+    }
   }
 
   parseBaseFrontSize() {
     let bfs = Number(this.state.baseFontSize);
     if (!Number.isFinite(bfs)) {
-      this.setState({ errorMessage: 'Font size of bases must be a number.', errorMessageKey: uuidv1() });
+      this.setState({
+        errorMessage: 'Font size of bases must be a number.',
+        errorMessageKey: uuidv1(),
+      });
       return null;
     }
-    if (bfs <= 1) {
-      this.setState({ errorMessage: 'Font size of bases must be at least 1.0.', errorMessageKey: uuidv1() });
+    if (bfs < 1) {
+      this.setState({
+        errorMessage: 'Font size of bases must be at least 1.',
+        errorMessageKey: uuidv1(),
+      });
       return null;
     }
     return pointsToPixels(bfs);
   }
 
   /**
+   * Returns null if either of the SVG or getSvgString callback
+   * props are missing.
+   * 
    * @param {number} scaling 
    * 
-   * @returns {PptxGenJs.PptxGenJs} 
+   * @returns {PptxGenJs.PptxGenJs|null} 
    */
   createPptx(scaling) {
+    if (!this.props.SVG) {
+      console.error('Missing SVG callback.');
+      return null;
+    } else if (!this.props.getSvgString) {
+      console.error('Missing getSvgString callback.');
+      return null;
+    }
     let div = document.createElement('div');
     div.style.cssText = 'max-width: 0px; max-height: 0px';
     document.body.appendChild(div);
@@ -222,20 +318,18 @@ class ExportPptx extends React.Component {
   }
 
   close() {
-    if (this.props.close) {
-      this.props.close();
+    if (!this.props.close) {
+      console.error('Missing close callback.');
+      return;
     }
+    this.props.close();
   }
 }
 
 ExportPptx.propTypes = {
-  SVG: PropTypes.func,
-  getSvgString: PropTypes.func,
-  close: PropTypes.func,
-};
-
-ExportPptx.defaultProps = {
-  close: () => {},
+  SVG: PropTypes.func.isRequired,
+  getSvgString: PropTypes.func.isRequired,
+  close: PropTypes.func.isRequired,
 };
 
 export {
