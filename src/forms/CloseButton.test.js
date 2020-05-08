f07f857f35bc6380068d1341297199f0f0dd9892
Philip Johnson <p.zhao.johnson@gmail.com>
2020-05-07 20:57:43 -0400

    Added test suite for CloseButton component

---
+++ src/forms/CloseButton.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import { CloseButton } from './CloseButton';
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+function getCloseButton() {
+  return container.childNodes[0];
+}
+
+it('calls onClick callback', () => {
+  let onClick = jest.fn();
+  act(() => {
+    render(<CloseButton onClick={onClick} />, container);
+    let cb = getCloseButton();
+    cb.dispatchEvent(
+      new Event('click', { bubbles: true })
+    );
+  });
+  expect(onClick.mock.calls.length).toBe(1);
+});
