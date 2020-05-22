a8eb078aa04e107d87821a0e71d0caf2cbf3f51d
Philip Johnson <p.zhao.johnson@gmail.com>
2020-05-22 18:19:13 -0400

    Converted index.js to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import App from './App';
 const { SVG } = require('@svgdotjs/svg.js');
 
-let _app = new App(() => SVG());
+new App(() => SVG());
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
