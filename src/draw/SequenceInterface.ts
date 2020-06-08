1b8d0c48d2a89fe6693facf56683459c01fc1b67
Philip Johnson <p.zhao.johnson@gmail.com>
2020-06-08 14:51:18 -0400

    Filled out Sequence interface

--- src/draw/SequenceInterface.ts
+++ src/draw/SequenceInterface.ts
@@ -1,4 +1,7 @@
-import { BaseSavableState } from './BaseInterface';
+import {
+  BaseInterface as Base,
+  BaseSavableState,
+} from './BaseInterface';
 
 export interface SequenceMostRecentProps {
   numberingAnchor: number;
@@ -14,6 +17,41 @@ export interface SequenceSavableState {
   numberingIncrement: number;
 }
 
-export interface SequenceInterface {}
+export interface SequenceInterface {
+  id: string;
+  characters: string;
+  numberingOffset: number;
+  numberingAnchor: number;
+  numberingIncrement: number;
+  length: number;
+  offsetPosition(p: number): number;
+  reversePositionOffset(op: number): number;
+  positionOutOfRange(p: number): boolean;
+  positionInRange(p: number): boolean;
+  offsetPositionOutOfRange(p: number): boolean;
+  offsetPositionInRange(p: number): boolean;
+  getBaseAtPosition(p: number): Base | undefined;
+  getBaseAtOffsetPosition(op: number): Base | undefined;
+  getBaseById(id: string): Base | undefined;
+  getBasesInRange(p5: number, p3: number): Base[];
+  forEachBase(f: (b: Base, p: number) => void): void;
+  baseIds(): string[];
+  positionOfBase(b: Base): number;
+  offsetPositionOfBase(b: Base): number;
+  contains(b: Base): boolean;
+  clockwiseNormalAngleAtPosition(p: number): number;
+  counterClockwiseNormalAngleAtPosition(p: number): number;
+  innerNormalAngleAtPosition(p: number): number;
+  outerNormalAngleAtPosition(p: number): number;
+  appendBase(b: Base): void;
+  appendBases(bs: Base[]): void;
+  insertBaseAtPosition(b: Base, p: number): void;
+  onAddBase(f: (b: Base) => void): void;
+  fireAddBase(b: Base): void;
+  removeBaseAtPosition(p: number): void;
+  remove(): void;
+  savableState(): SequenceSavableState;
+  refreshIds(): void;
+}
 
 export default SequenceInterface;
