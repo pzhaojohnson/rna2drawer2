eeb76eedaccae9f6a51d106b6801c1cbf65122d3
unknown <p.zhao.johnson@gmail.com>
2020-07-04 13:28:19 -0400

    Add FlatOutermostLoopField component

---
+++ src/forms/edit/layout/FlatOutermostLoopField.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import CheckboxField from '../../fields/CheckboxField';
+import App from '../../../App';
+
+interface Props {
+  hasFlatOutermostLoop: boolean;
+  set: (v: boolean) => void;
+}
+
+export class FlatOutermostLoopField extends React.Component {
+  props!: Props;
+
+  static create(app: App): React.ReactElement {
+    return (
+      <FlatOutermostLoopField
+        hasFlatOutermostLoop={app.strictDrawing.hasFlatOutermostLoop()}
+        set={(v: boolean) => {
+          if (v != app.strictDrawing.hasFlatOutermostLoop()) {
+            app.pushUndo();
+            if (v) {
+              app.strictDrawing.flatOutermostLoop();
+            } else {
+              app.strictDrawing.roundOutermostLoop();
+            }
+            app.drawingChangedNotByInteraction();
+          }
+        }}
+      />
+    );
+  }
+
+  render(): React.ReactElement {
+    return (
+      <CheckboxField
+        name={'Flat Outermost Loop'}
+        initialValue={this.props.hasFlatOutermostLoop}
+        set={(v: boolean) => this.props.set(v)}
+      />
+    );
+  }
+}
+
+export default FlatOutermostLoopField;
