333b2b545f7ad6711e12aea34e610091bc89d93e
unknown <p.zhao.johnson@gmail.com>
2020-07-04 12:38:43 -0400

    Remove closeToEachOther function

--- src/draw/closeToEachOther.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Returns true if the absolute difference between the two numbers
- * is less than or equal to the maximum allowed difference.
- */
-function closeToEachOther(x: number, y: number, maxDiff: number): boolean {
-  return Math.abs(x - y) <= maxDiff;
-}
-
-export default closeToEachOther;
