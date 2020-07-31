95815fe46bbcbd4010b289f0e08b626c68df91b2
unknown <p.zhao.johnson@gmail.com>
2020-07-31 16:04:47 -0400

    Add test suite for hoveredComplement function

---
+++ src/draw/interact/fold/hoveredComplement.test.js
@@ -0,0 +1,48 @@
+import hoveredComplement from './hoveredComplement';
+import StrictDrawing from '../../StrictDrawing';
+import NodeSVG from '../../NodeSVG';
+import FoldingMode from './FoldingMode';
+import { selectedCharacters } from './selected';
+import charactersInRange from './charactersInRange';
+import IntegerRange from './IntegerRange';
+
+let sd = new StrictDrawing();
+sd.addTo(document.body, () => NodeSVG());
+let mode = new FoldingMode(sd);
+sd.appendSequence('asdf', 'ggggaaaacccc');
+sd.appendSequence('qwer', 'uuuuggggaaaa');
+sd.appendSequence('zxcv', 'uugg');
+
+it('nothing hovered', () => {
+  mode.hovered = undefined;
+  mode.selected = { tightEnd: 8, looseEnd: 12 };
+  expect(hoveredComplement(mode)).toBeFalsy();
+});
+
+it('nothing selected', () => {
+  mode.hovered = 10;
+  mode.selected = undefined;
+  expect(hoveredComplement(mode)).toBeFalsy();
+});
+
+it('no hovered complements', () => {
+  mode.hovered = 6;
+  mode.selected = { tightEnd: 9, looseEnd: 12 };
+  expect(hoveredComplement(mode)).toBeFalsy();
+});
+
+it("returns the 3' most hovered complement", () => {
+  mode.hovered = 4;
+  mode.selected = { tightEnd: 13, looseEnd: 16 };
+  let c = hoveredComplement(mode);
+  expect(c.start).toBe(4);
+  expect(c.end).toBe(7);
+});
+
+it('does not return a complement that overlaps with selected', () => {
+  mode.hovered = 25;
+  mode.selected = { tightEnd: 26, looseEnd: 28 };
+  expect(selectedCharacters(mode)).toBe('ugg');
+  expect(charactersInRange(mode, new IntegerRange(25, 27))).toBe('uug');
+  expect(hoveredComplement(mode)).toBeFalsy();
+});
