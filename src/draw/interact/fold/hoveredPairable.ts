a0c96e1e3a273eedb2e1c307312b9939433d3e48
unknown <p.zhao.johnson@gmail.com>
2020-08-02 16:00:37 -0400

    hoveredPairable function now uses allPairables function

--- src/draw/interact/fold/hoveredPairable.ts
+++ src/draw/interact/fold/hoveredPairable.ts
@@ -1,33 +1,21 @@
 import { FoldingModeInterface as FoldingMode } from './FoldingModeInterface';
-import charactersInRange from './charactersInRange';
-import areComplementary from './areComplementary';
-import { selectedRange } from './selected';
+import allPairables from './allPairables';
 import IntegerRange from './IntegerRange';
 
 /**
- * Returns null if nothing is selected or no complement is hovered.
+ * Returns null if no pairable is hovered.
  */
 export function hoveredPairable(mode: FoldingMode): (IntegerRange | null) {
-  if (!mode.hovered) {
-    return null;
-  }
-  let rSelected = selectedRange(mode);
-  if (!rSelected) {
-    return null;
-  }
-  let csSelected = charactersInRange(mode, rSelected);
-  for (let i = 0; i < rSelected.size; i++) {
-    let p5 = mode.hovered - i;
-    let p3 = p5 + rSelected.size - 1;
-    let r = new IntegerRange(p5, p3);
-    let cs = charactersInRange(mode, r);
-    let complementary = areComplementary(cs, csSelected);
-    let overlapsSelected = r.overlapsWith(rSelected);
-    if (complementary && !overlapsSelected) {
-      return r;
+  let pairables = allPairables(mode);
+  let hovered = null as (IntegerRange | null);
+  pairables.forEach(p => {
+    if (typeof mode.hovered == 'number' && p.contains(mode.hovered)) {
+      if (!hovered || p.start > hovered.start) {
+        hovered = p;
+      }
     }
-  }
-  return null;
+  });
+  return hovered;
 }
 
 export default hoveredPairable;
