22f12d1e4c895097562a8fe28a3d1071ec4ce21f
unknown <p.zhao.johnson@gmail.com>
2020-08-13 09:30:41 -0400

    Change base highlighting styles when annotating bases

--- src/draw/interact/annotate/setAllBaseHighlightings.ts
+++ src/draw/interact/annotate/setAllBaseHighlightings.ts
@@ -4,7 +4,7 @@ import highlightBase from '../highlight/highlightBase';
 export function setAllBaseHighlightings(mode: AnnotatingMode) {
   mode.drawing.forEachBase((b, p) => {
     if (mode.selected.has(p) || p == mode.hovered) {
-      let stroke = p == mode.hovered ? '#000000': '#404040';
+      let stroke = p == mode.hovered ? '#000000': '#a6a6a6';
       if (!b.highlighting || b.highlighting.stroke != stroke) {
         let radius = 1.5 * b.fontSize;
         if (b.outline) {
@@ -15,11 +15,11 @@ export function setAllBaseHighlightings(mode: AnnotatingMode) {
           fillOpacity: 0,
           stroke: stroke,
           strokeWidth: 1.25,
-          strokeOpacity: 0.125,
+          strokeOpacity: 0.25,
         });
         h.pulsateBetween({
           radius: 1.15 * radius,
-          strokeOpacity: 0.0625,
+          strokeOpacity: 0.125,
         }, { duration: 2000 });
       }
     } else {
