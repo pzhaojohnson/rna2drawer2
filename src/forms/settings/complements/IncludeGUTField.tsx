7fec189f6aea577dbb8c6b96b5857b63546a0478
unknown <p.zhao.johnson@gmail.com>
2020-08-15 13:18:29 -0400

    Add field to toggle including GUT pairs

---
+++ src/forms/settings/complements/IncludeGUTField.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import CheckboxField from '../../fields/CheckboxField';
+import App from '../../../App';
+
+export function IncludeGUTField(app: App): React.ReactElement {
+  return (
+    <CheckboxField
+      name={'Include GUT Pairs'}
+      initialValue={app.strictDrawingInteraction.foldingMode.includeGUT}
+      set={v => {
+        app.strictDrawingInteraction.foldingMode.includeGUT = v;
+      }}
+    />
+  );
+}
+
+export default IncludeGUTField;
