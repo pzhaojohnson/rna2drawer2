db4b87fd84793a12107df16eef5034f71fc602af
Philip Johnson <p.zhao.johnson@gmail.com>
2020-12-21 20:16:07 -0500

    Change button text

--- src/menu/mode/TriangularizeButton.tsx
+++ src/menu/mode/TriangularizeButton.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function TriangularizeButton(props: Props): React.ReactElement {
   return (
     <DroppedButton
-      text={'Triangularize Loops'}
+      text={'(Un)Flatten Loops'}
       onClick={() => {
         if (!props.app.strictDrawingInteraction.triangularizing()) {
           props.app.strictDrawingInteraction.startTriangularizing();
