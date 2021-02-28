9eca53b559cf8f2a98f478e1fa24c4afc7935934
unknown <p.zhao.johnson@gmail.com>
2021-02-28 12:50:02 -0500

    Adjust base highlighting styles for annotating mode

--- src/draw/interact/annotate/setAllBaseHighlightings.ts
+++ src/draw/interact/annotate/setAllBaseHighlightings.ts
@@ -5,23 +5,23 @@ export function setAllBaseHighlightings(mode: AnnotatingMode) {
   let bHovered = typeof mode.hovered == 'number' ? mode.drawing.getBaseAtOverallPosition(mode.hovered) : undefined;
   mode.drawing.forEachBase((b, p) => {
     if (mode.selected.has(p) || p == mode.hovered) {
-      let radius = 1.25 * b.fontSize;
+      let radius = 1.15 * b.fontSize;
       if (b.outline) {
         radius = Math.max(radius, 1.25 * b.outline.radius);
       }
-      let stroke = p == mode.hovered ? '#404040': '#808080';
+      let stroke = p == mode.hovered ? '#000000': '#808080';
       if (!b.highlighting || b.highlighting.stroke != stroke) {
         let h = highlightBase(b, {
           radius: radius,
           fill: 'none',
           stroke: stroke,
-          strokeWidth: 1.25,
-          strokeOpacity: 0.75,
+          strokeWidth: 0.75,
+          strokeOpacity: p == mode.hovered ? 0.8 : 0.3,
         });
         h.pulsateBetween({
-          radius: 1.25 * radius,
-          strokeOpacity: 0.1,
-        }, { duration: 1500 });
+          radius: 1.125 * radius,
+          strokeOpacity: p == mode.hovered ? 0.8 : 0.15,
+        }, { duration: p == mode.hovered ? 750 : 1750 });
       }
       // if close to hovered base
       if (b.highlighting && bHovered && b.distanceBetweenCenters(bHovered) < 5 * radius) {
