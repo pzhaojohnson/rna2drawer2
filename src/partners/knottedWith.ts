3c717820415cc568b1693997d05e7b8f512a2598
Philip Johnson <p.zhao.johnson@gmail.com>
2021-05-02 17:08:00 -0400

    Refactor knottedPairs to knotted

--- src/partners/knottedWith.ts
+++ src/partners/knottedWith.ts
@@ -5,16 +5,16 @@ import { areKnotted } from './areKnotted';
 export type Pair = [number, number];
 
 export function knottedWith(partners: Partners, pair: Pair): Pair[] {
-  let knottedPairs: Pair[] = [];
+  let knotted: Pair[] = [];
   let u = Math.min(...pair);
   let d = Math.max(...pair);
   for (let p = u + 1; p < d; p++) {
     let q = partnerOf(partners, p);
     if (typeof q == 'number') {
       if (areKnotted(pair, [p, q])) {
-        knottedPairs.push([p, q]);
+        knotted.push([p, q]);
       }
     }
   }
-  return knottedPairs;
+  return knotted;
 }
