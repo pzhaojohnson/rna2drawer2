8dad9465b3c84899a808d00e0f9d2efa263db22f
Philip Johnson <p.zhao.johnson@gmail.com>
2021-05-12 08:36:08 -0400

    Add Stem and UnpairedRegion type definitions

---
+++ src/partners/Stem.ts
@@ -0,0 +1,9 @@
+// a consecutive stack of pairs
+export type Stem = {
+  // the 5' most position of the stem
+  position5: number;
+  // the 3' most position of the stem
+  position3: number;
+  // the number of pairs in the stem
+  size: number;
+};
