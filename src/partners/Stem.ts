1ed25a57a32fcdd6ba2efb6f5f71fbf3cc4e63aa
Philip Johnson <p.zhao.johnson@gmail.com>
2021-05-12 21:15:53 -0400

    Add contains function for stems

--- src/partners/Stem.ts
+++ src/partners/Stem.ts
@@ -1,4 +1,8 @@
-import { Pair } from './Pair';
+import {
+  Pair,
+  partner5,
+  partner3,
+} from './Pair';
 
 // a consecutive stack of pairs
 export type Stem = {
@@ -19,4 +23,13 @@ export function topPair(st: Stem): Pair {
     st.position5 + st.size - 1,
     st.position3 - st.size + 1,
   ];
+}
+
+export function contains(st: Stem, p: number): boolean {
+  let bpr = bottomPair(st);
+  let tpr = topPair(st);
+  return (
+    (p >= partner5(bpr) && p <= partner5(tpr))
+    || (p <= partner3(bpr) && p >= partner3(tpr))
+  );
 }
