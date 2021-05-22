572e5328201147502d46ed72da3c22a44fc57a54
Philip Johnson <p.zhao.johnson@gmail.com>
2021-05-22 13:18:29 -0400

    Use predefined types and functions for pairs in knottedWith function

--- src/partners/knottedWith.ts
+++ src/partners/knottedWith.ts
@@ -1,16 +1,15 @@
 import { Partners, partnerOf } from './Partners';
+import { Pair, partner5, partner3 } from './Pair';
 import { areKnotted } from './areKnotted';
 
-export type Pair = [number, number];
-
-export function knottedWith(partners: Partners, pair: Pair): Pair[] {
+export function knottedWith(partners: Partners, pr: Pair): Pair[] {
   let knotted: Pair[] = [];
-  let u = Math.min(...pair);
-  let d = Math.max(...pair);
-  for (let p = u + 1; p < d; p++) {
+  let p5 = partner5(pr);
+  let p3 = partner3(pr);
+  for (let p = p5 + 1; p < p3; p++) {
     let q = partnerOf(partners, p);
     if (typeof q == 'number') {
-      if (areKnotted(pair, [p, q])) {
+      if (areKnotted(pr, [p, q])) {
         knotted.push([p, q]);
       }
     }
