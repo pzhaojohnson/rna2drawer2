643cf6c6e1605e4ca12498f8502491fe48c6fae0
Philip Johnson <p.zhao.johnson@gmail.com>
2021-05-23 15:44:04 -0400

    Add pairs function for partners notation

--- src/partners/Partners.test.js
+++ src/partners/Partners.test.js
@@ -1,5 +1,6 @@
 import {
   partnerOf,
+  pairs,
   unstructuredPartners,
 } from './Partners';
 
@@ -20,6 +21,35 @@ describe('partnerOf function', () => {
     expect(partnerOf(partners, 0)).toBe(undefined);
     expect(partnerOf(partners, -1)).toBe(undefined);
     expect(partnerOf(partners, partners.length + 5)).toBe(undefined);
+  });
+});
+
+describe('pairs function', () => {
+  it('empty partners', () => {
+    expect(pairs([])).toStrictEqual([]);
+  });
+
+  it('unstructured partners', () => {
+    let partners = [undefined, null, null, undefined];
+    expect(pairs(partners)).toStrictEqual([]);
+  });
+
+  it('multiple stems', () => {
+    let partners = [12, 11, null, 8,  7, undefined, 5, 4, undefined, null, 2, 1, null];
+    expect(pairs(partners)).toStrictEqual(
+      [[1, 12], [2, 11], [4, 8], [5, 7]]
+    );
+  });
+
+  it('unassigned positions', () => {
+    let partners = [6, 5, null, null, 2, 1];
+    // must iterate over unassigned positions
+    // to find this pair
+    partners[25 - 1] = 32;
+    partners[32 - 1] = 25;
+    expect(pairs(partners)).toStrictEqual(
+      [[1, 6], [2, 5], [25, 32]]
+    );
   });
 });
 
--- src/partners/Partners.ts
+++ src/partners/Partners.ts
@@ -1,7 +1,21 @@
+import { Pair } from './Pair';
+
 export type Partners = (number | null | undefined)[];
 
 export function partnerOf(partners: Partners, p: number): number | null | undefined {
   return partners[p - 1];
+}
+
+// returns all pairs in the partners notation
+export function pairs(partners: Partners): Pair[] {
+  let prs: Pair[] = [];
+  partners.forEach((q, i) => {
+    let p = i + 1;
+    if (typeof q == 'number' && p < q) {
+      prs.push([p, q]);
+    }
+  });
+  return prs;
 }
 
 export function unstructuredPartners(length=0): Partners {
