723a631a174062256f49579b2c7c682c73f3ad64
unknown <p.zhao.johnson@gmail.com>
2021-05-29 17:26:21 -0400

    Use predefined partners notation type definition and functions in stemProps module

--- src/draw/layout/singleseq/strict/stemProps.ts
+++ src/draw/layout/singleseq/strict/stemProps.ts
@@ -1,4 +1,6 @@
 import { PerBaseStrictLayoutProps as PerBaseProps } from './PerBaseStrictLayoutProps';
+import { Partners } from 'Partners/Partners';
+import { pair, unpair } from 'Partners/edit';
 import { containingStem } from 'Partners/containing';
 
 export function resetStemProps(props: PerBaseProps) {
@@ -19,15 +21,14 @@ interface IntegerRange {
   end: number;
 }
 
-export function willPair(partners: (number | null)[], perBaseProps: PerBaseProps[], r1: IntegerRange, r2: IntegerRange) {
+export function willPair(partners: Partners, perBaseProps: PerBaseProps[], r1: IntegerRange, r2: IntegerRange) {
   let [rBefore, rAfter] = r1.start < r2.start ? [r1, r2] : [r2, r1];
   let props5 = PerBaseProps.getOrCreatePropsAtPosition(perBaseProps, rBefore.start);
   resetStemProps(props5);
   let paired = [...partners];
   for (let p = rBefore.start; p <= rBefore.end; p++) {
     let q = rAfter.end - (p - rBefore.start);
-    paired[p - 1] = q;
-    paired[q - 1] = p;
+    pair(paired, p, q);
   }
   let st1 = containingStem(partners, rBefore.end + 1);
   let st2 = containingStem(paired, rBefore.end + 1);
@@ -41,14 +42,10 @@ export function willPair(partners: (number | null)[], perBaseProps: PerBaseProps
   }
 }
 
-export function willUnpair(partners: (number | null)[], perBaseProps: PerBaseProps[], r: IntegerRange) {
+export function willUnpair(partners: Partners, perBaseProps: PerBaseProps[], r: IntegerRange) {
   let unpaired = [...partners];
   for (let p = r.start; p <= r.end; p++) {
-    let q = unpaired[p - 1];
-    if (typeof q == 'number') {
-      unpaired[p - 1] = null;
-      unpaired[q - 1] = null;
-    }
+    unpair(unpaired, p);
   }
   [r.start - 1, r.end + 1].forEach(p => {
     let st1 = containingStem(partners, p);
@@ -68,11 +65,11 @@ export function willUnpair(partners: (number | null)[], perBaseProps: PerBasePro
   });
 }
 
-export function willRemove(partners: (number | null)[], perBaseProps: PerBaseProps[], r: IntegerRange) {
+export function willRemove(partners: Partners, perBaseProps: PerBaseProps[], r: IntegerRange) {
   willUnpair(partners, perBaseProps, r);
 }
 
-export function willInsertAt(partners: (number | null)[], perBaseProps: PerBaseProps[], p: number) {
+export function willInsertAt(partners: Partners, perBaseProps: PerBaseProps[], p: number) {
   let st = containingStem(partners, p);
   if (st && st.position5 < p) {
     let fromProps = PerBaseProps.getOrCreatePropsAtPosition(perBaseProps, st.position5);
