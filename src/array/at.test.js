31b038a3dda63f6a165f96e4af440feae00b3108
unknown <p.zhao.johnson@gmail.com>
2021-06-06 15:09:02 -0400

    Add atPosition function

--- src/array/at.test.js
+++ src/array/at.test.js
@@ -1,14 +1,31 @@
-import { atIndex } from './at';
+import {
+  atIndex,
+  atPosition,
+} from './at';
 
-it('index is in bounds', () => {
-  let arr = [1, 2, 3, 4, 5, 6];
-  expect(atIndex(arr, 0)).toBe(1);
-  expect(atIndex(arr, 3)).toBe(4);
-  expect(atIndex(arr, 5)).toBe(6);
+it('atIndex function', () => {
+  let arr = [2, 'z', null, undefined, 1];
+  arr.length = 10; // has empty items
+  expect(atIndex(arr, 0)).toBe(2);
+  expect(atIndex(arr, 1)).toBe('z');
+  expect(atIndex(arr, 2)).toBe(null);
+  expect(atIndex(arr, 3)).toBe(undefined);
+  expect(atIndex(arr, 4)).toBe(1);
+  expect(atIndex(arr, 5)).toBe(undefined); // empty item
+  expect(atIndex(arr, -1)).toBe(undefined); // negative index
+  expect(atIndex(arr, arr.length + 10)).toBe(undefined); // above bounds
 });
 
-it('index is out of bounds', () => {
-  let arr = [1, 2, 3, 4, 5];
-  expect(atIndex(arr, 5)).toBe(undefined);
-  expect(atIndex(arr, 12)).toBe(undefined);
+it('atPosition function', () => {
+  let arr = ['asdf', 2, undefined, null, 8];
+  arr.length = 20; // has empty items
+  expect(atPosition(arr, 1)).toBe('asdf');
+  expect(atPosition(arr, 2)).toBe(2);
+  expect(atPosition(arr, 3)).toBe(undefined);
+  expect(atPosition(arr, 4)).toBe(null);
+  expect(atPosition(arr, 5)).toBe(8);
+  expect(atPosition(arr, 6)).toBe(undefined); // empty item
+  expect(atPosition(arr, 0)).toBe(undefined); // position zero
+  expect(atPosition(arr, -1)).toBe(undefined); // negative position
+  expect(atPosition(arr, arr.length + 20)).toBe(undefined); // above bounds
 });
--- src/array/at.ts
+++ src/array/at.ts
@@ -1,3 +1,7 @@
 export function atIndex<E>(arr: E[], i: number): E | undefined {
   return arr[i];
 }
+
+export function atPosition<E>(arr: E[], p: number): E | undefined {
+  return arr[p - 1];
+}
