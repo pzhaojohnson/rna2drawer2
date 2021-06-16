94d48bfa6a0c4e816f87f8413f33191b722a9b31
unknown <p.zhao.johnson@gmail.com>
2021-06-16 10:55:39 -0400

    Add function to add saved numbering to a base

--- src/draw/bases/numbering/save.test.js
+++ src/draw/bases/numbering/save.test.js
@@ -1,11 +1,22 @@
-import { savableState } from './save';
+import { savableState, addSavedNumbering } from './save';
 import { NodeSVG } from 'Draw/NodeSVG';
 import Base from 'Draw/Base';
 import { addNumbering } from './add';
 
+function areSameElement(ele1, ele2) {
+  return (
+    ele1.id() // ID is truthy
+    && ele1.id() == ele2.id()
+    && ele1.svg() // SVG string is truthy
+    && ele1.svg() == ele2.svg()
+  );
+}
+
 let container = null;
 let svg = null;
-let base = null;
+
+let base1 = null;
+let base2 = null;
 
 beforeEach(() => {
   container = document.createElement('div');
@@ -14,11 +25,14 @@ beforeEach(() => {
   svg = NodeSVG();
   svg.addTo(container);
 
-  base = Base.create(svg, 'G', 10, 50);
+  // have same character and positioning
+  base1 = Base.create(svg, 'G', 10, 50);
+  base2 = Base.create(svg, 'G', 10, 50);
 });
 
 afterEach(() => {
-  base = null;
+  base1 = null;
+  base2 = null;
 
   svg.clear();
   svg.remove();
@@ -30,12 +44,12 @@ afterEach(() => {
 
 describe('savableState function', () => {
   it('returns correct values', () => {
-    addNumbering(base, 500);
+    addNumbering(base1, 500);
     let textId = Math.random().toString();
     let lineId = Math.random().toString();
-    base.numbering.text.id(textId);
-    base.numbering.line.id(lineId);
-    let state = savableState(base.numbering);
+    base1.numbering.text.id(textId);
+    base1.numbering.line.id(lineId);
+    let state = savableState(base1.numbering);
     expect(state).toEqual({
       className: 'BaseNumbering',
       textId: textId,
@@ -44,10 +58,71 @@ describe('savableState function', () => {
   });
 
   it('returned object can be converted to and from JSON', () => {
-    addNumbering(base, 1012);
-    let state1 = savableState(base.numbering);
+    addNumbering(base1, 1012);
+    let state1 = savableState(base1.numbering);
     let string1 = JSON.stringify(state1);
     let state2 = JSON.parse(string1);
     expect(state2).toEqual(state1);
+  });
+});
+
+describe('addSavedNumbering function', () => {
+  it('finds text and line elements of saved numbering', () => {
+    addNumbering(base1, 872);
+    let bn1 = base1.numbering;
+    let state = savableState(bn1);
+    addSavedNumbering(base2, state);
+    let bn2 = base2.numbering;
+    expect(areSameElement(bn1.text, bn2.text)).toBeTruthy();
+    expect(areSameElement(bn1.line, bn2.line)).toBeTruthy();
+  });
+
+  it('passes base center to newly created numbering', () => {
+    addNumbering(base1, 480);
+    base1.numbering.basePadding = 16.8;
+    let state = savableState(base1.numbering);
+    addSavedNumbering(base2, state);
+    // requires that the base center was passed
+    expect(base2.numbering.basePadding).toBeCloseTo(16.8);
+  });
+
+  it('throws if unable to find text element', () => {
+    addNumbering(base1, 20);
+    let state = savableState(base1.numbering);
+    base1.numbering.text.remove();
+    expect(
+      () => addSavedNumbering(base2, state)
+    ).toThrow();
+  });
+
+  it('throws if unable to find line element', () => {
+    addNumbering(base1, 1);
+    let state = savableState(base1.numbering);
+    base1.numbering.line.remove();
+    expect(
+      () => addSavedNumbering(base2, state)
+    ).toThrow();
+  });
+
+  it('throws if base is already numbered', () => {
+    // would rather not remove numbering that a base already has
+    // since doing so might remove the text and line elements of
+    // the saved numbering (e.g., if this function was called twice
+    // on the same base and for the same saved numbering)
+    addNumbering(base1, 30);
+    let state = savableState(base1.numbering);
+    addNumbering(base2, 40);
+    expect(
+      () => addSavedNumbering(base2, state)
+    ).toThrow();
+  });
+
+  it('throws if saved state has wrong class name', () => {
+    addNumbering(base1, 50);
+    let state = savableState(base1.numbering);
+    state.className = 'BaseNumbring';
+    expect(
+      () => addSavedNumbering(base2, state)
+    ).toThrow();
   });
 });
--- src/draw/bases/numbering/save.ts
+++ src/draw/bases/numbering/save.ts
@@ -1,12 +1,36 @@
 import {
-  BaseNumberingInterface as BaseNumbering,
+  BaseNumberingInterface,
   SavableState,
 } from './BaseNumberingInterface';
+import { BaseInterface as Base } from 'Draw/BaseInterface';
+import * as SVG from '@svgdotjs/svg.js';
+import { findTextByUniqueId, findLineByUniqueId } from 'Draw/saved';
+import { BaseNumbering } from './BaseNumbering';
 
-export function savableState(bn: BaseNumbering): SavableState {
+export function savableState(bn: BaseNumberingInterface): SavableState {
   return {
     className: 'BaseNumbering',
     textId: String(bn.text.id()),
     lineId: String(bn.line.id()),
   };
+}
+
+export type SavedState = { [key: string]: unknown }
+
+export function addSavedNumbering(b: Base, saved: SavedState): void | never {
+  if (saved.className != 'BaseNumbering') {
+    throw new Error('Saved state is not for a base numbering.');
+  }
+  let svg = b.text.root();
+  if (!svg || !(svg instanceof SVG.Svg)) {
+    throw new Error('Unable to retrieve root SVG element of base.');
+  } else {
+    let text = findTextByUniqueId(svg, saved.textId);
+    let line = findLineByUniqueId(svg, saved.lineId);
+    let bn = new BaseNumbering(text, line, { x: b.xCenter, y: b.yCenter });
+    if (b.numbering) {
+      throw new Error('Base already has numbering.');
+    }
+    b.numbering = bn;
+  }
 }
