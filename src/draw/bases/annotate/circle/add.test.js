c16788a9be28739612ab4c08b70080f9393c52d5
Philip Johnson <p.zhao.johnson@gmail.com>
2021-06-26 18:07:58 -0400

    Add functions to add base outlines and highlighting

--- src/draw/bases/annotate/circle/add.test.js
+++ src/draw/bases/annotate/circle/add.test.js
@@ -1,12 +1,14 @@
 import {
+  addOutline,
+  addHighlighting,
   removeOutline,
   removeHighlighting,
 } from './add';
 import { NodeSVG } from 'Draw/NodeSVG';
 import Base from 'Draw/Base';
 
-function wasRemoved(ele) {
-  return ele.root() ? false : true;
+function wasRemoved(cba) {
+  return cba.circle.root() ? false : true;
 }
 
 let container = null;
@@ -34,13 +36,63 @@ afterEach(() => {
   container = null;
 });
 
+describe('addOutline function', () => {
+  it('adds outline and positions circle', () => {
+    base.moveTo(178.5, 359);
+    expect(base.outline).toBe(undefined);
+    addOutline(base);
+    expect(base.outline).toBeTruthy(); // added outline
+    let o = base.outline;
+    // positioned circle
+    expect(o.circle.attr('cx')).toBeCloseTo(178.5);
+    expect(o.circle.attr('cy')).toBeCloseTo(359);
+  });
+
+  it('removes previous outline if present', () => {
+    addOutline(base);
+    let o1 = base.outline;
+    expect(wasRemoved(o1)).toBeFalsy();
+    addOutline(base);
+    expect(wasRemoved(o1)).toBeTruthy(); // removed previous outline
+    expect(base.outline.id).not.toEqual(o1.id); // added new outline
+    // double-check that IDs are defined
+    expect(o1.id).toBeTruthy();
+    expect(base.outline.id).toBeTruthy();
+  });
+});
+
+describe('addHighlighting function', () => {
+  it('adds highlighting and positions circle', () => {
+    base.moveTo(802, 1012.12);
+    expect(base.highlighting).toBe(undefined);
+    addHighlighting(base);
+    expect(base.highlighting).toBeTruthy(); // added highlighting
+    let h = base.highlighting;
+    // positioned circle
+    expect(h.circle.attr('cx')).toBeCloseTo(802);
+    expect(h.circle.attr('cy')).toBeCloseTo(1012.12);
+  });
+
+  it('removes previous highlighting if present', () => {
+    addHighlighting(base);
+    let h1 = base.highlighting;
+    expect(wasRemoved(h1)).toBeFalsy();
+    addHighlighting(base);
+    expect(wasRemoved(h1)).toBeTruthy(); // removed previous highlighting
+    expect(base.highlighting.id).not.toEqual(h1.id); // added new highlighting
+    // double-check that IDs are defined
+    expect(h1.id).toBeTruthy();
+    expect(base.highlighting.id).toBeTruthy();
+  });
+});
+
 describe('removeOutline function', () => {
-  it('removes circle and reference', () => {
-    base.addCircleOutline();
-    let c = base.outline.circle;
-    expect(wasRemoved(c)).toBeFalsy();
+  it('removes outline itself and reference', () => {
+    addOutline(base);
+    let cba = base.outline;
+    expect(wasRemoved(cba)).toBeFalsy();
     removeOutline(base);
-    expect(wasRemoved(c)).toBeTruthy(); // removed circle
+    expect(wasRemoved(cba)).toBeTruthy(); // removed outline itself
     expect(base.outline).toBe(undefined); // removed reference
   });
 
@@ -53,12 +105,12 @@ describe('removeOutline function', () => {
 });
 
 describe('removeHighlighting function', () => {
-  it('removes circle and reference', () => {
-    base.addCircleHighlighting();
-    let c = base.highlighting.circle;
-    expect(wasRemoved(c)).toBeFalsy();
+  it('removes highlighting itself and reference', () => {
+    addHighlighting(base);
+    let cba = base.highlighting;
+    expect(wasRemoved(cba)).toBeFalsy();
     removeHighlighting(base);
-    expect(wasRemoved(c)).toBeTruthy(); // removed circle
+    expect(wasRemoved(cba)).toBeTruthy(); // removed highlighting itself
     expect(base.highlighting).toBe(undefined); // removed reference
   });
 
--- src/draw/bases/annotate/circle/add.ts
+++ src/draw/bases/annotate/circle/add.ts
@@ -1,7 +1,38 @@
-import { CircleBaseAnnotationInterface as CircleBaseAnnotation } from './CircleBaseAnnotationInterface';
+import { CircleBaseAnnotationInterface } from './CircleBaseAnnotationInterface';
+import { CircleBaseAnnotation } from './CircleBaseAnnotation';
 import { BaseInterface as Base } from 'Draw/BaseInterface';
+import * as SVG from '@svgdotjs/svg.js';
 
-function remove(cba: CircleBaseAnnotation) {
+// creates a circle annotation for the given base
+function create(b: Base): CircleBaseAnnotation | undefined {
+  let svg = b.text.root();
+  if (!(svg instanceof SVG.Svg)) {
+    console.error('Unable to retrieve root SVG element of base.');
+  } else {
+    let c = svg.circle(10);
+    let baseCenter = { x: b.xCenter, y: b.yCenter };
+    c.attr({ 'cx': baseCenter.x, 'cy': baseCenter.y });
+    return new CircleBaseAnnotation(c, baseCenter.x, baseCenter.y);
+  }
+}
+
+export function addOutline(b: Base) {
+  let cba = create(b);
+  if (cba) {
+    removeOutline(b);
+    b.outline = cba;
+  }
+}
+
+export function addHighlighting(b: Base) {
+  let cba = create(b);
+  if (cba) {
+    removeHighlighting(b);
+    b.highlighting = cba;
+  }
+}
+
+function remove(cba: CircleBaseAnnotationInterface) {
   cba.circle.remove();
 }
 
