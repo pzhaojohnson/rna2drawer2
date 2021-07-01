0570bba367c7e88b909ce53c8947a9159b47f939
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-01 10:22:49 -0400

    Add functions for working with straight bond values

---
+++ src/draw/bonds/straight/values.test.js
@@ -0,0 +1,97 @@
+import { values, setValues } from './values';
+import { NodeSVG } from 'Draw/NodeSVG';
+import Base from 'Draw/Base';
+import { StraightBond } from './StraightBond';
+import { round } from 'Math/round';
+
+function roundBasePaddings(vs, places=3) {
+  vs.basePadding1 = round(vs.basePadding1, places);
+  vs.basePadding2 = round(vs.basePadding2, places);
+}
+
+let container = null;
+let svg = null;
+let bond = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  svg = NodeSVG();
+  svg.addTo(container);
+
+  let line = svg.line(50, 20, 100, 20);
+  let base1 = Base.create(svg, 'T', 25, 20);
+  let base2 = Base.create(svg, 'A', 150, 20);
+  bond = new StraightBond(line, base1, base2);
+});
+
+afterEach(() => {
+  bond = null;
+
+  svg.clear();
+  svg.remove();
+  svg = null;
+
+  container.remove();
+  container = null;
+});
+
+describe('values function', () => {
+  it('returns values', () => {
+    // make all values different to test if they are mixed up
+    bond.line.attr({
+      'stroke': '#665a1f',
+      'stroke-width': 8.57,
+    });
+    bond.setPadding1(18.3);
+    bond.setPadding2(9.7);
+    let vs = values(bond);
+    roundBasePaddings(vs);
+    expect(vs).toEqual({
+      line: {
+        'stroke': '#665a1f',
+        'stroke-width': 8.57,
+      },
+      basePadding1: 18.3,
+      basePadding2: 9.7,
+    });
+  });
+});
+
+describe('setValues function', () => {
+  it('sets provided values', () => {
+    // make all values different to test if they are mixed up
+    let vs1 = {
+      line: {
+        'stroke': '#ab90f0',
+        'stroke-width': 12.02,
+      },
+      basePadding1: 9.8,
+      basePadding2: 12.1,
+    };
+    setValues(bond, vs1);
+    let vs2 = values(bond);
+    roundBasePaddings(vs2);
+    expect(vs2).toEqual(vs1);
+  });
+
+  it("doesn't set values to undefined", () => {
+    let vs1 = {
+      line: {
+        'stroke': '#66a210',
+        'stroke-width': 9.3,
+      },
+      basePadding1: 12.6,
+      basePadding2: 3.2,
+    };
+    setValues(bond, vs1);
+    // with a defined line values object
+    setValues(bond, { line: {} });
+    // with an undefined line values object
+    setValues(bond, {});
+    let vs2 = values(bond);
+    roundBasePaddings(vs2);
+    expect(vs2).toEqual(vs1);
+  });
+});
---
+++ src/draw/bonds/straight/values.ts
@@ -0,0 +1,52 @@
+import { StraightBondInterface as StraightBond } from './StraightBondInterface';
+
+export type Values = {
+  line: {
+    'stroke'?: string;
+    'stroke-width'?: number;
+  },
+  basePadding1?: number;
+  basePadding2?: number;
+}
+
+export function values(sb: StraightBond): Values {
+  let vs: Values = {
+    line: {},
+  };
+  let lineAttrs = {
+    'stroke': sb.line.attr('stroke'),
+    'stroke-width': sb.line.attr('stroke-width'),
+  };
+  if (typeof lineAttrs['stroke'] == 'string') {
+    vs.line['stroke'] = lineAttrs['stroke'];
+  }
+  if (typeof lineAttrs['stroke-width'] == 'number') {
+    vs.line['stroke-width'] = lineAttrs['stroke-width'];
+  }
+  let basePadding1 = sb.getPadding1();
+  let basePadding2 = sb.getPadding2();
+  if (typeof basePadding1 == 'number') {
+    vs.basePadding1 = basePadding1;
+  }
+  if (typeof basePadding2 == 'number') {
+    vs.basePadding2 = basePadding2;
+  }
+  return vs;
+}
+
+export function setValues(sb: StraightBond, vs: Values) {
+  if (vs.line) {
+    if (typeof vs.line['stroke'] == 'string') {
+      sb.line.attr({ 'stroke': vs.line['stroke'] });
+    }
+    if (typeof vs.line['stroke-width'] == 'number') {
+      sb.line.attr({ 'stroke-width': vs.line['stroke-width'] });
+    }
+  }
+  if (typeof vs.basePadding1 == 'number') {
+    sb.setPadding1(vs.basePadding1);
+  }
+  if (typeof vs.basePadding2 == 'number') {
+    sb.setPadding2(vs.basePadding2);
+  }
+}
