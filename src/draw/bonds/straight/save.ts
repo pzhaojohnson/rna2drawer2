362bb0f5cd85aa54246b6c849d1d8963cf8e308d
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-04 08:46:51 -0400

    Move functions for adding saved primary and secondary bonds to their own module

--- src/draw/bonds/straight/save.ts
+++ src/draw/bonds/straight/save.ts
@@ -1,14 +1,7 @@
-import { DrawingInterface as Drawing } from 'Draw/DrawingInterface';
 import {
   StraightBondInterface,
   StraightBondSavableState as SavableState,
 } from './StraightBondInterface';
-import { PrimaryBond } from './PrimaryBond';
-import { SecondaryBond } from './SecondaryBond';
-import { secondaryBondTypes } from './SecondaryBondInterface';
-import { findLineByUniqueId } from 'Draw/saved/svg';
-import { getBaseByUniqueId } from 'Draw/saved/bases';
-import { values } from './values';
 
 export function savableState(sb: StraightBondInterface): SavableState {
   return {
@@ -17,52 +10,4 @@ export function savableState(sb: StraightBondInterface): SavableState {
     baseId1: sb.base1.id,
     baseId2: sb.base2.id,
   };
-}
-
-export type SavedState = { [key: string]: unknown }
-
-function assertIsSavedStraightBond(state: SavedState): void | never {
-  if (state.className != 'StraightBond') {
-    throw new Error('Saved state is not for a straight bond.');
-  }
-}
-
-export function addSavedPrimaryBonds(drawing: Drawing, saved: SavedState[]): void | never {
-  saved.forEach(state => {
-    assertIsSavedStraightBond(state);
-    let pb = new PrimaryBond(
-      findLineByUniqueId(drawing.svg, state.lineId),
-      getBaseByUniqueId(drawing, state.baseId1),
-      getBaseByUniqueId(drawing, state.baseId2),
-    );
-    drawing.primaryBonds.push(pb);
-    PrimaryBond.recommendedDefaults = values(pb);
-  });
-}
-
-export function addSavedSecondaryBonds(drawing: Drawing, saved: SavedState[]): void | never {
-  saved.forEach(state => {
-    assertIsSavedStraightBond(state);
-    let sb = new SecondaryBond(
-      findLineByUniqueId(drawing.svg, state.lineId),
-      getBaseByUniqueId(drawing, state.baseId1),
-      getBaseByUniqueId(drawing, state.baseId2),
-    );
-    drawing.secondaryBonds.push(sb);
-
-    // set recommended defaults for secondary bonds
-    let sbt = sb.type;
-    let sbvs = values(sb);
-    secondaryBondTypes.forEach(t => {
-      let vs = SecondaryBond.recommendedDefaults[t];
-      SecondaryBond.recommendedDefaults[t] = {
-        ...sbvs,
-        line: {
-          ...sbvs.line,
-          // only line stroke may differ by secondary bond type
-          'stroke': t == sbt ? sbvs.line.stroke : vs.line.stroke,
-        },
-      };
-    });
-  });
 }
