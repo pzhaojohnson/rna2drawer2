579b634c35a903b68c48dd8907fd432adc6c4e8e
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-10 11:14:54 -0400

    Add midpoint function

---
+++ src/math/midpoint.test.js
@@ -0,0 +1,33 @@
+import { midpoint2D } from './midpoint';
+import { round } from 'Math/round';
+
+function round2D(p, places=3) {
+  return {
+    x: round(p.x, places),
+    y: round(p.y, places),
+  };
+}
+
+it('midpoint2D function', () => {
+  [
+    {
+      p1: { x: 10, y: 5 },
+      p2: { x: 20, y: 10 },
+      mp: { x: 15, y: 7.5 },
+    },
+    {
+      p1: { x: -120, y: 250},
+      p2: { x: 90, y: -1000 },
+      mp: { x: -15, y: -375 },
+    },
+    {
+      p1: { x: 2000, y: 1000 },
+      p2: { x: 1998, y: 1002 },
+      mp: { x: 1999, y: 1001 },
+    },
+  ].forEach(ps => {
+    let p = midpoint2D(ps.p1, ps.p2);
+    p = round2D(p, 3);
+    expect(p).toEqual(ps.mp);
+  });
+});
---
+++ src/math/midpoint.ts
@@ -0,0 +1,8 @@
+import { Point2D } from './Point';
+
+export function midpoint2D(p1: Point2D, p2: Point2D): Point2D {
+  return {
+    x: (p1.x + p2.x) / 2,
+    y: (p1.y + p2.y) / 2,
+  };
+}
