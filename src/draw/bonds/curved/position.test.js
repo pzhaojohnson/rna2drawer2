e757491d267459fa7782f25e6b37e5b30a7ddabd
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-10 17:48:08 -0400

    Add function to position quadratic bezier bonds

---
+++ src/draw/bonds/curved/position.test.js
@@ -0,0 +1,84 @@
+import { position } from './position';
+import { NodeSVG } from 'Draw/NodeSVG';
+import Base from 'Draw/Base';
+import { QuadraticBezierBond } from './QuadraticBezierBond';
+
+let container = null;
+let svg = null;
+let bond = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  svg = NodeSVG();
+  svg.addTo(container);
+
+  let path = svg.path('M 20 30 Q 50 40 10 200');
+  let base1 = Base.create(svg, 'A', 20, 30);
+  let base2 = Base.create(svg, 'U', 50, 60);
+  bond = new QuadraticBezierBond(path, base1, base2);
+});
+
+afterEach(() => {
+  bond = null;
+
+  svg.clear();
+  svg.remove();
+  svg = null;
+
+  container.remove();
+  container = null;
+});
+
+describe('position function', () => {
+  it('positions bond', () => {
+    bond.base1.moveTo(101, 203.5);
+    bond.base2.moveTo(802.9, 935);
+    position(bond, {
+      basePadding1: 12.8,
+      basePadding2: 19.7,
+      controlPointDisplacement: {
+        magnitude: 82.4,
+        angle: 3.87,
+      },
+    });
+    let pa = bond.path.array();
+    expect(pa.length).toBe(2);
+    let m = pa[0];
+    expect(m[0]).toBe('M');
+    expect(m[1]).toBeCloseTo(110.924617);
+    expect(m[2]).toBeCloseTo(211.583438);
+    expect(m.length).toBe(3);
+    let q = pa[1];
+    expect(q[0]).toBe('Q');
+    expect(q[1]).toBeCloseTo(448.955950);
+    expect(q[2]).toBeCloseTo(486.904413);
+    expect(q[3]).toBeCloseTo(790.689089);
+    expect(q[4]).toBeCloseTo(919.540903);
+    expect(q.length).toBe(5);
+  });
+
+  it('smoke test', () => {
+    for (let i = 0; i < 50; i++) {
+      bond.base1.moveTo(
+        (1000 * Math.random()) - 500, // include negatives
+        (1000 * Math.random()) - 500,
+      );
+      bond.base2.moveTo(
+        (1000 * Math.random()) - 500,
+        (1000 * Math.random()) - 500,
+      );
+      expect(() => {
+        position(bond, {
+          basePadding1: 200 * Math.random(),
+          basePadding2: 200 * Math.random(),
+          controlPointDisplacement: {
+            magnitude: 300 * Math.random(),
+            angle: (20 * Math.random()) - 10, // include negatives
+          },
+        });
+      }).not.toThrow();
+    }
+  });
+});
---
+++ src/draw/bonds/curved/position.ts
@@ -0,0 +1,40 @@
+import { QuadraticBezierBondInterface as QuadraticBezierBond } from './QuadraticBezierBondInterface';
+import { Positioning } from './positioning';
+import { midpoint2D as midpoint } from 'Math/midpoint';
+
+export function position(bond: QuadraticBezierBond, p: Positioning) {
+  try {
+    let baseCenter1 = { x: bond.base1.xCenter, y: bond.base1.yCenter };
+    let baseCenter2 = { x: bond.base2.xCenter, y: bond.base2.yCenter };
+
+    let mp = midpoint(baseCenter1, baseCenter2);
+    let a12 = Math.atan2(baseCenter2.y - baseCenter1.y, baseCenter2.x - baseCenter1.x);
+    let amc = a12 + p.controlPointDisplacement.angle;
+    let controlPoint = {
+      x: mp.x + (p.controlPointDisplacement.magnitude * Math.cos(amc)),
+      y: mp.y + (p.controlPointDisplacement.magnitude * Math.sin(amc)),
+    };
+
+    let a1c = Math.atan2(controlPoint.y - baseCenter1.y, controlPoint.x - baseCenter1.x);
+    let startingPoint = {
+      x: baseCenter1.x + (p.basePadding1 * Math.cos(a1c)),
+      y: baseCenter1.y + (p.basePadding1 * Math.sin(a1c)),
+    };
+
+    let a2c = Math.atan2(controlPoint.y - baseCenter2.y, controlPoint.x - baseCenter2.x);
+    let endPoint = {
+      x: baseCenter2.x + (p.basePadding2 * Math.cos(a2c)),
+      y: baseCenter2.y + (p.basePadding2 * Math.sin(a2c)),
+    };
+
+    let d = [
+      'M', startingPoint.x, startingPoint.y,
+      'Q', controlPoint.x, controlPoint.y, endPoint.x, endPoint.y,
+    ].join(' ');
+    bond.path.plot(d);
+
+  } catch (error) {
+    console.error(error);
+    console.error('Unable to position quadratic bezier bond.');
+  }
+}
