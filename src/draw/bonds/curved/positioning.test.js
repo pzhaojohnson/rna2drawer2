bc4893f48b220b98d18afc88a98d071d18fd5396
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-10 17:10:59 -0400

    Add function to retrieve the positioning of a quadratic bezier bond

---
+++ src/draw/bonds/curved/positioning.test.js
@@ -0,0 +1,62 @@
+import { positioning } from './positioning';
+import { NodeSVG } from 'Draw/NodeSVG';
+import Base from 'Draw/Base';
+import { QuadraticBezierBond } from './QuadraticBezierBond';
+import { normalizeAngle } from 'Draw/normalizeAngle';
+import { round } from 'Math/round';
+
+function roundPositioning(p, places=3) {
+  p.basePadding1 = round(p.basePadding1, places);
+  p.basePadding2 = round(p.basePadding2, places);
+  p.controlPointDisplacement = {
+    magnitude: round(p.controlPointDisplacement.magnitude, places),
+    angle: round(p.controlPointDisplacement.angle, places),
+  };
+}
+
+let container = null;
+let svg = null;
+let bond = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  svg = NodeSVG();
+  svg.addTo(container);
+
+  let path = svg.path('M 20 30 Q 40 50 60 70');
+  let base1 = Base.create(svg, 'Q', 10, 20);
+  let base2 = Base.create(svg, 'W', 100, 200);
+  bond = new QuadraticBezierBond(path, base1, base2);
+});
+
+afterEach(() => {
+  bond = null;
+
+  svg.clear();
+  svg.remove();
+  svg = null;
+
+  container.remove();
+  container = null;
+});
+
+describe('positioning function', () => {
+  it('retrieves positioning', () => {
+    bond.base1.moveTo(52, 156);
+    bond.base2.moveTo(78, 212);
+    bond.path.plot('M 60.469585 155.281582 Q 120.513767 150.188438 86.047063 200.300223');
+    let p = positioning(bond);
+    p.controlPointDisplacement.angle = normalizeAngle(p.controlPointDisplacement.angle);
+    roundPositioning(p);
+    expect(p).toEqual({
+      basePadding1: 8.5,
+      basePadding2: 14.2,
+      controlPointDisplacement: {
+        magnitude: 65,
+        angle: 4.6,
+      },
+    });
+  });
+});
---
+++ src/draw/bonds/curved/positioning.ts
@@ -0,0 +1,78 @@
+import { QuadraticBezierBondInterface as QuadraticBezierBond } from './QuadraticBezierBondInterface';
+import { midpoint2D as midpoint } from 'Math/midpoint';
+import { distance2D as distance } from 'Math/distance';
+import { normalizeAngle } from 'Draw/normalizeAngle';
+
+export type Positioning = {
+  basePadding1: number;
+  basePadding2: number;
+
+  // relative to the midpoint between the centers
+  // of bases 1 and 2 of the bond
+  controlPointDisplacement: {
+    magnitude: number;
+
+    // relative to the angle from the center of base 1
+    // to the center of base 2
+    angle: number;
+  }
+}
+
+function isPositioning(p: any): p is Positioning {
+  return (
+    p instanceof Object
+    && typeof p.basePadding1 == 'number'
+    && typeof p.basePadding2 == 'number'
+    && p.controlPointDisplacement instanceof Object
+    && typeof p.controlPointDisplacement.magnitude == 'number'
+    && typeof p.controlPointDisplacement.angle == 'number'
+  );
+}
+
+function positioningIsFinite(p: Positioning): boolean {
+  return (
+    Number.isFinite(p.basePadding1)
+    && Number.isFinite(p.basePadding2)
+    && Number.isFinite(p.controlPointDisplacement.magnitude)
+    && Number.isFinite(p.controlPointDisplacement.angle)
+  );
+}
+
+// returns undefined if unable to retrieve positioning
+export function positioning(bond: QuadraticBezierBond): Positioning | undefined {
+  try {
+    // forgo type checking until end of try block
+    let pa: any = bond.path.array();
+    let m = pa[0];
+    let q = pa[1];
+    let startingPoint = { x: m[1], y: m[2] };
+    let controlPoint = { x: q[1], y: q[2] };
+    let endPoint = { x: q[3], y: q[4] };
+
+    let baseCenter1 = { x: bond.base1.xCenter, y: bond.base1.yCenter };
+    let baseCenter2 = { x: bond.base2.xCenter, y: bond.base2.yCenter };
+    
+    let mp = midpoint(baseCenter1, baseCenter2);
+    let a12 = Math.atan2(baseCenter2.y - baseCenter1.y, baseCenter2.x - baseCenter1.x);
+    let amc = Math.atan2(controlPoint.y - mp.y, controlPoint.x - mp.x);
+    
+    let p = {
+      basePadding1: distance(baseCenter1.x, baseCenter1.y, startingPoint.x, startingPoint.y),
+      basePadding2: distance(baseCenter2.x, baseCenter2.y, endPoint.x, endPoint.y),
+      controlPointDisplacement: {
+        magnitude: distance(mp.x, mp.y, controlPoint.x, controlPoint.y),
+        angle: normalizeAngle(amc, a12) - a12,
+      },
+    };
+
+    // type check (and check for finiteness) before returning
+    if (isPositioning(p) && positioningIsFinite(p)) {
+      return p;
+    } else {
+      console.error('Unable to retrieve positioning of quadratic bezier bond.');
+    }
+  } catch (error) {
+    console.error(error);
+    console.error('Unable to retrieve positioning of quadratic bezier bond.');
+  }
+}
