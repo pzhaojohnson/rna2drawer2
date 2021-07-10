606d4930874d236e9a45f8c152139e044b630279
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-10 10:55:38 -0400

    Add function to check if an SVG path specifies a quadratic bezier curve

---
+++ src/draw/bonds/curved/QuadraticBezierCurve.ts
@@ -0,0 +1,33 @@
+import * as SVG from '@svgdotjs/svg.js';
+
+export type QuadraticBezierCurve = [
+  ['M', number, number],
+  ['Q', number, number, number, number],
+]
+
+function isFiniteNumber(v: unknown): v is number {
+  return typeof v == 'number' && Number.isFinite(v);
+}
+
+// not sure if this function can be turned into a true type predicate
+export function isQuadraticBezierCurve(pa: SVG.PathArray): boolean {
+  try {
+    let m = pa[0];
+    let q = pa[1];
+    return (
+      m
+      && m[0] == 'M'
+      && isFiniteNumber(m[1])
+      && isFiniteNumber(m[2])
+      && q
+      && q[0] == 'Q'
+      && isFiniteNumber(q[1])
+      && isFiniteNumber(q[2])
+      && isFiniteNumber(q[3])
+      && isFiniteNumber(q[4])
+    );
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
