f86be94214b17bec4f3190b7a67e2987b74dc6c6
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-11 08:45:09 -0400

    Add function to shift the control point of a quadratic bezier bond

---
+++ src/draw/bonds/curved/drag.test.js
@@ -0,0 +1,77 @@
+import { shiftControlPoint } from './drag';
+import { NodeSVG } from 'Draw/NodeSVG';
+import Base from 'Draw/Base';
+import { QuadraticBezierBond } from './QuadraticBezierBond';
+import { TertiaryBond } from './TertiaryBond';
+import { setValues } from './values';
+
+let container = null;
+let svg = null;
+let bond = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  svg = NodeSVG();
+  svg.addTo(container);
+
+  let path = svg.path('M 10 20 Q 44 55 66 77');
+  let base1 = Base.create(svg, 'A', 20, 30);
+  let base2 = Base.create(svg, 'B', 200, 100);
+  bond = new QuadraticBezierBond(path, base1, base2);
+  setValues(bond, TertiaryBond.recommendedDefaults);
+});
+
+afterEach(() => {
+  bond = null;
+
+  svg.clear();
+  svg.remove();
+  svg = null;
+
+  container.remove();
+  container = null;
+});
+
+describe('shiftControlPoint function', () => {
+  it('updates path and displacement', () => {
+    bond.base1.moveTo(33, 67);
+    bond.base2.moveTo(1120, 821);
+    bond.basePadding1 = 23.6;
+    bond.basePadding2 = 9.4;
+    bond.setControlPointDisplacement({
+      magnitude: 33.2,
+      angle: 0.9,
+    });
+    
+    shiftControlPoint(bond, { x: 44.5, y: -62 });
+    
+    // updated path
+    let pa = bond.path.array();
+    expect(pa.length).toBe(2);
+    let m = pa[0];
+    expect(m[0]).toBe('M');
+    expect(m[1]).toBeCloseTo(53.326680);
+    expect(m[2]).toBeCloseTo(78.991081);
+    expect(m.length).toBe(3);
+    let q = pa[1];
+    expect(q[1]).toBeCloseTo(623.134724);
+    expect(q[2]).toBeCloseTo(415.131298);
+    expect(q[3]).toBeCloseTo(1112.720082);
+    expect(q[4]).toBeCloseTo(815.053336);
+    expect(q.length).toBe(5);
+
+    // updated displacement
+    expect(bond.controlPointDisplacement().magnitude).toBeCloseTo(54.847054);
+    expect(bond.controlPointDisplacement().angle).toBeCloseTo(5.122431);
+
+    // should be able to reposition the bond
+    // and maintain the new displacement
+    bond.base1.moveTo(6000, 3000);
+    bond.base2.moveTo(-200, 12);
+    bond.reposition();
+    expect(bond.controlPointDisplacement().magnitude).toBeCloseTo(54.847054);
+    expect(bond.controlPointDisplacement().angle).toBeCloseTo(5.122431);
+  });
+});
---
+++ src/draw/bonds/curved/drag.ts
@@ -0,0 +1,56 @@
+import { QuadraticBezierBondInterface as QuadraticBezierBond } from './QuadraticBezierBondInterface';
+import { midpoint2D as midpoint } from 'Math/midpoint';
+import { distance2D as distance } from 'Math/distance';
+import { normalizeAngle } from 'Draw/normalizeAngle';
+import { ControlPointDisplacement } from './positioning';
+
+function isControlPointDisplacement(v: any): v is ControlPointDisplacement {
+  return (
+    v instanceof Object
+    && typeof v.magnitude == 'number'
+    && typeof v.angle == 'number'
+  );
+}
+
+function controlPointDisplacementIsFinite(cpd: ControlPointDisplacement): boolean {
+  return Number.isFinite(cpd.magnitude) && Number.isFinite(cpd.angle);
+}
+
+export type Vector = {
+  x: number;
+  y: number;
+}
+
+export function shiftControlPoint(bond: QuadraticBezierBond, v: Vector) {
+  try {
+    // forgo type checking until end of try block
+    let pa: any = bond.path.array();
+    let q = pa[1];
+    let controlPoint = { x: q[1], y: q[2] };
+
+    let baseCenter1 = { x: bond.base1.xCenter, y: bond.base1.yCenter };
+    let baseCenter2 = { x: bond.base2.xCenter, y: bond.base2.yCenter };
+    let mp = midpoint(baseCenter1, baseCenter2);
+    let a12 = Math.atan2(baseCenter2.y - baseCenter1.y, baseCenter2.x - baseCenter1.x);
+
+    controlPoint.x += v.x;
+    controlPoint.y += v.y;
+    let amc = Math.atan2(controlPoint.y - mp.y, controlPoint.x - mp.x);
+
+    let cpd = {
+      magnitude: distance(mp.x, mp.y, controlPoint.x, controlPoint.y),
+      angle: normalizeAngle(amc, a12) - a12,
+    };
+
+    // type check (and check for finiteness) before setting
+    if (isControlPointDisplacement(cpd) && controlPointDisplacementIsFinite(cpd)) {
+      bond.setControlPointDisplacement(cpd);
+    } else {
+      console.error('Unable to shift control point of quadratic bezier bond.');
+    }
+    
+  } catch (error) {
+    console.error(error);
+    console.error('Unable to shift control point of quadratic bezier bond.');
+  }
+}
