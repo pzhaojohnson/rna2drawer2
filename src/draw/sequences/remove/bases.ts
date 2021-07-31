87f429cb81597fdf123edb873f1f6cfacbe40643
Philip Johnson <p.zhao.johnson@gmail.com>
2021-07-31 16:24:30 -0400

    Update the bonds in a drawing when removing bases

--- src/draw/sequences/remove/bases.ts
+++ src/draw/sequences/remove/bases.ts
@@ -1,11 +1,38 @@
 import { DrawingInterface as Drawing } from 'Draw/DrawingInterface';
 import { BaseInterface as Base } from 'Draw/bases/BaseInterface';
+import { PrimaryBondInterface as PrimaryBond } from 'Draw/bonds/straight/PrimaryBondInterface';
+import { addPrimaryBond } from 'Draw/bonds/straight/add';
+import { removePrimaryBondById, removeSecondaryBondById } from 'Draw/bonds/straight/remove';
+import { removeTertiaryBondById } from 'Draw/bonds/curved/remove';
 import { removeCircleOutline, removeCircleHighlighting } from 'Draw/bases/annotate/circle/add';
 import { removeNumbering } from 'Draw/bases/number/add';
+import { atPosition } from 'Array/at';
 import { updateBaseNumberings } from 'Draw/sequences/number';
 import { orientBaseNumberings } from 'Draw/bases/number/orient';
 
-export function removeBases(drawing: Drawing, bs: Base[]) {
+// removes all bonds with the bases
+function removeBondsWith(drawing: Drawing, bs: Base[]) {
+  let bonds = [
+    ...drawing.primaryBonds,
+    ...drawing.secondaryBonds,
+    ...drawing.tertiaryBonds,
+  ];
+  let ids: string[] = [];
+  let s = new Set(bs);
+  bonds.forEach(bond => {
+    if (s.has(bond.base1) || s.has(bond.base2)) {
+      ids.push(bond.id);
+    }
+  });
+  ids.forEach(id => {
+    removePrimaryBondById(drawing, id);
+    removeSecondaryBondById(drawing, id);
+    removeTertiaryBondById(drawing, id);
+  });
+}
+
+// removes the bases themselves
+function _removeBases(drawing: Drawing, bs: Base[]) {
   bs.forEach(b => {
     let seq = drawing.sequences.find(seq => seq.bases.includes(b));
     if (!seq) {
@@ -25,6 +52,36 @@ export function removeBases(drawing: Drawing, bs: Base[]) {
       }
     }
   });
+}
+
+// returns undefined if there is no primary bond between the two bases
+function findPrimaryBondBetween(drawing: Drawing, b1: Base, b2: Base): PrimaryBond | undefined {
+  return drawing.primaryBonds.find(
+    pb => pb.binds(b1) && pb.binds(b2)
+  );
+}
+
+function hasPrimaryBondBetween(drawing: Drawing, b1: Base, b2: Base): boolean {
+  return findPrimaryBondBetween(drawing, b1, b2) ? true : false;
+}
+
+// adds missing primary bonds between consecutive bases in sequences
+function addMissingPrimaryBonds(drawing: Drawing) {
+  drawing.sequences.forEach(seq => {
+    for (let p1 = 1; p1 < seq.bases.length; p1++) {
+      let b1 = atPosition(seq.bases, p1);
+      let b2 = atPosition(seq.bases, p1 + 1);
+      if (b1 && b2 && !hasPrimaryBondBetween(drawing, b1, b2)) {
+        addPrimaryBond(drawing, b1, b2);
+      }
+    }
+  });
+}
+
+export function removeBases(drawing: Drawing, bs: Base[]) {
+  removeBondsWith(drawing, bs);
+  _removeBases(drawing, bs);
+  addMissingPrimaryBonds(drawing);
   drawing.sequences.forEach(seq => updateBaseNumberings(seq));
   orientBaseNumberings(drawing);
 }
