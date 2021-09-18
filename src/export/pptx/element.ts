a3b7bcefe41849635d91f5f450cdacdebd41d0e5
Philip Johnson <p.zhao.johnson@gmail.com>
2021-09-18 10:30:01 -0400

    Define xmlns attribute for SVG images in exported PPTX files

--- src/export/pptx/element.ts
+++ src/export/pptx/element.ts
@@ -26,12 +26,15 @@ export type ImageOptions = {
 export function svgImageOptions(ele: SVG.Element): ImageOptions {
   let bbox = ele.bbox();
   let sw = strokeWidth(ele);
-  
-  // create a nested SVG document containing a copy of the element
+
   let svg = ele.root();
-  let nested = svg.nested(); // the nested SVG document
+  let nested = svg.nested(); // a nested SVG document
+
+  // xmlns attribute must be defined
+  nested.attr('xmlns', 'http://www.w3.org/2000/svg');
+
   nested.svg(ele.svg());
-  let first = nested.first(); // the copy of the element
+  let first = nested.first(); // a copy of the element
 
   // make the nested SVG document just large enough to hold the element copy
   nested.viewbox(0, 0, bbox.width + sw, bbox.height + sw);
@@ -39,13 +42,13 @@ export function svgImageOptions(ele: SVG.Element): ImageOptions {
 
   // center the element copy in the nested SVG document
   first.dmove(-bbox.x + (sw / 2), -bbox.y + (sw / 2));
-  
+
   let svgString = nested.svg();
-  
+
   // remove the nested SVG document
   nested.clear();
   nested.remove();
-  
+
   return {
     data: 'data:image/svg+xml;base64,' + window.btoa(svgString),
     x: round(pixelsToInches(bbox.x - (sw / 2)), 4),
@@ -59,7 +62,7 @@ export function svgImageOptions(ele: SVG.Element): ImageOptions {
 // (SVG images of zero area seem to cause errors when exporting)
 export function addAsSvgImage(slide: PptxGenJS.Slide, ele: SVG.Element) {
   let options = svgImageOptions(ele);
-  
+
   if (options.w > 0 && options.h > 0) {
     slide.addImage(options);
   }
