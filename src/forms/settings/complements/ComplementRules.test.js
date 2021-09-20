afca67c4be783c6a7316b6a31c284b4fd0751245
Philip Johnson <p.zhao.johnson@gmail.com>
2021-09-19 20:15:26 -0400

    Remove unnecessary unit test for complement rules form

--- src/forms/settings/complements/ComplementRules.test.js
+++
@@ -1,9 +0,0 @@
-import ComplementRules from './ComplementRules';
-import App from '../../../App';
-import { NodeSVG } from 'Draw/svg/NodeSVG';
-
-let app = new App(() => NodeSVG());
-
-it('renders', () => {
-  expect(ComplementRules(app)).toBeTruthy();
-});
