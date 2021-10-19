b4f6b9f26722e1feb2ddffe8e2abbf5aafac44d2
Philip Johnson <p.zhao.johnson@gmail.com>
2021-10-19 19:06:04 -0400

    Remove unused field props type definition

--- src/forms/edit/baseNumbering/FieldProps.ts
+++
@@ -1,7 +0,0 @@
-import { BaseNumberingInterface as BaseNumbering } from 'Draw/bases/number/BaseNumberingInterface';
-
-export interface FieldProps {
-  getBaseNumberings: () => BaseNumbering[];
-  pushUndo: () => void;
-  changed: () => void;
-}
