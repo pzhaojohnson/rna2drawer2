61d8ff112cb86195e7c9048301088b6c6fe2c9a7
Philip Johnson <p.zhao.johnson@gmail.com>
2021-10-23 09:32:18 -0400

    Remove unused field props type definition for primary bonds

--- src/forms/edit/primaryBonds/FieldProps.ts
+++
@@ -1,7 +0,0 @@
-import { PrimaryBondInterface as PrimaryBond } from 'Draw/bonds/straight/PrimaryBondInterface';
-
-export interface FieldProps {
-  getPrimaryBonds: () => PrimaryBond[];
-  pushUndo: () => void;
-  changed: () => void;
-}
