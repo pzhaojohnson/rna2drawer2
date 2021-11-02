cd77c52df4f805708092910d2695b2c1a07747ae
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-01 22:14:32 -0400

    Convert stroke opacity input for primary bonds to a class component

--- src/forms/edit/primaryBonds/StrokeOpacityInput.tsx
+++ src/forms/edit/primaryBonds/StrokeOpacityInput.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useState } from 'react';
 import textFieldStyles from 'Forms/fields/text/TextField.css';
 import { AppInterface as App } from 'AppInterface';
 import { PrimaryBondInterface } from 'Draw/bonds/straight/PrimaryBondInterface';
@@ -14,37 +13,44 @@ export type Props = {
   primaryBonds: PrimaryBondInterface[];
 }
 
-// returns an empty string for an empty primary bonds array
+type Value = string;
+
+type State = {
+  value: Value;
+}
+
+// returns an empty string value for an empty primary bonds array
 // or if not all primary bonds have the same stroke opacity
-function currStrokeOpacityPercentage(primaryBonds: PrimaryBondInterface[]): string {
-  let sops = new Set<number>();
+function currStrokeOpacityPercentage(primaryBonds: PrimaryBondInterface[]): Value {
+  let sops = new Set<Value>();
   primaryBonds.forEach(pb => {
     let so = pb.line.attr('stroke-opacity');
     let n = parseNumber(so);
     if (n) {
       let sop = 100 * n.valueOf();
-      sops.add(round(sop, 0));
+      sop = round(sop, 0);
+      sops.add(sop + '%');
     }
   });
   if (sops.size == 1) {
-    return sops.values().next().value + '%';
+    return sops.values().next().value;
   } else {
     return '';
   }
 }
 
-function valueIsValid(value: string): boolean {
-  let n = Number.parseFloat(value);
-  return Number.isFinite(n);
+function isBlank(v: Value): boolean {
+  return v.trim().length == 0;
 }
 
-function valuesAreEqual(value1: string, value2: string): boolean {
-  return Number.parseFloat(value1) == Number.parseFloat(value2);
+function areEqual(v1: Value, v2: Value): boolean {
+  return Number.parseFloat(v1) == Number.parseFloat(v2);
 }
 
-// converts values less than 0 to 0 and greater than 1 to 1
-function clampOpacity(o: number): number {
-  if (o < 0) {
+function constrainOpacity(o: number): number {
+  if (!Number.isFinite(o)) {
+    return 1;
+  } else if (o < 0) {
     return 0;
   } else if (o > 1) {
     return 1;
@@ -53,42 +59,55 @@ function clampOpacity(o: number): number {
   }
 }
 
-function setStrokeOpacitiesIfShould(props: Props, value: string) {
-  if (valueIsValid(value)) {
-    if (!valuesAreEqual(value, currStrokeOpacityPercentage(props.primaryBonds))) {
-      props.app.pushUndo();
-      let sop = Number.parseFloat(value);
-      let so = sop / 100;
-      so = clampOpacity(so);
-      so = round(so, 4);
-      props.primaryBonds.forEach(pb => {
-        pb.line.attr({ 'stroke-opacity': so });
-      });
-      PrimaryBond.recommendedDefaults.line['stroke-opacity'] = so;
-      props.app.drawingChangedNotByInteraction();
+export class StrokeOpacityInput extends React.Component<Props> {
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      value: currStrokeOpacityPercentage(props.primaryBonds),
+    };
+  }
+  
+  render() {
+    return (
+      <input
+        type='text'
+        className={textFieldStyles.input}
+        value={this.state.value}
+        onChange={event => this.setState({ value: event.target.value })}
+        onBlur={() => {
+          this.submit();
+          this.props.app.drawingChangedNotByInteraction();
+        }}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
+            this.submit();
+            this.props.app.drawingChangedNotByInteraction();
+          }
+        }}
+        style={{ width: '32px', textAlign: 'end' }}
+      />
+    );
+  }
+
+  submit() {
+    if (!isBlank(this.state.value)) {
+      let sop = Number.parseFloat(this.state.value);
+      if (Number.isFinite(sop)) {
+        if (!areEqual(this.state.value, currStrokeOpacityPercentage(this.props.primaryBonds))) {
+          this.props.app.pushUndo();
+          let so = sop / 100;
+          so = constrainOpacity(so);
+          so = round(so, 4);
+          this.props.primaryBonds.forEach(pb => {
+            pb.line.attr({ 'stroke-opacity': so });
+          });
+          PrimaryBond.recommendedDefaults.line['stroke-opacity'] = so;
+          this.props.app.drawingChangedNotByInteraction();
+        }
+      }
     }
   }
-}
-
-export function StrokeOpacityInput(props: Props) {
-  let [value, setValue] = useState(currStrokeOpacityPercentage(props.primaryBonds));
-  return (
-    <input
-      type='text'
-      className={textFieldStyles.input}
-      value={value}
-      onChange={event => setValue(event.target.value)}
-      onBlur={() => {
-        setStrokeOpacitiesIfShould(props, value);
-        props.app.drawingChangedNotByInteraction();
-      }}
-      onKeyUp={event => {
-        if (event.key.toLowerCase() == 'enter') {
-          setStrokeOpacitiesIfShould(props, value);
-          props.app.drawingChangedNotByInteraction();
-        }
-      }}
-      style={{ width: '32px', textAlign: 'end' }}
-    />
-  );
 }
