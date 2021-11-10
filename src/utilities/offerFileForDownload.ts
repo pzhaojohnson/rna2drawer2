890c331a46a7e14244edf640dc44e81a0337e81b
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-10 11:16:45 -0500

    Rework code to offer files for download and stop checking input file props for truthiness

--- src/utilities/offerFileForDownload.ts
+++ src/utilities/offerFileForDownload.ts
@@ -1,38 +1,41 @@
-export interface FileProps {
+type BlobDownloadOptions = {
+
+  // the name for the download
+  name: string;
+}
+
+function offerBlobForDownload(blob: Blob, options: BlobDownloadOptions) {
+  let url = URL.createObjectURL(blob);
+
+  let div = document.createElement('div');
+  div.style.cssText = 'max-width: 0px; max-height: 0px';
+  document.body.appendChild(div);
+
+  let a = document.createElement('a');
+  div.appendChild(a);
+  a.innerHTML = '&nbsp;';
+  a.href = url;
+  a.download = options.name;
+
+  let me = new MouseEvent('click', {});
+  a.dispatchEvent(me);
+
+  document.body.removeChild(div);
+}
+
+export type FileProps = {
   name: string;
   type: string;
   contents: string;
 }
 
 export function offerFileForDownload(fileProps: FileProps) {
-  if (!fileProps.name) {
-    console.error('Missing file name.');
-    return;
-  } else if (!fileProps.type) {
-    console.error('Missing file type.');
-    return;
-  } else if (!fileProps.contents) {
-    console.error('Missing file contents.');
-    return;
-  }
   let blob = new Blob(
     [fileProps.contents],
     { type: fileProps.type },
   );
-  offerBlobForDownload(blob, fileProps.name);
-}
-
-function offerBlobForDownload(blob: Blob, downloadName: string) {
-  let url = URL.createObjectURL(blob);
-  let div = document.createElement('div');
-  div.style.cssText = 'max-width: 0px; max-height: 0px';
-  document.body.appendChild(div);
-  let a = document.createElement('a');
-  div.appendChild(a);
-  a.innerHTML = '&nbsp;';
-  a.href = url;
-  a.download = downloadName;
-  let me = new MouseEvent('click', {});
-  a.dispatchEvent(me);
-  document.body.removeChild(div);
+  offerBlobForDownload(
+    blob,
+    { name: fileProps.name },
+  );
 }
