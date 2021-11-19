0fef2da2448efe20d4d6c5c3c0c7023f7206cc30
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-19 11:20:01 -0500

    Remove pedantic unit testing of openNewTab function

--- src/menu/openNewTab.test.js
+++
@@ -1,7 +0,0 @@
-import openNewTab from './openNewTab';
-
-it('calls window.open with current URL', () => {
-  window.open = jest.fn();
-  openNewTab();
-  expect(window.open.mock.calls[0][0]).toBe(document.URL);
-});
