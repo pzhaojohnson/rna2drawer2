68dc5ab03033f056cbeae53355527a922f6fd0dd
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-24 10:15:54 -0500

    Rework test suites for zoom in and zoom out buttons

--- src/infobar/zoom/ZoomInButton.test.js
+++ src/infobar/zoom/ZoomInButton.test.js
@@ -1,18 +1,20 @@
-import { ZoomInButton } from './ZoomInButton';
-import React from 'react';
+import * as React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
-import App from 'App';
+import { App } from 'App';
 import { NodeSVG } from 'Draw/svg/NodeSVG';
 import { resize } from 'Draw/dimensions';
 import { zoom, setZoom } from 'Draw/zoom';
 
-let app = new App(() => NodeSVG());
+import { ZoomInButton } from './ZoomInButton';
 
+let app = null;
 let container = null;
 
 beforeEach(() => {
+  app = new App(() => NodeSVG());
+
   container = document.createElement('div');
   document.body.appendChild(container);
 });
@@ -21,23 +23,31 @@ afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
+
+  app = null;
 });
 
-describe('ZoomInButton component', () => {
-  it('zooms in on click', () => {
-    resize(app.strictDrawing.drawing, { width: 200, height: 300 });
-    let curr = 0.6;
-    setZoom(app.strictDrawing.drawing, curr);
-    act(() => {
-      render(<ZoomInButton app={app} />, container);
-    });
-    // repeated clicks
-    for (let i = 0; i < 5; i++) {
-      let prev = curr;
-      fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
-      curr = zoom(app.strictDrawing.drawing);
-      expect(curr).toBeGreaterThan(prev);
-      prev = curr;
-    }
+test('ZoomInButton component', () => {
+  act(() => {
+    render(<ZoomInButton app={app} />, container);
   });
+
+  let drawing = app.strictDrawing.drawing;
+  resize(drawing, { width: 200, height: 300 });
+
+  [0.2, 0.5, 1, 1.6, 3.8, 6.25].forEach(z => {
+    setZoom(drawing, z);
+    // click zoom in button
+    fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
+    // zoomed in
+    expect(zoom(drawing)).toBeGreaterThan(z);
+  });
+
+  // should be higher than any preset zoom
+  let z = 1000;
+  setZoom(drawing, z);
+  // click zoom in button
+  fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
+  // maintained zoom
+  expect(zoom(drawing)).toBeCloseTo(z, 6);
 });
--- src/infobar/zoom/ZoomOutButton.test.js
+++ src/infobar/zoom/ZoomOutButton.test.js
@@ -1,18 +1,20 @@
-import { ZoomOutButton } from './ZoomOutButton';
-import React from 'react';
+import * as React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { fireEvent } from '@testing-library/react';
-import App from 'App';
+import { App } from 'App';
 import { NodeSVG } from 'Draw/svg/NodeSVG';
 import { resize } from 'Draw/dimensions';
 import { zoom, setZoom } from 'Draw/zoom';
 
-let app = new App(() => NodeSVG());
+import { ZoomOutButton } from './ZoomOutButton';
 
+let app = null;
 let container = null;
 
 beforeEach(() => {
+  app = new App(() => NodeSVG());
+
   container = document.createElement('div');
   document.body.appendChild(container);
 });
@@ -21,23 +23,31 @@ afterEach(() => {
   unmountComponentAtNode(container);
   container.remove();
   container = null;
+
+  app = null;
 });
 
-describe('ZoomOutButton component', () => {
-  it('zooms out on click', () => {
-    resize(app.strictDrawing.drawing, { width: 200, height: 300 });
-    let curr = 4.5;
-    setZoom(app.strictDrawing.drawing, curr);
-    act(() => {
-      render(<ZoomOutButton app={app} />, container);
-    });
-    // repeated clicks
-    for (let i = 0; i < 5; i++) {
-      let prev = curr;
-      fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
-      curr = zoom(app.strictDrawing.drawing);
-      expect(curr).toBeLessThan(prev);
-      prev = curr;
-    }
+test('ZoomOutButton component', () => {
+  act(() => {
+    render(<ZoomOutButton app={app} />, container);
   });
+
+  let drawing = app.strictDrawing.drawing;
+  resize(drawing, { width: 200, height: 300 });
+
+  [0.41, 0.8, 1, 2.5, 6.2, 8].forEach(z => {
+    setZoom(drawing, z);
+    // click zoom out button
+    fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
+    // zoomed out
+    expect(zoom(drawing)).toBeLessThan(z);
+  });
+
+  // should be lower than any preset zoom
+  let z = 0.00001;
+  setZoom(drawing, z);
+  // click zoom out button
+  fireEvent(container.firstChild, new MouseEvent('click', { bubbles: true }));
+  // maintained zoom
+  expect(zoom(drawing)).toBeCloseTo(z, 6);
 });
