eaf022acf604d542b60f51cc8f0d0ce0689625e8
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-27 09:40:14 -0500

    Remove unused isBold function

--- src/forms/fields/font/isBold.ts
+++
@@ -1,7 +0,0 @@
-export function isBold(fontWeight: string | number): boolean {
-  if (typeof fontWeight == 'number') {
-    return fontWeight >= 700;
-  } else {
-    return fontWeight == 'bold';
-  }
-}
