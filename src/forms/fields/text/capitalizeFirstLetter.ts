d94c20345ed1099cc473797183df484446409e41
Philip Johnson <p.zhao.johnson@gmail.com>
2021-11-27 10:50:27 -0500

    Remove unused areAllSameNumber and capitalizeFirstLetter functions

--- src/forms/fields/text/capitalizeFirstLetter.ts
+++
@@ -1,9 +0,0 @@
-export function capitalizeFirstLetter(s: string): string {
-  if (s.length == 0) {
-    return '';
-  } else {
-    return s.charAt(0).toUpperCase() + s.substring(1);
-  }
-}
-
-export default capitalizeFirstLetter;
