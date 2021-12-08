ccfb3b1367ae703d546055d79519157f35c8e999
Philip Johnson <p.zhao.johnson@gmail.com>
2021-12-08 11:41:41 -0500

    Add placeholder text to subsequence textarea

--- src/forms/edit/sequence/insertSubsequence/InsertSubsequence.tsx
+++ src/forms/edit/sequence/insertSubsequence/InsertSubsequence.tsx
@@ -104,6 +104,7 @@ export function InsertSubsequence(props: Props) {
             }}
             onBlur={() => setInputs(constrainInputs(inputs))}
             rows={9}
+            placeholder=' ...an RNA or DNA subsequence ..."CUGCCA"'
             style={{ marginTop: '4px' }}
           />
         </div>
