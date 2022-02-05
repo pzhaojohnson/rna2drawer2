a55bf9540d186aec45cfb6010355b3d2e70c83fc
Philip Johnson <p.zhao.johnson@gmail.com>
2022-02-05 12:26:15 -0500

    Add explanatory comments

--- src/draw/svg/interpretColor.ts
+++ src/draw/svg/interpretColor.ts
@@ -1,5 +1,7 @@
 import * as SVG from '@svgdotjs/svg.js';
 
+// uses the SVG.Color constructor to interpret the given value as a color
+// and returns undefined if the given value cannot be interpreted
 export function interpretColor(v: any): SVG.Color | undefined {
   try {
     return new SVG.Color(v);
--- src/draw/svg/interpretNumber.ts
+++ src/draw/svg/interpretNumber.ts
@@ -1,5 +1,7 @@
 import * as SVG from '@svgdotjs/svg.js';
 
+// uses the SVG.Number constructor to interpret the given value as a number
+// and returns undefined if the given value cannot be interpreted
 export function interpretNumber(v: any): SVG.Number | undefined {
   try {
     return new SVG.Number(v);
