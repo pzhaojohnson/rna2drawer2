353afd9700011c5e24c4ed0239777c19979be963
Philip Johnson <p.zhao.johnson@gmail.com>
2022-02-18 05:45:48 -0500

    Add stemsAreEqual function

---
+++ src/partners/stemsAreEqual.test.js
@@ -0,0 +1,28 @@
+import { stemsAreEqual } from './stemsAreEqual';
+
+test('stemsAreEqual function', () => {
+
+  // are equal
+  expect(stemsAreEqual(
+    { position5: 12, position3: 31, size: 6 },
+    { position5: 12, position3: 31, size: 6 },
+  )).toBeTruthy();
+
+  // different 5' most positions
+  expect(stemsAreEqual(
+    { position5: 12, position3: 31, size: 6 },
+    { position5: 11, position3: 31, size: 6 },
+  )).toBeFalsy();
+
+  // different 3' most positions
+  expect(stemsAreEqual(
+    { position5: 12, position3: 32, size: 6 },
+    { position5: 12, position3: 31, size: 6 },
+  )).toBeFalsy();
+
+  // different sizes
+  expect(stemsAreEqual(
+    { position5: 12, position3: 31, size: 6 },
+    { position5: 12, position3: 31, size: 5 },
+  )).toBeFalsy();
+});
---
+++ src/partners/stemsAreEqual.ts
@@ -0,0 +1,11 @@
+import { Stem } from 'Partners/Stem';
+import { bottomPair } from 'Partners/Stem';
+import { topPair } from 'Partners/Stem';
+import { pairsAreEqual } from 'Partners/pairsAreEqual';
+
+export function stemsAreEqual(stem1: Stem, stem2: Stem): boolean {
+  return (
+    pairsAreEqual(bottomPair(stem1), bottomPair(stem2))
+    && pairsAreEqual(topPair(stem1), topPair(stem2))
+  );
+}
