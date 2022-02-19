aa3ee9a37591f414e6f773db7ea46eaa432ad61a
Philip Johnson <p.zhao.johnson@gmail.com>
2022-02-19 16:24:57 -0500

    Add createLinker function

--- src/partners/Linker.test.js
+++ src/partners/Linker.test.js
@@ -1,5 +1,14 @@
+import { createLinker } from './Linker';
 import { upstreamBoundingPosition } from './Linker';
 import { downstreamBoundingPosition } from './Linker';
+
+test('createLinker function', () => {
+  expect(createLinker(
+    { upstreamBoundingPosition: 61, downstreamBoundingPosition: 88 }
+  )).toStrictEqual(
+    { boundingPosition5: 61, boundingPosition3: 88 }
+  );
+});
 
 test('upstreamBoundingPosition and downstreamBoundingPosition functions', () => {
   let linker = { boundingPosition5: 20, boundingPosition3: 74 };
--- src/partners/Linker.ts
+++ src/partners/Linker.ts
@@ -2,6 +2,19 @@ import { UnpairedRegion } from 'Partners/UnpairedRegion';
 
 export type Linker = UnpairedRegion;
 
+export type LinkerSpecification = (
+  { upstreamBoundingPosition: number, downstreamBoundingPosition: number }
+);
+
+// allows for linker objects to be specified in different ways
+// without knowledge of the underlying object structure
+export function createLinker(spec: LinkerSpecification): Linker {
+  return {
+    boundingPosition5: spec.upstreamBoundingPosition,
+    boundingPosition3: spec.downstreamBoundingPosition,
+  };
+}
+
 export function upstreamBoundingPosition(linker: Linker): number {
   return linker.boundingPosition5;
 }
