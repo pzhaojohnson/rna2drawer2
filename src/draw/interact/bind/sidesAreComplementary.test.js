e58845de77096983db7a0f8c7520ae8d2bd90e1f
Philip Johnson <p.zhao.johnson@gmail.com>
2022-03-16 14:54:45 -0400

    Make unit testing of sidesAreComplementary function more extensive and remove unused motifsAreComplementary function

--- src/draw/interact/bind/sidesAreComplementary.test.js
+++ src/draw/interact/bind/sidesAreComplementary.test.js
@@ -1,7 +1,6 @@
 import { Drawing } from 'Draw/Drawing';
 import * as SVG from 'Draw/svg/NodeSVG';
 import { appendSequence } from 'Draw/sequences/add/sequence';
-import { sideToMotif } from './sideToMotif';
 
 import { sidesAreComplementary } from './sidesAreComplementary';
 
@@ -24,21 +23,49 @@ afterEach(() => {
 });
 
 describe('sidesAreComplementary function', () => {
-  test('when the two sides are complementary', () => {
-    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAUUUUAAAAUUUU'});
+  test('perfect complements', () => {
+    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAAAUUUUUUGCGCGC' });
+
+    // sides with multiple bases
+    let side1 = sequence.bases.slice(1, 5);
+    let side2 = sequence.bases.slice(6, 10);
+    expect(sidesAreComplementary(side1, side2)).toBeTruthy();
+    expect(sidesAreComplementary(side2, side1)).toBeTruthy(); // switch order
+
+    // sides with one base each
+    side1 = [sequence.atPosition(13)];
+    side2 = [sequence.atPosition(16)];
+    expect(sidesAreComplementary(side1, side2)).toBeTruthy();
+    expect(sidesAreComplementary(side2, side1)).toBeTruthy(); // switch order
+  });
+
+  it('is not case-sensitive', () => {
+    let sequence = appendSequence(drawing, { id: 'Id', characters: 'AAAAuuuu' });
     let side1 = sequence.bases.slice(0, 3);
-    let side2 = sequence.bases.slice(5, 8);
+    let side2 = sequence.bases.slice(4, 7);
     expect(sidesAreComplementary(side1, side2)).toBeTruthy();
   });
 
-  test('when the two sides are not complementary', () => {
-    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAAAAAAAAAAAAA'});
+  test('non-complementary sides', () => {
+    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAAAAAAAAAAAAA' });
     let side1 = sequence.bases.slice(0, 3);
     let side2 = sequence.bases.slice(5, 8);
     expect(sidesAreComplementary(side1, side2)).toBeFalsy();
   });
 
-  test('when the two sides overlap', () => {
+  test('sides of different lengths', () => {
+    let sequence = appendSequence(drawing, { id: 'Id', characters: 'AAAAUUUUGGGGCCCC' });
+    let side1 = sequence.bases.slice(1, 3);
+    let side2 = [sequence.atPosition(6)];
+    expect(sidesAreComplementary(side1, side2)).toBeFalsy();
+    expect(sidesAreComplementary(side2, side1)).toBeFalsy(); // switch order
+  });
+
+  test('sides of length zero', () => {
+    expect(sidesAreComplementary([], [])).toBeTruthy();
+  });
+
+  test('overlapping sides', () => {
     let sequence = appendSequence(drawing, { id: 'GC', characters: 'GCGCGCGCGCGC' });
     let side1 = sequence.bases.slice(0, 4);
     // has a complementary motif but overlaps with side 1
@@ -46,23 +73,56 @@ describe('sidesAreComplementary function', () => {
     expect(sidesAreComplementary(side1, side2)).toBeFalsy();
   });
 
-  test('when either of the two sides cannot be converted to a motif string', () => {
+  test('when a base has no text content', () => {
     let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAUUUUAAAAUUUU'});
     let side1 = sequence.bases.slice(0, 3);
     let side2 = sequence.bases.slice(5, 8);
     expect(sidesAreComplementary(side1, side2)).toBeTruthy();
     expect(sidesAreComplementary(side2, side1)).toBeTruthy(); // switch order
     side1[1].text.text(''); // remove text content
-    expect(sideToMotif(side1)).toBeUndefined();
+    expect(side1[1].text.text()).toBe('');
     expect(sidesAreComplementary(side1, side2)).toBeFalsy();
     expect(sidesAreComplementary(side2, side1)).toBeFalsy(); // switch order
   });
 
-  it('passes options', () => {
-    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'RRRRUUUUAAAAUUUU'});
+  test('when the text content of a base has multiple characters', () => {
+    let sequence = appendSequence(drawing, { id: 'asdf', characters: 'AAAAUUUUAAAAUUUU'});
+    let side1 = sequence.bases.slice(0, 3);
+    let side2 = sequence.bases.slice(5, 8);
+    expect(sidesAreComplementary(side1, side2)).toBeTruthy();
+    expect(sidesAreComplementary(side2, side1)).toBeTruthy(); // switch order
+    side1[1].text.text('AA'); // make two characters
+    expect(side1[1].text.text()).toBe('AA');
+    expect(sidesAreComplementary(side1, side2)).toBeFalsy();
+    expect(sidesAreComplementary(side2, side1)).toBeFalsy(); // switch order
+  });
+
+  test('GUT option', () => {
+    let sequence = appendSequence(drawing, { id: 'GUT', characters: 'GGUUTT' });
+    let side1 = [sequence.atPosition(2)];
+    let side2 = [sequence.atPosition(4)];
+    let side3 = [sequence.atPosition(6)];
+    expect(sidesAreComplementary(side1, side2, { GUT: true })).toBeTruthy();
+    expect(sidesAreComplementary(side1, side3, { GUT: true })).toBeTruthy();
+    expect(sidesAreComplementary(side1, side2, { GUT: false })).toBeFalsy();
+    expect(sidesAreComplementary(side1, side3, { GUT: false })).toBeFalsy();
+  });
+
+  test('IUPAC option', () => {
+    let sequence = appendSequence(drawing, { id: 'IUPAC', characters: 'RRRRUUUUAAAAUUUU'});
     let side1 = sequence.bases.slice(0, 3);
     let side2 = sequence.bases.slice(5, 8);
     expect(sidesAreComplementary(side1, side2, { IUPAC: true })).toBeTruthy();
     expect(sidesAreComplementary(side1, side2, { IUPAC: false })).toBeFalsy();
+  });
+
+  test('mismatchesAllowed option', () => {
+    let sequence = appendSequence(drawing, { id: 'Mis', characters: 'AAGGCCaaaaGGCCUU' });
+    let side1 = sequence.bases.slice(2, 8);
+    let side2 = sequence.bases.slice(8, 14);
+    expect(sidesAreComplementary(side1, side2, { mismatchesAllowed: 0 })).toBeFalsy();
+    expect(sidesAreComplementary(side1, side2, { mismatchesAllowed: 1 })).toBeFalsy();
+    expect(sidesAreComplementary(side1, side2, { mismatchesAllowed: 2 })).toBeTruthy();
+    expect(sidesAreComplementary(side1, side2, { mismatchesAllowed: 3 })).toBeTruthy();
   });
 });
--- src/draw/interact/bind/sidesAreComplementary.ts
+++ src/draw/interact/bind/sidesAreComplementary.ts
@@ -1,10 +1,24 @@
 import { Side } from './Side';
 import { sidesOverlap } from './Side';
 import { charactersAreComplementary } from './charactersAreComplementary';
-import { Options } from './motifsAreComplementary';
 
-export { Options };
+export type Options = {
 
+  // whether to consider Gs complementary with Us and Ts
+  GUT?: boolean;
+
+  // whether to allow complements based on IUPAC single letter codes
+  IUPAC?: boolean;
+
+  // the number of mismatched characters allowed before two motifs are no longer
+  // considered complementary
+  // (interpreted as zero if left unspecified)
+  mismatchesAllowed?: number;
+};
+
+/**
+ * It is vacuously true that two sides both of length zero are complementary.
+ */
 export function sidesAreComplementary(side1: Side, side2: Side, options?: Options): boolean {
   let GUT = options?.GUT;
   let IUPAC = options?.IUPAC;
@@ -14,6 +28,8 @@ export function sidesAreComplementary(side1: Side, side2: Side, options?: Option
     return true;
   } else if (side1.length != side2.length) {
     return false;
+  } else if (sidesOverlap(side1, side2)) {
+    return false;
   }
 
   let mismatches = 0;
@@ -21,7 +37,9 @@ export function sidesAreComplementary(side1: Side, side2: Side, options?: Option
   while (i < side1.length && mismatches <= mismatchesAllowed) {
     let c1 = side1[i].text.text().toUpperCase();
     let c2 = side2[side2.length - i - 1].text.text().toUpperCase();
-    if (!charactersAreComplementary(c1, c2, { GUT, IUPAC })) {
+    if (c1.length != 1 || c2.length != 1) {
+      mismatches++;
+    } else if (!charactersAreComplementary(c1, c2, { GUT, IUPAC })) {
       mismatches++;
     }
     i++;
