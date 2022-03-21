5bfa6926cd94627092415896d394451c660a4c18
Philip Johnson <p.zhao.johnson@gmail.com>
2022-03-20 21:21:28 -0400

    Add ToolControls component

--- src/infobar/Infobar.tsx
+++ src/infobar/Infobar.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import styles from './Infobar.css';
-import { useState } from 'react';
 import { AppInterface as App } from 'AppInterface';
-import { ToolSelect } from 'Infobar/tools/ToolSelect';
-import { BindingToolControls } from 'Infobar/tools/bind/BindingToolControls';
+import { ToolControls } from 'Infobar/tools/ToolControls';
 import { ZoomAdjust } from './zoom/ZoomAdjust';
 
 export type Props = {
@@ -11,26 +9,10 @@ export type Props = {
 }
 
 export function Infobar(props: Props) {
-  let [toolSelectIsOpen, setToolSelectIsOpen] = useState(false);
-
-  let strictDrawingInteraction = props.app.strictDrawingInteraction;
-  let currentTool = strictDrawingInteraction.currentTool;
-  let bindingTool = strictDrawingInteraction.bindingTool;
-
   return props.app.strictDrawing.isEmpty() ? null : (
     <div className={styles.infobar} >
       <div style={{ width: '6px' }} />
-      <ToolSelect
-        app={props.app}
-        onOpen={() => setToolSelectIsOpen(true)}
-        onClose={() => setToolSelectIsOpen(false)}
-      />
-      {toolSelectIsOpen || currentTool != bindingTool ? null : (
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-          <div style={{ width: '8px' }} />
-          <BindingToolControls app={props.app} />
-        </div>
-      )}
+      <ToolControls app={props.app} />
       <div className={styles.spacer} />
       <ZoomAdjust app={props.app} />
     </div>
---
+++ src/infobar/tools/ToolControls.tsx
@@ -0,0 +1,43 @@
+import { AppInterface as App } from 'AppInterface';
+
+import * as React from 'react';
+import { useState } from 'react';
+import { ToolSelect } from 'Infobar/tools/ToolSelect';
+import { BindingToolControls } from 'Infobar/tools/bind/BindingToolControls';
+
+export type Props = {
+
+  // a reference to the whole app
+  app: App;
+}
+
+export function ToolControls(props: Props) {
+  let [toolSelectIsOpen, setToolSelectIsOpen] = useState(false);
+
+  let strictDrawingInteraction = props.app.strictDrawingInteraction;
+  let currentTool = strictDrawingInteraction.currentTool;
+  let bindingTool = strictDrawingInteraction.bindingTool;
+
+  return (
+    <div
+      style={{
+        alignSelf: 'stretch',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
+      <ToolSelect
+        app={props.app}
+        onOpen={() => setToolSelectIsOpen(true)}
+        onClose={() => setToolSelectIsOpen(false)}
+      />
+      {toolSelectIsOpen || currentTool != bindingTool ? null : (
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
+          <div style={{ width: '8px' }} />
+          <BindingToolControls app={props.app} />
+        </div>
+      )}
+    </div>
+  );
+}
