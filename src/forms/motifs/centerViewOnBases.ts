c248902b6ad6ff7bc38c620e07e49cdf8533c9e2
Philip Johnson <p.zhao.johnson@gmail.com>
2022-03-22 14:41:14 -0400

    Use client coordinates to center view on bases

--- src/forms/motifs/centerViewOnBases.ts
+++ src/forms/motifs/centerViewOnBases.ts
@@ -1,7 +1,22 @@
 import { StrictDrawingInterface as StrictDrawing } from 'Draw/strict/StrictDrawingInterface';
 import { BaseInterface as Base } from 'Draw/bases/BaseInterface';
+import { Point2D as Point } from 'Math/points/Point';
 import { centroid2D as centroid } from 'Math/points/centroid';
-import { centerViewOn } from 'Draw/view';
+import { displacement2D as displacement } from 'Math/points/displacement';
+
+type Rect = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
+
+function centerOfRect(rect: Rect): Point {
+  return {
+    x: rect.x + (rect.width / 2),
+    y: rect.y + (rect.height / 2),
+  };
+}
 
 /**
  * Does nothing for an empty array of bases.
@@ -11,6 +26,14 @@ export function centerViewOnBases(strictDrawing: StrictDrawing, bases: Base[]) {
     return;
   }
 
-  let p = centroid(bases.map(base => base.center()));
-  centerViewOn(strictDrawing.drawing, p);
+  let textClientRects = bases.map(base => base.text.node.getBoundingClientRect());
+  let textClientCenters = textClientRects.map(rect => centerOfRect(rect));
+  let textClientCentroid = centroid(textClientCenters);
+
+  let drawingClientRect = strictDrawing.drawing.svgContainer.getBoundingClientRect();
+  let drawingClientCenter = centerOfRect(drawingClientRect);
+
+  let shift = displacement(drawingClientCenter, textClientCentroid);
+  strictDrawing.drawing.svgContainer.scrollLeft += shift.x;
+  strictDrawing.drawing.svgContainer.scrollTop += shift.y;
 }
