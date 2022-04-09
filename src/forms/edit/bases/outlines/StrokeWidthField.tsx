7490573a1cb5d006e3c2bfac479b711dc9a94f93
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-09 14:30:31 -0400

    Condense code for base outlines StrokeWidthField

--- src/forms/edit/bases/outlines/StrokeWidthField.tsx
+++ src/forms/edit/bases/outlines/StrokeWidthField.tsx
@@ -1,69 +1,39 @@
-import * as React from 'react';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
 import type { App } from 'App';
 import { CircleBaseAnnotation } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
-import { interpretNumber } from 'Draw/svg/interpretNumber';
-import { round } from 'Math/round';
+
+import * as SVG from '@svgdotjs/svg.js';
+import { strokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
+import { setStrokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
+import { numberToDisplayableString as displayableString } from 'Forms/inputs/numbers/numberToDisplayableString';
+import { isBlank } from 'Parse/isBlank';
+
+import * as React from 'react';
+import textFieldStyles from 'Forms/inputs/text/TextField.css';
+
+// returns the circle elements of the outlines
+function circles(outlines: CircleBaseAnnotation[]): SVG.Circle[] {
+  return outlines.map(outline => outline.circle);
+}
 
 export type Props = {
+
+  // a reference to the whole app
   app: App;
 
   // the outlines to edit
   outlines: CircleBaseAnnotation[];
 }
 
-type Value = string;
-
-type State = {
-  value: Value;
-}
-
-// returns an empty string value for an empty outlines array
-// or if not all outlines have the same stroke width
-function currStrokeWidth(outlines: CircleBaseAnnotation[]): Value {
-  let sws = new Set<Value>();
-  outlines.forEach(o => {
-    let sw = o.circle.attr('stroke-width');
-    let n = interpretNumber(sw);
-    if (n) {
-      let pxs = n.convert('px').valueOf();
-      pxs = round(pxs, 2);
-      sws.add(pxs.toString());
-    }
-  });
-  if (sws.size == 1) {
-    return sws.values().next().value;
-  } else {
-    return '';
-  }
-}
-
-function isBlank(v: Value): boolean {
-  return v.trim().length == 0;
-}
-
-function areEqual(v1: Value, v2: Value): boolean {
-  return Number.parseFloat(v1) == Number.parseFloat(v2);
-}
-
-function constrainStrokeWidth(sw: number): number {
-  if (!Number.isFinite(sw)) {
-    return 1;
-  } else if (sw < 0) {
-    return 0;
-  } else {
-    return sw;
-  }
-}
-
 export class StrokeWidthField extends React.Component<Props> {
-  state: State;
+  state: {
+    value: string;
+  };
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      value: currStrokeWidth(props.outlines),
+      value: displayableString(strokeWidth(circles(props.outlines)), { places: 2 }),
     };
   }
 
@@ -97,19 +67,20 @@ export class StrokeWidthField extends React.Component<Props> {
   }
 
   submit() {
-    if (!isBlank(this.state.value)) {
-      let sw = Number.parseFloat(this.state.value);
-      if (Number.isFinite(sw)) {
-        if (!areEqual(this.state.value, currStrokeWidth(this.props.outlines))) {
-          this.props.app.pushUndo();
-          sw = constrainStrokeWidth(sw);
-          this.props.outlines.forEach(o => {
-            o.circle.attr({ 'stroke-width': sw });
-          });
-          CircleBaseAnnotation.recommendedDefaults.circle['stroke-width'] = sw;
-          this.props.app.refresh();
-        }
-      }
+    if (isBlank(this.state.value)) {
+      return;
     }
+
+    let n = Number.parseFloat(this.state.value);
+    if (!Number.isFinite(n)) {
+      return;
+    } else if (n == strokeWidth(circles(this.props.outlines))) {
+      return;
+    }
+
+    this.props.app.pushUndo();
+    setStrokeWidth(circles(this.props.outlines), n);
+    CircleBaseAnnotation.recommendedDefaults.circle['stroke-width'] = n;
+    this.props.app.refresh();
   }
 }
