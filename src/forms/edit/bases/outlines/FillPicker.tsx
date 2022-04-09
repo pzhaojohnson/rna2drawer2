e64338efd1026c9ebf3583f9773efdf4e94bc83a
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-09 12:25:03 -0400

    Condense code for base outlines FillPicker

--- src/forms/edit/bases/outlines/FillPicker.tsx
+++ src/forms/edit/bases/outlines/FillPicker.tsx
@@ -1,63 +1,43 @@
-import * as React from 'react';
-import { ColorPicker, Value } from 'Forms/inputs/color/ColorPicker';
 import type { App } from 'App';
 import { CircleBaseAnnotation } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
-import { interpretColor } from 'Draw/svg/interpretColor';
+
+import * as SVG from '@svgdotjs/svg.js';
+import { fill } from 'Forms/inputs/svg/fill/fill';
+import { fillEquals } from 'Forms/inputs/svg/fill/fill';
+import { setFill } from 'Forms/inputs/svg/fill/fill';
+
+import * as React from 'react';
+import { ColorPicker } from 'Forms/inputs/color/ColorPicker';
+
+// returns the circle elements of the outlines
+function circles(outlines: CircleBaseAnnotation[]): SVG.Circle[] {
+  return outlines.map(outline => outline.circle);
+}
 
 export type Props = {
+
+  // a reference to the whole app
   app: App;
 
   // the outlines to edit
   outlines: CircleBaseAnnotation[];
 }
 
-// returns undefined for an empty outlines array
-// or if not all outlines have the same fill
-function currFill(outlines: CircleBaseAnnotation[]): Value | undefined {
-  let hexs = new Set<string>();
-  outlines.forEach(o => {
-    let c = interpretColor(o.circle.attr('fill'));
-    if (c) {
-      hexs.add(c.toHex().toLowerCase());
-    }
-  });
-  if (hexs.size == 1) {
-    let hex = hexs.values().next().value;
-    let c = interpretColor(hex);
-    if (c) {
-      return { color: c };
-    }
-  }
-}
-
-function areEqual(v1?: Value, v2?: Value): boolean {
-  if (v1 && v2) {
-    return (
-      v1.color.toHex().toLowerCase() == v2.color.toHex().toLowerCase()
-      && v1.alpha == v2.alpha
-    );
-  } else {
-    return v1 == v2;
-  }
-}
-
 export function FillPicker(props: Props) {
   return (
     <ColorPicker
-      value={currFill(props.outlines)}
+      value={fill(circles(props.outlines))}
       onClose={event => {
-        if (event.target.value) {
-          let value = event.target.value;
-          if (!areEqual(value, currFill(props.outlines))) {
-            props.app.pushUndo();
-            let hex = value.color.toHex();
-            props.outlines.forEach(o => {
-              o.circle.attr({ 'fill': hex });
-            });
-            CircleBaseAnnotation.recommendedDefaults.circle['fill'] = hex;
-            props.app.refresh();
-          }
+        if (!event.target.value) {
+          return;
+        } else if (fillEquals(circles(props.outlines), event.target.value.color)) {
+          return;
         }
+
+        props.app.pushUndo();
+        setFill(circles(props.outlines), event.target.value.color);
+        CircleBaseAnnotation.recommendedDefaults.circle['fill'] = event.target.value.color.toHex();
+        props.app.refresh();
       }}
       disableAlpha={true}
     />
