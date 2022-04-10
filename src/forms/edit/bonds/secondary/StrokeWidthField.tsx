5d9fc8b76887d20407043a750941e5ef73163e63
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-10 07:47:34 -0400

    Condense code for secondary bonds StrokeWidthField

--- src/forms/edit/bonds/secondary/StrokeWidthField.tsx
+++ src/forms/edit/bonds/secondary/StrokeWidthField.tsx
@@ -1,70 +1,40 @@
-import * as React from 'react';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
 import type { App } from 'App';
 import { SecondaryBond } from 'Draw/bonds/straight/SecondaryBond';
 import { secondaryBondTypes } from 'Draw/bonds/straight/SecondaryBond';
-import { interpretNumber } from 'Draw/svg/interpretNumber';
-import { round } from 'Math/round';
+
+import * as SVG from '@svgdotjs/svg.js';
+import { strokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
+import { setStrokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
+import { numberToDisplayableString as displayableString } from 'Forms/inputs/numbers/numberToDisplayableString';
+import { isBlank } from 'Parse/isBlank';
+
+import * as React from 'react';
+import textFieldStyles from 'Forms/inputs/text/TextField.css';
+
+// returns the line elements of the secondary bonds
+function lines(secondaryBonds: SecondaryBond[]): SVG.Line[] {
+  return secondaryBonds.map(bond => bond.line);
+}
 
 export type Props = {
+
+  // a reference to the whole app
   app: App;
 
   // the secondary bonds to edit
   secondaryBonds: SecondaryBond[];
 }
 
-type Value = string;
-
-type State = {
-  value: Value;
-}
-
-// returns an empty string value for an empty secondary bonds array
-// or if not all secondary bonds have the same stroke width
-function currStrokeWidth(secondaryBonds: SecondaryBond[]): Value {
-  let sws = new Set<Value>();
-  secondaryBonds.forEach(sb => {
-    let sw = sb.line.attr('stroke-width');
-    let n = interpretNumber(sw);
-    if (n) {
-      let pxs = n.convert('px').valueOf();
-      pxs = round(pxs, 2);
-      sws.add(pxs.toString());
-    }
-  });
-  if (sws.size == 1) {
-    return sws.values().next().value;
-  } else {
-    return '';
-  }
-}
-
-function isBlank(v: Value): boolean {
-  return v.trim().length == 0;
-}
-
-function areEqual(v1: Value, v2: Value): boolean {
-  return Number.parseFloat(v1) == Number.parseFloat(v2);
-}
-
-function constrainStrokeWidth(sw: number): number {
-  if (!Number.isFinite(sw)) {
-    return 1;
-  } else if (sw < 0) {
-    return 0;
-  } else {
-    return sw;
-  }
-}
-
 export class StrokeWidthField extends React.Component<Props> {
-  state: State;
+  state: {
+    value: string;
+  };
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
-      value: currStrokeWidth(props.secondaryBonds),
+      value: displayableString(strokeWidth(lines(props.secondaryBonds)), { places: 2 }),
     }
   }
 
@@ -99,22 +69,28 @@ export class StrokeWidthField extends React.Component<Props> {
   }
 
   submit() {
-    if (!isBlank(this.state.value)) {
-      let sw = Number.parseFloat(this.state.value);
-      if (Number.isFinite(sw)) {
-        if (!areEqual(this.state.value, currStrokeWidth(this.props.secondaryBonds))) {
-          this.props.app.pushUndo();
-          sw = constrainStrokeWidth(sw);
-          sw = round(sw, 2);
-          this.props.secondaryBonds.forEach(sb => {
-            sb.line.attr({ 'stroke-width': sw });
-          });
-          secondaryBondTypes.forEach(t => {
-            SecondaryBond.recommendedDefaults[t].line['stroke-width'] = sw;
-          });
-          this.props.app.refresh();
-        }
-      }
+    if (isBlank(this.state.value)) {
+      return;
     }
+
+    let value = Number.parseFloat(this.state.value);
+    if (!Number.isFinite(value)) {
+      return;
+    } else if (value == strokeWidth(lines(this.props.secondaryBonds))) {
+      return;
+    }
+
+    this.props.app.pushUndo();
+    setStrokeWidth(lines(this.props.secondaryBonds), value);
+
+    // may be different from the value that was specified
+    let constrainedValue = strokeWidth(lines(this.props.secondaryBonds));
+
+    secondaryBondTypes.forEach(t => {
+      SecondaryBond.recommendedDefaults[t].line['stroke-width'] = (
+        constrainedValue
+        ?? SecondaryBond.recommendedDefaults[t].line['stroke-width']
+      );
+    });
   }
 }
