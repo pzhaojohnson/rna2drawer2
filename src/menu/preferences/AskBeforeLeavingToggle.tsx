ca80ff1031126a2be7a51592eed67f4109bd3bb7
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-20 20:56:46 -0400

    Add check to toggled AskBeforeLeavingToggle

--- src/menu/preferences/AskBeforeLeavingToggle.tsx
+++ src/menu/preferences/AskBeforeLeavingToggle.tsx
@@ -2,6 +2,26 @@ import * as React from 'react';
 import styles from './AskBeforeLeavingToggle.css';
 import type { App } from 'App';
 
+function Check() {
+  return (
+    <svg
+      className={styles.check}
+      width="10.5px"
+      height="10.5px"
+      viewBox="0 0 96 96"
+      xmlns="http://www.w3.org/2000/svg"
+      overflow="hidden"
+    >
+      <path
+        d="M86.1 15.8 34.9 64.2 10.3 39 1.8 47.1 34.5 80.7 43.1 72.7 94.2 24.2Z"
+        stroke="#FFFFFF"
+        strokeWidth="3.66667"
+        fill="#FFFFFF"
+      />
+    </svg>
+  );
+}
+
 export type Props = {
   app: App;
 }
@@ -17,10 +37,12 @@ export class AskBeforeLeavingToggle extends React.Component<Props> {
         `}
         onClick={() => this.handleClick()}
       >
-        <div className={`${styles.textContainer}`} >
+        <div className={`${styles.pillContainer}`} >
           <p className={`${styles.text}`} >
             Ask Before Leaving
           </p>
+          <div className={styles.spacer} />
+          <Check />
         </div>
       </div>
     );
