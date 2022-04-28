e64a708e624fdb76f9c12ad5e30297010a28e162
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-28 05:53:33 -0400

    Use TextInputField, ErrorMessage and DottedNote components in EditBasesByCharacterForm

--- src/forms/edit/bases/by/character/EditBasesByCharacterForm.tsx
+++ src/forms/edit/bases/by/character/EditBasesByCharacterForm.tsx
@@ -1,12 +1,52 @@
-import * as React from 'react';
-import { useState } from 'react';
-import { PartialWidthContainer } from 'Forms/containers/PartialWidthContainer';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
-import errorMessageStyles from 'Forms/ErrorMessage.css';
-import { FormHistoryInterface } from 'Forms/history/FormHistoryInterface';
-import { SolidButton } from 'Forms/buttons/SolidButton';
 import type { App } from 'App';
 import { Base } from 'Draw/bases/Base';
+
+import * as React from 'react';
+import { useState } from 'react';
+
+import { PartialWidthContainer } from 'Forms/containers/PartialWidthContainer';
+import { FormHistoryInterface } from 'Forms/history/FormHistoryInterface';
+
+import { TextInputField } from 'Forms/inputs/text/TextInputField';
+
+import { SolidButton } from 'Forms/buttons/SolidButton';
+import { ErrorMessage } from 'Forms/ErrorMessage';
+
+import { DottedNote } from 'Forms/notes/DottedNote';
+
+function CharacterField(
+  props: {
+    value: string,
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    onBlur: (event: React.FocusEvent<HTMLInputElement>) => void,
+    onKeyUp: (event: React.KeyboardEvent<HTMLInputElement>) => void,
+  },
+) {
+  return (
+    <TextInputField
+      label='Character'
+      value={props.value}
+      onChange={props.onChange}
+      onBlur={props.onBlur}
+      onKeyUp={props.onKeyUp}
+      input={{ style: { width: '12px', textAlign: 'center' } }}
+    />
+  );
+}
+
+function SubmitButton(
+  props: {
+    onClick: () => void,
+  },
+) {
+  return (
+    <SolidButton
+      text='Select'
+      onClick={props.onClick}
+      style={{ marginTop: '24px' }}
+    />
+  );
+}
 
 export type Props = {
   app: App;
@@ -43,86 +83,57 @@ export function EditBasesByCharacterForm(props: Props) {
       title='Bases by Character'
       style={{ width: '348px' }}
     >
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-        <input
-          type='text'
-          className={textFieldStyles.input}
-          value={character}
-          onChange={event => {
-            if (event.target.value.trim() != character.trim()) {
-              setErrorMessage(new String(''));
-            }
-            setCharacter(event.target.value);
-          }}
-          onBlur={() => setCharacter(constrainCharacter(character))}
-          onKeyUp={event => {
-            if (event.key.toLowerCase() == 'enter') {
-              setCharacter(constrainCharacter(character));
-            }
-          }}
-          style={{ width: '12px', textAlign: 'center' }}
-        />
-        <div style={{ marginLeft: '8px' }} >
-          <p className={`${textFieldStyles.label} unselectable`} >
-            Character
-          </p>
-        </div>
-      </div>
-      <div style={{ marginTop: '24px' }} >
-        <SolidButton
-          text='Select'
-          onClick={() => {
-            if (character.length == 0) {
-              setErrorMessage(new String('Specify a character.'));
-              return;
-            }
+      <CharacterField
+        value={character}
+        onChange={event => {
+          if (event.target.value.trim() != character.trim()) {
+            setErrorMessage(new String(''));
+          }
+          setCharacter(event.target.value);
+        }}
+        onBlur={() => setCharacter(constrainCharacter(character))}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
+            setCharacter(constrainCharacter(character));
+          }
+        }}
+      />
+      <SubmitButton
+        onClick={() => {
+          if (character.length == 0) {
+            setErrorMessage(new String('Specify a character.'));
+            return;
+          }
 
-            let drawing = props.app.strictDrawing.drawing;
-            let bases = drawing.bases().filter(b => b.text.text() == character);
+          let drawing = props.app.strictDrawing.drawing;
+          let bases = drawing.bases().filter(b => b.text.text() == character);
 
-            if (bases.length == 0) {
-              setErrorMessage(new String('No bases have the entered character.'));
-              return;
-            }
+          if (bases.length == 0) {
+            setErrorMessage(new String('No bases have the entered character.'));
+            return;
+          }
 
-            props.unmount();
-            let drawingInteraction = props.app.strictDrawingInteraction;
-            drawingInteraction.currentTool = drawingInteraction.editingTool;
-            drawingInteraction.editingTool.editingType = Base;
-            drawingInteraction.editingTool.select(bases);
-            drawingInteraction.editingTool.renderForm();
+          props.unmount();
+          let drawingInteraction = props.app.strictDrawingInteraction;
+          drawingInteraction.currentTool = drawingInteraction.editingTool;
+          drawingInteraction.editingTool.editingType = Base;
+          drawingInteraction.editingTool.select(bases);
+          drawingInteraction.editingTool.renderForm();
 
-            prevCharacter = character;
-          }}
-        />
-      </div>
+          prevCharacter = character;
+        }}
+      />
       {!errorMessage.valueOf() ? null : (
-        <p
-          key={Math.random()}
-          className={`${errorMessageStyles.errorMessage} ${errorMessageStyles.fadesIn} unselectable`}
-          style={{ marginTop: '8px' }}
-        >
+        <ErrorMessage style={{ marginTop: '8px' }} >
           {errorMessage.valueOf()}
-        </p>
+        </ErrorMessage>
       )}
-      <div style={{ marginTop: '16px' }} >
-        <p
-          className='unselectable'
-          style={{ fontSize: '12px', color: 'rgba(0,0,0,0.95)' }}
-        >
-          <span style={{ fontWeight: 600, color: 'rgba(0,0,0,1)' }} >Note:&nbsp;</span>
-          Bases with the entered character will be selected and may then be edited.
-        </p>
-      </div>
-      <div style={{ marginTop: '6px' }} >
-        <p
-          className='unselectable'
-          style={{ fontSize: '12px', color: 'rgba(0,0,0,0.95)' }}
-        >
-          <span style={{ fontWeight: 600, color: 'rgba(0,0,0,1)' }} >Note:&nbsp;</span>
-          Is case-sensitive.
-        </p>
-      </div>
+      <DottedNote style={{ marginTop: '16px' }} >
+        Bases with the entered character will be selected and may then be edited.
+      </DottedNote>
+      <DottedNote style={{ marginTop: '12px' }} >
+        Is case-sensitive.
+      </DottedNote>
     </PartialWidthContainer>
   );
 }
