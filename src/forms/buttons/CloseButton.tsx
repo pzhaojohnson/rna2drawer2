64efb30385face74571781eb82cf0938467c2eba
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-30 10:19:39 -0400

    Use CSS to control CloseButton appearance on hover

--- src/forms/buttons/CloseButton.tsx
+++ src/forms/buttons/CloseButton.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
+import styles from './CloseButton.css';
 
-function CrossMark(
-  props: {
-    color: string,
-  },
-) {
+function CrossMark() {
   return (
-    <svg width="16px" height="16px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg" overflow="hidden" >
+    <svg
+      width="16px" height="16px" viewBox="0 0 96 96" overflow="hidden"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <defs>
         <clipPath id="clip0" >
           <rect x="592" y="312" width="96" height="96" />
@@ -14,8 +14,9 @@ function CrossMark(
       </defs>
       <g clipPath="url(#clip0)" transform="translate(-592 -312)" >
         <path
+          className={styles.crossMark}
           d="M608.707 391.707 639 361.414 670.293 392.707 671.707 391.293 640.414 360 671.707 328.707 670.293 327.293 639 358.586 608.707 328.293 607.293 329.707 637.586 360 607.293 390.293 608.707 391.707Z"
-          stroke={props.color} strokeWidth="0.333333" fill={props.color}
+          strokeWidth="0.333333"
         />
       </g>
     </svg>
@@ -27,43 +28,14 @@ interface Props {
 }
 
 export class CloseButton extends React.Component<Props> {
-  state: {
-    isHovered: boolean;
-  }
-
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      isHovered: false,
-    };
-  }
-
-  render(): React.ReactElement {
+  render() {
     return (
       <div
-        onMouseEnter={() => this.onMouseEnter()}
-        onMouseLeave={() => this.onMouseLeave()}
+        className={styles.closeButton}
         onClick={this.props.onClick}
-        style={{
-          width: '24px',
-          height: '24px',
-          backgroundColor: this.state.isHovered ? 'rgb(238,23,23)' : 'transparent',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
       >
-        <CrossMark color={this.state.isHovered ? '#ffffff' : '#2a2a2a'} />
+        <CrossMark />
       </div>
     );
-  }
-
-  onMouseEnter() {
-    this.setState({ isHovered: true });
-  }
-
-  onMouseLeave() {
-    this.setState({ isHovered: false });
   }
 }
