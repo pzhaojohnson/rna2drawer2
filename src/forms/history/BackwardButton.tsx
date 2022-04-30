1d402a45aecc1a150a88256fa195c473a2e05091
Philip Johnson <p.zhao.johnson@gmail.com>
2022-04-30 11:19:10 -0400

    Use JSX to express backward and forward icons and CSS to control enabled and disabled appearances

--- src/forms/history/BackwardButton.tsx
+++ src/forms/history/BackwardButton.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import styles from './BackwardButton.css';
 import { FormHistoryInterface as Props } from 'Forms/history/FormHistoryInterface';
-import darkBackwardIcon from './darkBackwardIcon.svg';
-import lightBackwardIcon from './lightBackwardIcon.svg';
+
+function BackwardIcon() {
+  return (
+    <svg width="16px" height="16px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg" overflow="hidden" >
+      <defs>
+        <clipPath id="clip0" >
+          <rect x="592" y="312" width="96" height="96" />
+        </clipPath>
+      </defs>
+      <g clipPath="url(#clip0)" transform="translate(-592 -312)" >
+        <path
+          className={styles.backwardIcon}
+          d="M651.947 388.191 623.697 359.935 651.947 331.684 656.19 335.926 632.181 359.935 656.191 383.949 651.947 388.191Z"
+          strokeWidth="0.666667"
+        />
+      </g>
+    </svg>
+  );
+}
 
 export function BackwardButton(props: Props) {
   return (
@@ -18,11 +35,7 @@ export function BackwardButton(props: Props) {
       }}
     >
       <div className={`${styles.backwardIconContainer}`} >
-        <img
-          className={`${styles.backwardIcon} unselectable`}
-          src={props.canGoBackward() ? darkBackwardIcon : lightBackwardIcon}
-          alt='Backward Icon'
-        />
+        <BackwardIcon />
       </div>
     </div>
   );
--- src/forms/history/ForwardButton.tsx
+++ src/forms/history/ForwardButton.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import styles from './ForwardButton.css';
 import { FormHistoryInterface as Props } from 'Forms/history/FormHistoryInterface';
-import darkForwardIcon from './darkForwardIcon.svg';
-import lightForwardIcon from './lightForwardIcon.svg';
+
+function ForwardIcon() {
+  return (
+    <svg width="16px" height="16px" viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg" overflow="hidden" >
+      <defs>
+        <clipPath id="clip0" >
+          <rect x="592" y="312" width="96" height="96" />
+        </clipPath>
+      </defs>
+      <g clipPath="url(#clip0)" transform="translate(-592 -312)" >
+        <path
+          className={styles.forwardIcon}
+          d="M628.06 388.187 623.817 383.945 647.825 359.937 623.816 335.922 628.059 331.68 656.31 359.937 628.06 388.187Z"
+          strokeWidth="0.666667"
+        />
+      </g>
+    </svg>
+  );
+}
 
 export function ForwardButton(props: Props) {
   return (
@@ -18,11 +35,7 @@ export function ForwardButton(props: Props) {
       }}
     >
       <div className={`${styles.forwardIconContainer}`} >
-        <img
-          className={`${styles.forwardIcon} unselectable`}
-          src={props.canGoForward() ? darkForwardIcon : lightForwardIcon}
-          alt='Forward Icon'
-        />
+        <ForwardIcon />
       </div>
     </div>
   );
