3a1d2f2b123f6641c75b3d70e8c9c9266fec5098
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-06 19:59:30 -0400

    Remove unused fontSize and color props from TextButton component

--- src/forms/buttons/TextButton.tsx
+++ src/forms/buttons/TextButton.tsx
@@ -4,8 +4,6 @@ import styles from './TextButton.css';
 interface Props {
   text: string;
   onClick: () => void;
-  fontSize?: string;
-  color?: string;
 }
 
 export function TextButton(props: Props) {
@@ -13,10 +11,6 @@ export function TextButton(props: Props) {
     <p
       className={styles.textButton}
       onClick={props.onClick}
-      style={{
-        fontSize: props.fontSize,
-        color: props.color,
-      }}
     >
       {props.text}
     </p>
