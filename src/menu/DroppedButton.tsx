d15c48c136391310e57823323736c34567710585
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-08 11:13:05 -0400

    Express dropped menu button check mark SVG images in JSX

--- src/menu/DroppedButton.tsx
+++ src/menu/DroppedButton.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import styles from './DroppedButton.css';
-import checkMark from './checkMark.svg';
 
 function CheckMark() {
   return (
-    <img
-      src={checkMark}
-      alt='Check Mark'
+    <svg
+      viewBox="0 0 96 96" xmlns="http://www.w3.org/2000/svg" overflow="hidden"
       style={{ height: '16px' }}
-    />
+    >
+      <path d="M86.1 15.8 34.9 64.2 10.3 39 1.8 47.1 34.5 80.7 43.1 72.7 94.2 24.2Z"
+        fill="#333333"
+      />
+    </svg>
   );
 }
 
