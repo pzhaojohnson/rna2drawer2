124ac491850212fcee6761b85a60cd0fc662cd6d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-13 17:14:57 -0400

    Express drawings in drawing slideshow as SVG strings instead of SVG images to reduce number of emitted assets in build

--- src/forms/welcome/drawingSlideshow/DrawingSlideshow.tsx
+++ src/forms/welcome/drawingSlideshow/DrawingSlideshow.tsx
@@ -3,28 +3,43 @@ import { useState, useEffect } from 'react';
 import styles from './DrawingSlideshow.css';
 import { v1 as uuidv1 } from 'uuid';
 
-import drawing1 from './drawing1.svg';
-import drawing2 from './drawing2.svg';
-import drawing3 from './drawing3.svg';
-import drawing4 from './drawing4.svg';
-import drawing5 from './drawing5.svg';
-import drawing6 from './drawing6.svg';
-import drawing7 from './drawing7.svg';
-import drawing8 from './drawing8.svg';
+import { drawingString1 } from './drawing1';
+import { drawingString2 } from './drawing2';
+import { drawingString3 } from './drawing3';
+import { drawingString4 } from './drawing4';
+import { drawingString5 } from './drawing5';
+import { drawingString6 } from './drawing6';
+import { drawingString7 } from './drawing7';
+import { drawingString8 } from './drawing8';
 
-const drawings = [
-  drawing1,
-  drawing2,
-  drawing3,
-  drawing4,
-  drawing5,
-  drawing6,
-  drawing7,
-  drawing8,
+let drawingStrings = [
+  drawingString1,
+  drawingString2,
+  drawingString3,
+  drawingString4,
+  drawingString5,
+  drawingString6,
+  drawingString7,
+  drawingString8,
 ];
 
+// necessary for unit testing with Jest
+if (URL.createObjectURL == undefined) {
+  console.error('URL.createObjectURL static method is undefined.');
+  Object.defineProperty(URL, 'createObjectURL', { value: () => {} });
+  console.error('Placeholder function assigned to URL.createObjectURL.');
+}
+
+let drawingURLs = (
+  drawingStrings.map(drawingString => (
+    URL.createObjectURL(
+      new Blob([drawingString], { type: 'image/svg+xml' })
+    )
+  ))
+);
+
 function pickRandomIndex() {
-  return Math.floor(Math.random() * drawings.length);
+  return Math.floor(Math.random() * drawingStrings.length);
 }
 
 interface Props {
@@ -35,22 +50,27 @@ interface Props {
 
 export function DrawingSlideshow(props: Props) {
   let [index, setIndex] = useState(pickRandomIndex());
+
+  let drawingURL = drawingURLs[index];
+
   let interval = 6;
+
   useEffect(() => {
     let nextIndex = pickRandomIndex();
     if (nextIndex == index) {
       nextIndex++;
     }
-    if (nextIndex >= drawings.length) {
+    if (nextIndex >= drawingStrings.length) {
       nextIndex = 0;
     }
     let timeoutId = setTimeout(() => setIndex(nextIndex), 1000 * interval);
     return () => clearTimeout(timeoutId);
   });
+
   return (
     <img
       key={uuidv1()}
-      src={drawings[index]}
+      src={drawingURL}
       alt='Drawing'
       style={{
         ...(props.style ?? {}),
