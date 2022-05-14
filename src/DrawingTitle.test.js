1897c913a196d393b8343d3b9096da92c42473ef
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-14 15:46:28 -0400

    Add DrawingTitle class

---
+++ src/DrawingTitle.test.js
@@ -0,0 +1,63 @@
+import { Drawing } from 'Draw/Drawing';
+import * as SVG from 'Draw/svg/NodeSVG';
+import { appendSequence } from 'Draw/sequences/add/sequence';
+
+import { DrawingTitle } from './DrawingTitle';
+
+let container = null;
+let drawing = null;
+let drawingTitle = null;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+
+  drawing = new Drawing({ SVG });
+  drawing.appendTo(container);
+
+  drawingTitle = new DrawingTitle({ drawing });
+});
+
+afterEach(() => {
+  drawingTitle = null;
+  drawing = null;
+
+  container.remove();
+  container = null;
+});
+
+describe('DrawingTitle class', () => {
+  describe('unspecifiedValue getter', () => {
+    test('when the drawing has no sequences', () => {
+      expect(drawing.sequences.length).toBe(0);
+      expect(drawingTitle.unspecifiedValue).toBe('Drawing');
+    });
+
+    test('when the drawing has one sequence', () => {
+      appendSequence(drawing, { id: 'AA 1234', characters: 'asdfASDFqwer' });
+      expect(drawingTitle.unspecifiedValue).toBe('AA 1234');
+    });
+
+    test('when the drawing has multiple sequences', () => {
+      appendSequence(drawing, { id: 'abc d34', characters: 'asdf' });
+      appendSequence(drawing, { id: 'Q', characters: 'qwer' });
+      appendSequence(drawing, { id: 'nmNMMNmn', characters: 'zxcv' });
+      expect(drawingTitle.unspecifiedValue).toBe('abc d34, Q, nmNMMNmn');
+    });
+
+    test('a sequence with a blank ID', () => {
+      appendSequence(drawing, { id: '  ', characters: 'qwer' });
+      expect(drawingTitle.unspecifiedValue).toBe('Drawing');
+    });
+  });
+
+  test('value getter and setter and unspecify method', () => {
+    appendSequence(drawing, { id: 'asdf', characters: 'asdfASDF' });
+    appendSequence(drawing, { id: 'qwer', characters: 'QWer', });
+    expect(drawingTitle.value).toBe('asdf, qwer'); // the unspecified value
+    drawingTitle.value = '112234';
+    expect(drawingTitle.value).toBe('112234'); // was specified
+    drawingTitle.unspecify();
+    expect(drawingTitle.value).toBe('asdf, qwer'); // was unspecified
+  });
+});
---
+++ src/DrawingTitle.ts
@@ -0,0 +1,64 @@
+import type { Drawing } from 'Draw/Drawing';
+import { isBlank } from 'Parse/isBlank';
+
+export type Options = {
+
+  // the drawing being titled
+  drawing: Drawing;
+};
+
+/**
+ * A title for a drawing with specified and unspecified values.
+ */
+export class DrawingTitle {
+  readonly options: Options;
+
+  _specifiedValue?: string;
+
+  constructor(options: Options) {
+    this.options = options;
+  }
+
+  get drawing() {
+    return this.options.drawing;
+  }
+
+  /**
+   * What the title of the drawing would automatically be
+   * if no title were specified.
+   *
+   * Returns the sequence IDs of the drawing joined by commas
+   * or the string "Drawing" if the title would otherwise be blank.
+   */
+  get unspecifiedValue(): string {
+    let sequencesIds = this.drawing.sequences.map(sequence => sequence.id);
+    let v = sequencesIds.join(', ');
+    return isBlank(v) ? 'Drawing' : v;
+  }
+
+  /**
+   * Returns the specified value if there is one.
+   * and returns the unspecified value otherwise.
+   */
+  get value(): string {
+    if (this._specifiedValue != undefined) {
+      return this._specifiedValue;
+    } else {
+      return this.unspecifiedValue;
+    }
+  }
+
+  /**
+   * Use to specify a value.
+   */
+  set value(v: string) {
+    this._specifiedValue = v;
+  }
+
+  /**
+   * Causes any prior specified value to be forgotten.
+   */
+  unspecify() {
+    this._specifiedValue = undefined;
+  }
+}
