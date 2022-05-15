173ebd55a7689ec949def124bc83a123e2c2b758
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-15 12:54:24 -0400

    Add describe block

--- src/App.test.js
+++ src/App.test.js
@@ -3,26 +3,28 @@ import { appendSequence } from 'Draw/sequences/add/sequence';
 
 import { App } from './App';
 
-test('appendTo and remove methods', () => {
-  let app = new App({ SVG });
-  let container = document.createElement('div');
-  let siblings = [document.createElement('div'), document.createElement('div')];
-  siblings.forEach(sibling => container.appendChild(sibling));
-  expect(container.contains(app.node)).toBeFalsy();
-  app.appendTo(container);
-  expect(container.lastChild).toBe(app.node); // appended to end
-  app.remove();
-  expect(container.contains(app.node)).toBeFalsy(); // was removed
-});
+describe('App class', () => {
+  test('appendTo and remove methods', () => {
+    let app = new App({ SVG });
+    let container = document.createElement('div');
+    let siblings = [document.createElement('div'), document.createElement('div')];
+    siblings.forEach(sibling => container.appendChild(sibling));
+    expect(container.contains(app.node)).toBeFalsy();
+    app.appendTo(container);
+    expect(container.lastChild).toBe(app.node); // appended to end
+    app.remove();
+    expect(container.contains(app.node)).toBeFalsy(); // was removed
+  });
 
-it('updateDocumentTitle method', () => {
-  let app = new App({ SVG });
-  // make sure title is not already RNA2Drawer
-  document.title = 'asdf';
-  expect(app.drawing.isEmpty()).toBeTruthy();
-  app.updateDocumentTitle();
-  expect(document.title).toBe('RNA2Drawer');
-  appendSequence(app.strictDrawing.drawing, { id: '1123nm', characters: 'asdfQWER' });
-  app.updateDocumentTitle();
-  expect(document.title).toBe('1123nm');
+  it('updateDocumentTitle method', () => {
+    let app = new App({ SVG });
+    // make sure title is not already RNA2Drawer
+    document.title = 'asdf';
+    expect(app.drawing.isEmpty()).toBeTruthy();
+    app.updateDocumentTitle();
+    expect(document.title).toBe('RNA2Drawer');
+    appendSequence(app.strictDrawing.drawing, { id: '1123nm', characters: 'asdfQWER' });
+    app.updateDocumentTitle();
+    expect(document.title).toBe('1123nm');
+  });
 });
