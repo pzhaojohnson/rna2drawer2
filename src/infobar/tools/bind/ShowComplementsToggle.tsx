545667057306d1a8809867018f720b00dc45dfef
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-17 21:58:24 -0400

    Add check to toggled ShowComplementsToggle

--- src/infobar/tools/bind/ShowComplementsToggle.tsx
+++ src/infobar/tools/bind/ShowComplementsToggle.tsx
@@ -3,6 +3,26 @@ import type { App } from 'App';
 import * as React from 'react';
 import styles from './ShowComplementsToggle.css';
 
+function Check() {
+  return (
+    <svg
+      className={styles.check}
+      width="10.5px"
+      height="10.5px"
+      viewBox="0 0 96 96"
+      xmlns="http://www.w3.org/2000/svg"
+      overflow="hidden"
+    >
+      <path
+        d="M86.1 15.8 34.9 64.2 10.3 39 1.8 47.1 34.5 80.7 43.1 72.7 94.2 24.2Z"
+        stroke="#0D0D63"
+        strokeWidth="2.5"
+        fill="#0D0D63"
+      />
+    </svg>
+  );
+}
+
 export type Props = {
 
   // a reference to the whole app
@@ -16,7 +36,7 @@ export function ShowComplementsToggle(props: Props) {
   let untoggledClassName = styles.untoggledShowComplementsToggle;
 
   return (
-    <p
+    <div
       className={`
         ${styles.showComplementsToggle}
         ${bindingTool.showComplements ? toggledClassName : untoggledClassName}
@@ -26,7 +46,11 @@ export function ShowComplementsToggle(props: Props) {
         props.app.refresh();
       }}
     >
-      Show Complements
-    </p>
+      <p className={styles.text} >
+        Show Complements
+      </p>
+      <div className={styles.spacer} />
+      <Check />
+    </div>
   );
 }
