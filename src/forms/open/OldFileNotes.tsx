d424f7b73ab463f1f777d4d5557504cda86276d5
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-17 22:58:19 -0400

    Move text leading into preserved aspects in old file notes to its own line

--- src/forms/open/OldFileNotes.tsx
+++ src/forms/open/OldFileNotes.tsx
@@ -8,12 +8,17 @@ function Disclaimer() {
       <div style={{ minWidth: '10px' }} />
       <p className={styles.disclaimerText} >
         Not all aspects of a drawing from the first version of RNA2Drawer
-        (before the web app) will be preserved -
-        <span style={{ fontWeight: 700, fontStyle: 'italic', color: '#101010' }}>
-          &nbsp;The following will be preserved...
-        </span>
+        (before the web app) will be preserved
       </p>
     </div>
+  );
+}
+
+function PreservedAspectsLeadingText() {
+  return (
+    <p className={styles.preservedAspectsLeadingText} >
+      The following will be preserved...
+    </p>
   );
 }
 
@@ -37,6 +42,7 @@ export function OldFileNotes() {
   return (
     <div className={styles.oldFileNotes} >
       <Disclaimer />
+      <PreservedAspectsLeadingText />
       <div style={{ margin: '15px 0px 0px 60px' }} >
         <PreservedAspect>The sequence and its ID</PreservedAspect>
         <PreservedAspect>The secondary structure</PreservedAspect>
