3d19b1facac9396ea148df880ae177c3c3c727dc
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-18 20:16:21 -0400

    Use CheckboxField component for FlatOutermostLoopField

--- src/forms/edit/layout/FlatOutermostLoopField.tsx
+++ src/forms/edit/layout/FlatOutermostLoopField.tsx
@@ -1,8 +1,7 @@
 import type { App } from 'App';
 
 import * as React from 'react';
-import { Checkbox } from 'Forms/inputs/checkbox/Checkbox';
-import checkboxFieldStyles from 'Forms/inputs/checkbox/CheckboxField.css';
+import { CheckboxField } from 'Forms/inputs/checkbox/CheckboxField';
 
 import { hasFlatOutermostLoop } from './hasFlatOutermostLoop';
 
@@ -12,25 +11,18 @@ export type Props = {
 
 export function FlatOutermostLoopField(props: Props) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-      <Checkbox
-        checked={hasFlatOutermostLoop(props.app.strictDrawing)}
-        onChange={event => {
-          if (event.target.checked != hasFlatOutermostLoop(props.app.strictDrawing)) {
-            props.app.pushUndo();
-            let generalLayoutProps = props.app.strictDrawing.generalLayoutProps;
-            generalLayoutProps.outermostLoopShape = event.target.checked ? 'flat' : 'round';
-            props.app.strictDrawing.updateLayout();
-            props.app.refresh();
-          }
-        }}
-      />
-      <p
-        className={`${checkboxFieldStyles.label} unselectable`}
-        style={{ marginLeft: '6px' }}
-      >
-        Flat Outermost Loop
-      </p>
-    </div>
+    <CheckboxField
+      label='Flat Outermost Loop'
+      checked={hasFlatOutermostLoop(props.app.strictDrawing)}
+      onChange={event => {
+        if (event.target.checked != hasFlatOutermostLoop(props.app.strictDrawing)) {
+          props.app.pushUndo();
+          let generalLayoutProps = props.app.strictDrawing.generalLayoutProps;
+          generalLayoutProps.outermostLoopShape = event.target.checked ? 'flat' : 'round';
+          props.app.strictDrawing.updateLayout();
+          props.app.refresh();
+        }
+      }}
+    />
   );
 }
