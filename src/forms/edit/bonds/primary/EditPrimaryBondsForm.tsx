90ae46b5480a730d60e001adae410abf6e2caadf
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-22 16:13:11 -0400

    Add message for when drawing has no primary bonds

--- src/forms/edit/bonds/primary/EditPrimaryBondsForm.tsx
+++ src/forms/edit/bonds/primary/EditPrimaryBondsForm.tsx
@@ -2,6 +2,7 @@ import type { App } from 'App';
 import type { PrimaryBond } from 'Draw/bonds/straight/PrimaryBond';
 
 import * as React from 'react';
+import formStyles from './EditPrimaryBondsForm.css';
 import { PartialWidthContainer } from 'Forms/containers/PartialWidthContainer';
 import { FormHistoryInterface } from 'Forms/history/FormHistoryInterface';
 
@@ -12,6 +13,14 @@ import { StrokeWidthField } from './StrokeWidthField';
 import { BasePaddingField } from './BasePaddingField';
 import { StrokeLinecapField } from './StrokeLinecapField';
 import { ForwardAndBackwardButtons } from './ForwardAndBackwardButtons';
+
+function DrawingHasNoPrimaryBondsNotes() {
+  return (
+    <p className={formStyles.notesText} >
+      Drawing has no primary bonds...
+    </p>
+  );
+}
 
 export type Props = {
   app: App; // a reference to the whole app
@@ -31,7 +40,9 @@ export function EditPrimaryBondsForm(props: Props) {
       title='Primary Bonds'
       style={{ width: '324px' }}
     >
-      {props.primaryBonds.length == 0 ? (
+      {props.app.drawing.primaryBonds.length == 0 ? (
+        <DrawingHasNoPrimaryBondsNotes />
+      ) : props.primaryBonds.length == 0 ? (
         <p className={'unselectable'} style={{ fontSize: '12px' }} >
           No primary bonds to edit.
         </p>
