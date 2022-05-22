7b5c73892d4f10e89da7538a71f6f6e1ac276f4f
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-22 19:28:05 -0400

    Change EditPrimaryBondsButton of menu to open the form of the editing tool

--- src/menu/edit/EditPrimaryBondsButton.tsx
+++ src/menu/edit/EditPrimaryBondsButton.tsx
@@ -1,27 +1,24 @@
 import * as React from 'react';
 import { DroppedButton } from 'Menu/DroppedButton';
 import type { App } from 'App';
-import { EditPrimaryBondsForm } from 'Forms/edit/bonds/primary/EditPrimaryBondsForm';
-import { v4 as uuidv4 } from 'uuid';
+import { PrimaryBond } from 'Draw/bonds/straight/PrimaryBond';
 
 export type Props = {
   app: App;
 }
-
-const formKey = uuidv4();
 
 export function EditPrimaryBondsButton(props: Props) {
   return (
     <DroppedButton
       text='Primary Bonds'
       onClick={() => {
-        props.app.formContainer.renderForm(formProps => (
-          <EditPrimaryBondsForm
-            {...formProps}
-            app={props.app}
-            primaryBonds={[...props.app.strictDrawing.drawing.primaryBonds]}
-          />
-        ), { key: formKey });
+        let editingTool = props.app.drawingInteraction.editingTool;
+
+        if (editingTool.editingType != PrimaryBond) {
+          editingTool.editingType = PrimaryBond;
+        }
+
+        editingTool.renderForm();
       }}
     />
   );
