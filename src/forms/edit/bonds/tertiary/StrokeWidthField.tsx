d0382bbcfe1083784354f7473566f5ad81b281ec
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-22 09:29:07 -0400

    Use TextInputField component for StrokeWidthField in EditTertiaryBondsForm

--- src/forms/edit/bonds/tertiary/StrokeWidthField.tsx
+++ src/forms/edit/bonds/tertiary/StrokeWidthField.tsx
@@ -8,7 +8,7 @@ import { numberToDisplayableString as displayableString } from 'Forms/inputs/num
 import { isBlank } from 'Parse/isBlank';
 
 import * as React from 'react';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
+import { TextInputField } from 'Forms/inputs/text/TextInputField';
 
 // returns the path elements of the tertiary bonds
 function paths(tertiaryBonds: TertiaryBond[]): SVG.Path[] {
@@ -39,30 +39,24 @@ export class StrokeWidthField extends React.Component<Props> {
 
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-        <input
-          type='text'
-          className={textFieldStyles.input}
-          value={this.state.value}
-          onChange={event => this.setState({ value: event.target.value })}
-          onBlur={() => {
+      <TextInputField
+        label='Line Width'
+        value={this.state.value}
+        onChange={event => this.setState({ value: event.target.value })}
+        onBlur={() => {
+          this.submit();
+          this.props.app.refresh();
+        }}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
             this.submit();
             this.props.app.refresh();
-          }}
-          onKeyUp={event => {
-            if (event.key.toLowerCase() == 'enter') {
-              this.submit();
-              this.props.app.refresh();
-            }
-          }}
-          style={{ width: '36px' }}
-        />
-        <div style={{ marginLeft: '8px' }} >
-          <p className={`${textFieldStyles.label} unselectable`} >
-            Line Width
-          </p>
-        </div>
-      </div>
+          }
+        }}
+        input={{
+          style: { width: '36px' },
+        }}
+      />
     );
   }
 
