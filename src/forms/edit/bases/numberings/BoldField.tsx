4f8416ea6c989acba88d7dd2cd849afaabdfab4f
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-23 16:40:32 -0400

    Use CheckboxField component for BoldField in EditBaseNumberingsForm

--- src/forms/edit/bases/numberings/BoldField.tsx
+++ src/forms/edit/bases/numberings/BoldField.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { Checkbox } from 'Forms/inputs/checkbox/Checkbox';
-import checkboxFieldStyles from 'Forms/inputs/checkbox/CheckboxField.css';
+import { CheckboxField } from 'Forms/inputs/checkbox/CheckboxField';
 import type { App } from 'App';
 import { BaseNumbering } from 'Draw/bases/number/BaseNumbering';
 
@@ -28,26 +27,19 @@ function areAllBold(baseNumberings: BaseNumbering[]): boolean {
 
 export function BoldField(props: Props) {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-      <Checkbox
-        checked={areAllBold(props.baseNumberings)}
-        onChange={event => {
-          props.app.pushUndo();
-          let fw = event.target.checked ? 700 : 400;
-          props.baseNumberings.forEach(bn => {
-            bn.text.attr({ 'font-weight': fw });
-            bn.reposition();
-          });
-          BaseNumbering.recommendedDefaults.text['font-weight'] = fw;
-          props.app.refresh();
-        }}
-      />
-      <p
-        className={`${checkboxFieldStyles.label} unselectable`}
-        style={{ marginLeft: '6px' }}
-      >
-        Bold
-      </p>
-    </div>
+    <CheckboxField
+      label='Bold'
+      checked={areAllBold(props.baseNumberings)}
+      onChange={event => {
+        props.app.pushUndo();
+        let fw = event.target.checked ? 700 : 400;
+        props.baseNumberings.forEach(bn => {
+          bn.text.attr({ 'font-weight': fw });
+          bn.reposition();
+        });
+        BaseNumbering.recommendedDefaults.text['font-weight'] = fw;
+        props.app.refresh();
+      }}
+    />
   );
 }
