18455403585858adb57e953aefb871410c6dd1c8
Philip Johnson <p.zhao.johnson@gmail.com>
2022-05-27 11:26:20 -0400

    Add buttons to select secondary bonds to EditSecondaryBondsForm

--- src/forms/edit/bonds/secondary/EditSecondaryBondsForm.tsx
+++ src/forms/edit/bonds/secondary/EditSecondaryBondsForm.tsx
@@ -1,5 +1,5 @@
 import type { App } from 'App';
-import type { SecondaryBond } from 'Draw/bonds/straight/SecondaryBond';
+import { SecondaryBond } from 'Draw/bonds/straight/SecondaryBond';
 
 import * as React from 'react';
 import styles from './EditSecondaryBondsForm.css';
@@ -48,6 +48,113 @@ function NoSecondaryBondsAreSelectedNotes() {
   );
 }
 
+function SelectSecondaryBondsButton(
+  props: {
+    app: App,
+    type: 'all' | 'AUT' | 'GC' | 'GUT' | 'other',
+    children?: React.ReactNode,
+    style?: React.CSSProperties,
+  },
+) {
+  return (
+    <button
+      className={styles.selectSecondaryBondsButton}
+      onClick={() => {
+        let drawing = props.app.drawing;
+        let drawingInteraction = props.app.drawingInteraction;
+        let editingTool = drawingInteraction.editingTool;
+
+        drawingInteraction.currentTool = editingTool; // switch to editing tool
+        editingTool.editingType = SecondaryBond; // set to edit secondary bonds
+
+        if (props.type == 'all') {
+          editingTool.select([...drawing.secondaryBonds]);
+        } else {
+          editingTool.select(drawing.secondaryBonds.filter(sb => sb.type == props.type));
+        }
+      }}
+      style={props.style}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+function SelectAllSecondaryBondsButton(props: { app: App }) {
+  return (
+    <SelectSecondaryBondsButton {...props} type='all' >
+      All Secondary Bonds
+    </SelectSecondaryBondsButton>
+  );
+}
+
+function SelectAUTSecondaryBondsButton(props: { app: App }) {
+  return (
+    props.app.drawing.secondaryBonds.some(sb => sb.type == 'AUT') ? (
+      <SelectSecondaryBondsButton {...props} type='AUT' >
+        AU and AT Secondary Bonds
+      </SelectSecondaryBondsButton>
+    ) : (
+      null
+    )
+  );
+}
+
+function SelectGCSecondaryBondsButton(props: { app: App }) {
+  return (
+    props.app.drawing.secondaryBonds.some(sb => sb.type == 'GC') ? (
+      <SelectSecondaryBondsButton {...props} type='GC' >
+        GC Secondary Bonds
+      </SelectSecondaryBondsButton>
+    ) : (
+      null
+    )
+  );
+}
+
+function SelectGUTSecondaryBondsButton(props: { app: App }) {
+  return (
+    props.app.drawing.secondaryBonds.some(sb => sb.type == 'GUT') ? (
+      <SelectSecondaryBondsButton {...props} type='GUT' >
+        GU and GT Secondary Bonds
+      </SelectSecondaryBondsButton>
+    ) : (
+      null
+    )
+  );
+}
+
+function SelectOtherSecondaryBondsButton(props: { app: App }) {
+  return (
+    props.app.drawing.secondaryBonds.every(sb => sb.type == 'other') ? (
+      null // there is already the button to select all secondary bonds
+    ) : props.app.drawing.secondaryBonds.some(sb => sb.type == 'other') ? (
+      <SelectSecondaryBondsButton {...props} type='other' >
+        Other Secondary Bonds
+      </SelectSecondaryBondsButton>
+    ) : (
+      null
+    )
+  );
+}
+
+function SelectSecondaryBondsButtons(props: { app: App }) {
+  return (
+    <div style={{ marginTop: '24px' }} >
+      <p className={styles.notesText} >
+        Or select...
+      </p>
+      <div style={{ marginTop: '12px', display: 'flex', flexDirection: 'column' }} >
+        <SelectAllSecondaryBondsButton {...props} />
+        <SelectAUTSecondaryBondsButton {...props} />
+        <SelectGCSecondaryBondsButton {...props} />
+        <SelectGUTSecondaryBondsButton {...props} />
+        <SelectOtherSecondaryBondsButton {...props} />
+      </div>
+    </div>
+  );
+}
+
 export type Props = {
   app: App;
 
@@ -73,6 +180,7 @@ export function EditSecondaryBondsForm(props: Props) {
           <ForAllSecondaryBondsLabel />
           <BaseSpacingField {...props} />
           <NoSecondaryBondsAreSelectedNotes />
+          <SelectSecondaryBondsButtons {...props} />
         </div>
       ) : (
         <div style={{ display: 'flex', flexDirection: 'column' }} >
