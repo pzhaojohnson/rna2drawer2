10ea82d3b1179ebeb7902004c21b61e3fcdab475
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-14 10:42:24 -0400

    Replace SequenceInterface import with type import

--- src/forms/motifs/FindMotifsForm.tsx
+++ src/forms/motifs/FindMotifsForm.tsx
@@ -1,5 +1,5 @@
 import type { App } from 'App';
-import { SequenceInterface as Sequence } from 'Draw/sequences/SequenceInterface';
+import type { Sequence } from 'Draw/sequences/Sequence';
 import { numberingOffset } from 'Draw/sequences/numberingOffset';
 import type { Base } from 'Draw/bases/Base';
 
