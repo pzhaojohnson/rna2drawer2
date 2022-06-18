063bec3ea500da467edcd0c680a370911ad4ad08
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-18 17:10:17 -0400

    Use TextInputField component in AllowedMismatchField in ComplementRules form

--- src/forms/settings/complements/AllowedMismatchField.tsx
+++ src/forms/settings/complements/AllowedMismatchField.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
+import { TextInputField } from 'Forms/inputs/text/TextInputField';
 import type { App } from 'App';
 import type { BindingTool } from 'Draw/interact/bind/BindingTool';
 import { round } from 'Math/round';
@@ -42,30 +42,22 @@ export class AllowedMismatchField extends React.Component<Props> {
 
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-        <input
-          type='text'
-          className={textFieldStyles.input}
-          value={this.state.value}
-          onChange={event => this.setState({ value: event.target.value })}
-          onBlur={() => {
+      <TextInputField
+        label='Mismatch Allowed'
+        value={this.state.value}
+        onChange={event => this.setState({ value: event.target.value })}
+        onBlur={() => {
+          this.submit();
+          this.props.app.refresh();
+        }}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
             this.submit();
             this.props.app.refresh();
-          }}
-          onKeyUp={event => {
-            if (event.key.toLowerCase() == 'enter') {
-              this.submit();
-              this.props.app.refresh();
-            }
-          }}
-          style={{ width: '32px' }}
-        />
-        <div style={{ marginLeft: '8px' }} >
-          <p className={`${textFieldStyles.label} unselectable`} >
-            Mismatch Allowed
-          </p>
-        </div>
-      </div>
+          }
+        }}
+        input={{ style: { width: '32px' } }}
+      />
     );
   }
 
