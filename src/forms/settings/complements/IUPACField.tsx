659881d77e2b2eeb77f9f47261a86644e59ad32f
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-18 17:17:32 -0400

    Use CheckboxField component in IUPACField in ComplementRules form

--- src/forms/settings/complements/IUPACField.tsx
+++ src/forms/settings/complements/IUPACField.tsx
@@ -1,8 +1,7 @@
 import type { App } from 'App';
 
 import * as React from 'react';
-import { Checkbox } from 'Forms/inputs/checkbox/Checkbox';
-import checkboxFieldStyles from 'Forms/inputs/checkbox/CheckboxField.css';
+import { CheckboxField } from 'Forms/inputs/checkbox/CheckboxField';
 
 export type Props = {
 
@@ -14,21 +13,14 @@ export function IUPACField(props: Props) {
   let bindingTool = props.app.strictDrawingInteraction.bindingTool;
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
-      <Checkbox
-        checked={bindingTool.complementsOptions.IUPAC ?? false}
-        onChange={event => {
-          let bindingTool = props.app.strictDrawingInteraction.bindingTool;
-          bindingTool.complementsOptions.IUPAC = event.target.checked;
-          props.app.refresh();
-        }}
-      />
-      <p
-        className={`${checkboxFieldStyles.label} unselectable`}
-        style={{ marginLeft: '6px' }}
-      >
-        Use IUPAC Single Letter Codes
-      </p>
-    </div>
+    <CheckboxField
+      label='Use IUPAC Single Letter Codes'
+      checked={bindingTool.complementsOptions.IUPAC ?? false}
+      onChange={event => {
+        let bindingTool = props.app.strictDrawingInteraction.bindingTool;
+        bindingTool.complementsOptions.IUPAC = event.target.checked;
+        props.app.refresh();
+      }}
+    />
   );
 }
