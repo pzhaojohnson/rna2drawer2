23cc104bcac46fad8131a06818c029997acfb612
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-22 10:49:40 -0400

    Add describe block

--- src/forms/welcome/WelcomePage.test.js
+++ src/forms/welcome/WelcomePage.test.js
@@ -5,7 +5,9 @@ import * as React from 'react';
 
 import { WelcomePage } from './WelcomePage';
 
-it('renders', () => {
-  let app = new App({ SVG });
-  app.formContainer.renderForm(() => <WelcomePage app={app} />);
+describe('WelcomePage component', () => {
+  it('renders', () => {
+    let app = new App({ SVG });
+    app.formContainer.renderForm(() => <WelcomePage app={app} />);
+  });
 });
