d8e991748fb035a8eaaa2ebe52094a2df61b42c4
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-23 10:02:57 -0400

    Remove comma and change or to and

--- src/forms/new/DotBracketParsingDetails.tsx
+++ src/forms/new/DotBracketParsingDetails.tsx
@@ -15,7 +15,7 @@ export function DotBracketParsingDetails(): React.ReactElement {
           Matching parentheses "( )" indicate base-pairs in the secondary structure.
         </p>
         <p style={{ marginTop: '8px' }} >
-          {'Pseudoknotted base-pairs are specified by "[ ]", "{ }", or "< >".'}
+          {'Pseudoknotted base-pairs are specified by "[ ]", "{ }" and "< >".'}
         </p>
         <p style={{ marginTop: '8px' }} >
           All other characters and whitespace are ignored.
