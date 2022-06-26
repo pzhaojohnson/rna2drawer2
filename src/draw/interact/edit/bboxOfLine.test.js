e99eccb7ecdf4ea26f658f87a6e571eb5d4a832c
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-26 17:15:56 -0400

    Account for stroke-linecap in bboxOfLine function and expand test suite for bboxOfLine function

--- src/draw/interact/edit/bboxOfLine.test.js
+++ src/draw/interact/edit/bboxOfLine.test.js
@@ -1,6 +1,16 @@
-import { NodeSVG } from 'Draw/svg/NodeSVG';
+import * as SVG from '@svgdotjs/svg.js';
+import * as NodeSVG from 'Draw/svg/NodeSVG';
 
 import { bboxOfLine } from './bboxOfLine';
+
+function boxesAreClose(box1, box2) {
+  return (
+    Math.abs(box1.x - box2.x) < 0.005
+    && Math.abs(box1.y - box2.y) < 0.005
+    && Math.abs(box1.width - box2.width) < 0.005
+    && Math.abs(box1.height - box2.height) < 0.005
+  );
+}
 
 let container = null;
 let svg = null;
@@ -9,7 +19,7 @@ beforeEach(() => {
   container = document.createElement('div');
   document.body.appendChild(container);
 
-  svg = NodeSVG();
+  svg = NodeSVG.SVG();
   svg.addTo(container);
 });
 
@@ -21,33 +31,75 @@ afterEach(() => {
 });
 
 describe('bboxOfLine function', () => {
-  test('a line that goes from top-left to bottom-right', () => {
+  test('a line going from top-left to bottom-right', () => {
     let line = svg.line(20, 18, 28.66, 13);
     line.attr('stroke-width', 8);
-    let bbox = bboxOfLine(line);
-    expect(bbox.x).toBeCloseTo(18);
-    expect(bbox.y).toBeCloseTo(9.535923);
-    expect(bbox.width).toBeCloseTo(12.660088);
-    expect(bbox.height).toBeCloseTo(11.928152);
+
+    line.attr('stroke-linecap', 'butt'); // butt stroke-linecap
+    let bbox = new SVG.Box(18, 9.5359, 12.6600, 11.9281);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'square'); // square stroke-linecap
+    bbox = new SVG.Box(14.5358, 7.5358, 19.5882, 15.9282);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'round'); // round stroke-linecap
+    bbox = new SVG.Box(16, 9, 16.6599, 13);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
   });
 
-  test('a line that goes from bottom-left to top-right', () => {
+  test('a line going from bottom-left to top-right', () => {
     let line = svg.line(-31, 1, -27, 32);
     line.attr('stroke-width', 5);
-    let bbox = bboxOfLine(line);
-    expect(bbox.x).toBeCloseTo(-33.479444);
-    expect(bbox.y).toBeCloseTo(0.680071);
-    expect(bbox.width).toBeCloseTo(8.958889);
-    expect(bbox.height).toBeCloseTo(31.639856);
+
+    line.attr('stroke-linecap', 'butt'); // butt stroke-linecap
+    let bbox = new SVG.Box(-33.4794, 0.6800, 8.9588, 31.6398);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'square'); // square stroke-linecap
+    bbox = new SVG.Box(-33.7993, -1.7993, 9.5987, 36.5987);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'round'); // round stroke-linecap
+    bbox = new SVG.Box(-33.5, -1.5, 9, 36);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
   });
 
-  it('can handle string stroke widths', () => {
-    let line = svg.line(5, 10, 5, 20);
+  test('a stroke-width of zero', () => {
+    let line = svg.line(50, 60, 75.5, 37.6);
+    line.attr('stroke-width', 0);
+
+    let bbox = new SVG.Box(50, 37.6, 25.5, 22.4);
+
+    line.attr('stroke-linecap', 'butt'); // butt stroke-linecap
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+    line.attr('stroke-linecap', 'square'); // square stroke-linecap
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+    line.attr('stroke-linecap', 'round'); // round stroke-linecap
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+  });
+
+  test('when points 1 and 2 are the same point', () => {
+    let line = svg.line(20.3, 56, 20.3, 56);
+    line.attr('stroke-width', 5);
+
+    line.attr('stroke-linecap', 'butt'); // butt stroke-linecap
+    let bbox = new SVG.Box(20.3, 53.5, 0, 5);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'square'); // square stroke-linecap
+    bbox = new SVG.Box(17.8, 53.5, 5, 5);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+
+    line.attr('stroke-linecap', 'round'); // round stroke-linecap
+    bbox = new SVG.Box(17.8, 53.5, 5, 5);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
+  });
+
+  test('string attribute values', () => {
+    let line = svg.line('5px', '10', '15.2', '20.3px');
     line.attr('stroke-width', '6px');
-    let bbox = bboxOfLine(line);
-    expect(bbox.x).toBeCloseTo(2);
-    expect(bbox.y).toBeCloseTo(10);
-    expect(bbox.width).toBeCloseTo(6);
-    expect(bbox.height).toBeCloseTo(10);
+    let bbox = new SVG.Box(2.8683, 7.8890, 14.4632, 14.5218);
+    expect(boxesAreClose(bbox, bboxOfLine(line))).toBeTruthy();
   });
 });
--- src/draw/interact/edit/bboxOfLine.ts
+++ src/draw/interact/edit/bboxOfLine.ts
@@ -1,6 +1,23 @@
 import * as SVG from '@svgdotjs/svg.js';
 import { interpretNumericValue } from 'Draw/svg/interpretNumericValue';
+
+import { Point2D as Point } from 'Math/points/Point';
+import { displacement2D as displacement } from 'Math/points/displacement';
 import { direction2D as direction } from 'Math/points/direction';
+
+type CircleSpecification = {
+  center: Point;
+  diameter: number;
+};
+
+function bboxOfCircle(spec: CircleSpecification): SVG.Box {
+  return new SVG.Box(
+    spec.center.x - (spec.diameter / 2),
+    spec.center.y - (spec.diameter / 2),
+    spec.diameter,
+    spec.diameter,
+  );
+}
 
 /**
  * Calculates the bounding box of a line more precisely
@@ -8,21 +25,64 @@ import { direction2D as direction } from 'Math/points/direction';
  * to account for stroke-width and stroke-linecap.
  */
 export function bboxOfLine(line: SVG.Line): SVG.Box {
-  let bbox = line.bbox();
-
+  let x1 = interpretNumericValue(line.attr('x1'))?.valueOf();
+  let y1 = interpretNumericValue(line.attr('y1'))?.valueOf();
+  let x2 = interpretNumericValue(line.attr('x2'))?.valueOf();
+  let y2 = interpretNumericValue(line.attr('y2'))?.valueOf();
   let strokeWidth = interpretNumericValue(line.attr('stroke-width'))?.valueOf();
+  let strokeLineCap = line.attr('stroke-linecap');
 
-  if (strokeWidth == undefined) {
+  // if any values could not be interpreted
+  if (
+    x1 == undefined
+    || y1 == undefined
+    || x2 == undefined
+    || y2 == undefined
+    || strokeWidth == undefined
+  ) {
     // just return the bounding box calculated by the built-in bbox method
-    return bbox;
+    return line.bbox();
   }
 
-  let angle = direction({ x: bbox.width, y: bbox.height }) - (Math.PI / 2);
+  let p1 = { x: x1, y: y1 };
+  let p2 = { x: x2, y: y2 };
 
-  return new SVG.Box(
-    bbox.x - Math.abs((strokeWidth / 2) * Math.cos(angle)),
-    bbox.y - Math.abs((strokeWidth / 2) * Math.sin(angle)),
-    bbox.width + Math.abs(strokeWidth * Math.cos(angle)),
-    bbox.height + Math.abs(strokeWidth * Math.sin(angle)),
+  // first calculate the bounding box of points 1 and 2
+  let bbox = new SVG.Box(
+    Math.min(x1, x2),
+    Math.min(y1, y2),
+    Math.max(x1, x2) - Math.min(x1, x2),
+    Math.max(y1, y2) - Math.min(y1, y2),
   );
+
+  // then expand the bounding box to account for the stroke-width
+  let h = strokeWidth / 2;
+  let angle = direction(displacement(p1, p2)) - (Math.PI / 2);
+  bbox = new SVG.Box(
+    bbox.x - Math.abs(h * Math.cos(angle)),
+    bbox.y - Math.abs(h * Math.sin(angle)),
+    bbox.width + Math.abs(2 * h * Math.cos(angle)),
+    bbox.height + Math.abs(2 * h * Math.sin(angle)),
+  );
+
+  // then expand the bounding box again to account for
+  // a square stroke-linecap if present
+  if (strokeLineCap == 'square') {
+    let h = strokeWidth / 2;
+    let angle = direction(displacement(p1, p2));
+    bbox = new SVG.Box(
+      bbox.x - Math.abs(h * Math.cos(angle)),
+      bbox.y - Math.abs(h * Math.sin(angle)),
+      bbox.width + Math.abs(2 * h * Math.cos(angle)),
+      bbox.height + Math.abs(2 * h * Math.sin(angle)),
+    );
+  }
+
+  // also account for a round stroke-linecap if present
+  if (strokeLineCap == 'round') {
+    bbox = bbox.merge(bboxOfCircle({ center: p1, diameter: strokeWidth }));
+    bbox = bbox.merge(bboxOfCircle({ center: p2, diameter: strokeWidth }));
+  }
+
+  return bbox;
 }
