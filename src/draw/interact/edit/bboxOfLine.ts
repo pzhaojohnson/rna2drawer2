f1ce8a63f04e87a15c614345a51d45c51ba205a4
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-26 14:18:09 -0400

    Unbranch code

--- src/draw/interact/edit/bboxOfLine.ts
+++ src/draw/interact/edit/bboxOfLine.ts
@@ -10,18 +10,19 @@ import { direction2D as direction } from 'Math/points/direction';
 export function bboxOfLine(line: SVG.Line): SVG.Box {
   let bbox = line.bbox();
 
-  // bbox method doesn't seem to account for stroke width
   let strokeWidth = interpretNumericValue(line.attr('stroke-width'));
-  if (strokeWidth) {
-    let sw = strokeWidth.valueOf();
-    let a = direction({ x: bbox.width, y: bbox.height }) - (Math.PI / 2);
-    bbox = new SVG.Box(
-      bbox.x - Math.abs((sw / 2) * Math.cos(a)),
-      bbox.y - Math.abs((sw / 2) * Math.sin(a)),
-      bbox.width + Math.abs(sw * Math.cos(a)),
-      bbox.height + Math.abs(sw * Math.sin(a)),
-    );
+
+  if (strokeWidth == undefined) {
+    // just return the bounding box calculated by the built-in bbox method
+    return bbox;
   }
 
-  return bbox;
+  let sw = strokeWidth.valueOf();
+  let a = direction({ x: bbox.width, y: bbox.height }) - (Math.PI / 2);
+  return new SVG.Box(
+    bbox.x - Math.abs((sw / 2) * Math.cos(a)),
+    bbox.y - Math.abs((sw / 2) * Math.sin(a)),
+    bbox.width + Math.abs(sw * Math.cos(a)),
+    bbox.height + Math.abs(sw * Math.sin(a)),
+  );
 }
