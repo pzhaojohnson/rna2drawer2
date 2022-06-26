3ae7d72c2fb1bde296309baf1c37a0c2c63725b2
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-26 14:19:41 -0400

    Remove unnecessary sw local variable

--- src/draw/interact/edit/bboxOfLine.ts
+++ src/draw/interact/edit/bboxOfLine.ts
@@ -10,19 +10,19 @@ import { direction2D as direction } from 'Math/points/direction';
 export function bboxOfLine(line: SVG.Line): SVG.Box {
   let bbox = line.bbox();
 
-  let strokeWidth = interpretNumericValue(line.attr('stroke-width'));
+  let strokeWidth = interpretNumericValue(line.attr('stroke-width'))?.valueOf();
 
   if (strokeWidth == undefined) {
     // just return the bounding box calculated by the built-in bbox method
     return bbox;
   }
 
-  let sw = strokeWidth.valueOf();
   let a = direction({ x: bbox.width, y: bbox.height }) - (Math.PI / 2);
+
   return new SVG.Box(
-    bbox.x - Math.abs((sw / 2) * Math.cos(a)),
-    bbox.y - Math.abs((sw / 2) * Math.sin(a)),
-    bbox.width + Math.abs(sw * Math.cos(a)),
-    bbox.height + Math.abs(sw * Math.sin(a)),
+    bbox.x - Math.abs((strokeWidth / 2) * Math.cos(a)),
+    bbox.y - Math.abs((strokeWidth / 2) * Math.sin(a)),
+    bbox.width + Math.abs(strokeWidth * Math.cos(a)),
+    bbox.height + Math.abs(strokeWidth * Math.sin(a)),
   );
 }
