b415027cd906f3798d837c436631b9a88c508e3b
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-26 09:28:56 -0400

    Add comment

--- src/math/mean.ts
+++ src/math/mean.ts
@@ -1,5 +1,9 @@
 import { sum } from './sum';
 
+/**
+ * It is not firmly defined what is returned
+ * for an empty array of numbers.
+ */
 export function mean(nums: number[]): number {
   if (nums.length == 0) {
     return NaN;
