59a274215daa00c4d1e249bcd0a20114d70d19f7
Philip Johnson <p.zhao.johnson@gmail.com>
2022-06-27 15:33:14 -0400

    Fix issue where removeBases function was removing the wrong per base layout props

--- src/draw/strict/removeBases.ts
+++ src/draw/strict/removeBases.ts
@@ -1,17 +1,26 @@
 import type { StrictDrawing } from 'Draw/strict/StrictDrawing';
 import type { Base } from 'Draw/bases/Base';
 import { unpair } from 'Draw/strict/unpair';
+import { compareNumbersDescending } from 'Array/sort';
 import { removeBases as removeBasesFromDrawing } from 'Draw/sequences/remove/bases';
 
+function isNumber(v: unknown): v is number {
+  return typeof v == 'number';
+}
+
 function removePerBaseLayoutProps(strictDrawing: StrictDrawing, bs: Base[]) {
-  let perBaseLayoutProps = strictDrawing.perBaseLayoutProps();
   let seq = strictDrawing.layoutSequence();
   let basesToPositions = seq.basesToPositions();
-  bs.forEach(b => {
-    let p = basesToPositions.get(b);
-    if (typeof p == 'number') { // should always be
-      perBaseLayoutProps.splice(p - 1, 1);
-    }
+
+  // the base positions to remove
+  let ps = bs.map(b => basesToPositions.get(b)).filter(isNumber);
+
+  // splicing must be done in descending order by position to be correct
+  ps.sort(compareNumbersDescending);
+
+  let perBaseLayoutProps = strictDrawing.perBaseLayoutProps();
+  ps.forEach(p => {
+    perBaseLayoutProps.splice(p - 1, 1);
   });
   strictDrawing.setPerBaseLayoutProps(perBaseLayoutProps);
 }
