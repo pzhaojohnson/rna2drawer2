7c83e74538eeb202fb4253500fbe1fcd93a1acda
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-02 11:25:11 -0400

    Add DetailsToggle alias for OptionsToggle component

---
+++ src/forms/buttons/DetailsToggle.tsx
@@ -0,0 +1,11 @@
+import { OptionsToggle } from 'Forms/buttons/OptionsToggle';
+import { Props } from 'Forms/buttons/OptionsToggle';
+
+export {
+  /**
+   * An alias for the OptionsToggle component.
+   */
+  OptionsToggle as DetailsToggle,
+
+  Props,
+};
