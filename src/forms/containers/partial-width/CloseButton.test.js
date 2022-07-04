ca728f99cf144e581bad5e17341d89593e4bf88c
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-04 09:50:58 -0400

    Convert default import to module import

--- src/forms/containers/partial-width/CloseButton.test.js
+++ src/forms/containers/partial-width/CloseButton.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import { unmountComponentAtNode } from 'react-dom';
