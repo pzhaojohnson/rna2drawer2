1682471525f6b969dc25fbfd28b55f746ee46d3e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 09:13:45 -0400

    Refactor pr parameters to pair

--- src/partners/Pair.ts
+++ src/partners/Pair.ts
@@ -6,13 +6,13 @@ export type Pair = [number, number];
 /**
  * Returns the upstream partner of the pair.
  */
-export function upstreamPartner(pr: Pair): number {
-  return Math.min(...pr);
+export function upstreamPartner(pair: Pair): number {
+  return Math.min(...pair);
 }
 
 /**
  * Returns the downstream partner of the pair.
  */
-export function downstreamPartner(pr: Pair): number {
-  return Math.max(...pr);
+export function downstreamPartner(pair: Pair): number {
+  return Math.max(...pair);
 }
