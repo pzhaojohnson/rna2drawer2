b68b1f2e00d170d336c19923bdb373f182ec20b5
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 09:33:18 -0400

    Add comment

--- src/partners/pairs/pairsAreEqual.ts
+++ src/partners/pairs/pairsAreEqual.ts
@@ -2,6 +2,10 @@ import { Pair } from 'Partners/pairs/Pair';
 import { upstreamPartner } from 'Partners/pairs/Pair';
 import { downstreamPartner } from 'Partners/pairs/Pair';
 
+/**
+ * Returns true if the two pairs have the same upstream
+ * and downstream partners and false otherwise.
+ */
 export function pairsAreEqual(pair1: Pair, pair2: Pair): boolean {
   return (
     upstreamPartner(pair1) == upstreamPartner(pair2)
