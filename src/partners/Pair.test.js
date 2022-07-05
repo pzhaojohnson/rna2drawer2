83e921b4808088e82b2af0bd74683c591f610d45
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 09:09:02 -0400

    Refactor partner5 and partner3 functions to their upstreamPartner and downstreamPartner aliases

--- src/partners/Pair.test.js
+++ src/partners/Pair.test.js
@@ -1,18 +1,16 @@
-import {
-  partner5,
-  partner3,
-} from './Pair';
+import { upstreamPartner } from './Pair';
+import { downstreamPartner } from './Pair';
 
-it('partner5 function', () => {
-  // 5' partner is first
-  expect(partner5([33, 78])).toBe(33);
-  // 5' partner is second
-  expect(partner5([89, 73])).toBe(73);
+it('upstreamPartner function', () => {
+  // upstream partner is first
+  expect(upstreamPartner([33, 78])).toBe(33);
+  // upstream partner is second
+  expect(upstreamPartner([89, 73])).toBe(73);
 });
 
-it('partner3 function', () => {
-  // 3' partner is second
-  expect(partner3([20, 41])).toBe(41);
-  // 3' partner is first
-  expect(partner3([66, 52])).toBe(66);
+it('downstreamPartner function', () => {
+  // downstream partner is second
+  expect(downstreamPartner([20, 41])).toBe(41);
+  // downstream partner is first
+  expect(downstreamPartner([66, 52])).toBe(66);
 });
--- src/partners/Pair.ts
+++ src/partners/Pair.ts
@@ -1,16 +1,11 @@
 export type Pair = [number, number];
 
-// returns the 5' partner of the pair
-export function partner5(pr: Pair): number {
+// returns the upstream partner of the pair
+export function upstreamPartner(pr: Pair): number {
   return Math.min(...pr);
 }
 
-// returns the 3' partner of the pair
-export function partner3(pr: Pair): number {
+// returns the downstream partner of the pair
+export function downstreamPartner(pr: Pair): number {
   return Math.max(...pr);
 }
-
-export {
-  partner5 as upstreamPartner,
-  partner3 as downstreamPartner,
-};
