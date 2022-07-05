31304f67918f00f406ed1210720f7b3b90979651
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 09:14:51 -0400

    Refactor it function calls to test function calls

--- src/partners/Pair.test.js
+++ src/partners/Pair.test.js
@@ -1,14 +1,14 @@
 import { upstreamPartner } from './Pair';
 import { downstreamPartner } from './Pair';
 
-it('upstreamPartner function', () => {
+test('upstreamPartner function', () => {
   // upstream partner is first
   expect(upstreamPartner([33, 78])).toBe(33);
   // upstream partner is second
   expect(upstreamPartner([89, 73])).toBe(73);
 });
 
-it('downstreamPartner function', () => {
+test('downstreamPartner function', () => {
   // downstream partner is second
   expect(downstreamPartner([20, 41])).toBe(41);
   // downstream partner is first
