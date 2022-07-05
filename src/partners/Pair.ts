fc151a3c6eff819259450e4d9348543ca37b73b1
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 09:11:04 -0400

    Convert single line comments to documentation comments

--- src/partners/Pair.ts
+++ src/partners/Pair.ts
@@ -1,11 +1,15 @@
 export type Pair = [number, number];
 
-// returns the upstream partner of the pair
+/**
+ * Returns the upstream partner of the pair.
+ */
 export function upstreamPartner(pr: Pair): number {
   return Math.min(...pr);
 }
 
-// returns the downstream partner of the pair
+/**
+ * Returns the downstream partner of the pair.
+ */
 export function downstreamPartner(pr: Pair): number {
   return Math.max(...pr);
 }
