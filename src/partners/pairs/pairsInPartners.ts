bc24e2d144a06b88ab4cab122aec292c9f98a27d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-05 11:24:37 -0400

    Refactor prs local variable to pairs

--- src/partners/pairs/pairsInPartners.ts
+++ src/partners/pairs/pairsInPartners.ts
@@ -5,12 +5,12 @@ import type { Pair } from 'Partners/pairs/Pair';
  * Returns an array of all the pairs in the structure.
  */
 export function pairsInPartners(partners: Partners): Pair[] {
-  let prs: Pair[] = [];
+  let pairs: Pair[] = [];
   partners.forEach((q, i) => {
     let p = i + 1;
     if (typeof q == 'number' && p < q) {
-      prs.push([p, q]);
+      pairs.push([p, q]);
     }
   });
-  return prs;
+  return pairs;
 }
