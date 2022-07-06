e80ccaa237a9dc8fc0dec02098b13ec18ac1ded8
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-06 17:37:13 -0400

    Add comment

--- src/partners/stems/stemEnclosesPosition.ts
+++ src/partners/stems/stemEnclosesPosition.ts
@@ -3,6 +3,13 @@ import { topPair } from 'Partners/Stem';
 import { upstreamPartner } from 'Partners/pairs/Pair';
 import { downstreamPartner } from 'Partners/pairs/Pair';
 
+/**
+ * Returns true if the position lies between the upstream
+ * and downstream sides of the stem and false otherwise.
+ *
+ * This function returns false if the position is inside the stem,
+ * though this behavior may not be firmly defined.
+ */
 export function stemEnclosesPosition(stem: Stem, position: number): boolean {
   return (
     upstreamPartner(topPair(stem)) < position
