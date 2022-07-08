317ab2d7e31548470c9987ad8942b8b6565f301d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 07:54:49 -0400

    Add blank line

--- src/partners/stems/stemContainsPosition.ts
+++ src/partners/stems/stemContainsPosition.ts
@@ -6,6 +6,7 @@ import { PairWrapper } from 'Partners/pairs/PairWrapper';
 export function stemContainsPosition(stem: Stem, p: number): boolean {
   let bp = new PairWrapper(bottomPair(stem));
   let tp = new PairWrapper(topPair(stem));
+
   return (
     (p >= bp.upstreamPartner && p <= tp.upstreamPartner)
     || (p <= bp.downstreamPartner && p >= tp.downstreamPartner)
