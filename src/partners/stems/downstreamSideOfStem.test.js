bd44353a8c8ef6156469900fd2cebdc1de8550c8
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 07:43:03 -0400

    Use createStem function to create stems

--- src/partners/stems/downstreamSideOfStem.test.js
+++ src/partners/stems/downstreamSideOfStem.test.js
@@ -1,16 +1,17 @@
+import { createStem } from 'Partners/stems/Stem';
 import { sortNumbers } from 'Array/sortNumbers';
 
 import { downstreamSideOfStem } from './downstreamSideOfStem';
 
 describe('downstreamSideOfStem function', () => {
   test('a stem with just one pair', () => {
-    let stem = { position5: 12, position3: 33, size: 1 };
+    let stem = createStem({ bottomPair: [12, 33], numPairs: 1 });
     let side = downstreamSideOfStem(stem);
     expect(side).toStrictEqual([33]);
   });
 
   test('a stem with more than one pair', () => {
-    let stem = { position5: 123, position3: 158, size: 5 };
+    let stem = createStem({ bottomPair: [158, 123], numPairs: 5 });
     let side = downstreamSideOfStem(stem);
     sortNumbers(side);
     expect(side).toStrictEqual([154, 155, 156, 157, 158]);
