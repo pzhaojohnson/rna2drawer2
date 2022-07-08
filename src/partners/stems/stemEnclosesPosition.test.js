659099b8d8299e32210fef1e4014e9ddcc773615
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 08:38:26 -0400

    Use createStem function to create stems

--- src/partners/stems/stemEnclosesPosition.test.js
+++ src/partners/stems/stemEnclosesPosition.test.js
@@ -1,8 +1,10 @@
+import { createStem } from 'Partners/stems/Stem';
+
 import { stemEnclosesPosition } from './stemEnclosesPosition';
 
 describe('stemEnclosesPosition function', () => {
   test('when no positions are enclosed by the stem', () => {
-    let stem = { position5: 9, position3: 12, size: 2 };
+    let stem = createStem({ bottomPair: [9, 12], numPairs: 2 });
     expect(stemEnclosesPosition(stem, 8)).toBeFalsy(); // before stem
     expect(stemEnclosesPosition(stem, 9)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 10)).toBeFalsy(); // in upstream side
@@ -12,7 +14,7 @@ describe('stemEnclosesPosition function', () => {
   });
 
   test('when there are positions enclosed by the stem', () => {
-    let stem = { position5: 6, position3: 13, size: 3 };
+    let stem = createStem({ bottomPair: [13, 6], numPairs: 3 });
     expect(stemEnclosesPosition(stem, 5)).toBeFalsy(); // before stem
     expect(stemEnclosesPosition(stem, 6)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 7)).toBeFalsy(); // in upstream side
