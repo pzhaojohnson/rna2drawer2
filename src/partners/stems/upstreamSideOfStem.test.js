de086f0ccf879151b34780b8720361b280bd3d50
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 07:47:48 -0400

    Edit test names

--- src/partners/stems/upstreamSideOfStem.test.js
+++ src/partners/stems/upstreamSideOfStem.test.js
@@ -3,13 +3,13 @@ import { sortNumbers } from 'Array/sortNumbers';
 import { upstreamSideOfStem } from './upstreamSideOfStem';
 
 describe('upstreamSideOfStem function', () => {
-  test('a stem of size one', () => {
+  test('a stem with just one pair', () => {
     let stem = { position5: 12, position3: 33, size: 1 };
     let side = upstreamSideOfStem(stem);
     expect(side).toStrictEqual([12]);
   });
 
-  test('a stem of size greater than one', () => {
+  test('a stem with more than one pair', () => {
     let stem = { position5: 123, position3: 156, size: 5 };
     let side = upstreamSideOfStem(stem);
     sortNumbers(side);
