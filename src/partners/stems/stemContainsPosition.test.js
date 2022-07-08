f60a0bbf071068c2160b5f1cd0eeabb535fe2c41
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 08:08:38 -0400

    Refactor st local variables to stem

--- src/partners/stems/stemContainsPosition.test.js
+++ src/partners/stems/stemContainsPosition.test.js
@@ -4,54 +4,54 @@ import { stemContainsPosition } from './stemContainsPosition';
 
 describe('stemContainsPosition function', () => {
   test('a stem with just one pair', () => {
-    let st = createStem({ bottomPair: [20, 31], numPairs: 1 });
+    let stem = createStem({ bottomPair: [20, 31], numPairs: 1 });
 
     // contained
-    expect(stemContainsPosition(st, 20)).toBeTruthy();
-    expect(stemContainsPosition(st, 31)).toBeTruthy();
+    expect(stemContainsPosition(stem, 20)).toBeTruthy();
+    expect(stemContainsPosition(stem, 31)).toBeTruthy();
 
     // just outside edges
-    expect(stemContainsPosition(st, 19)).toBeFalsy();
-    expect(stemContainsPosition(st, 21)).toBeFalsy();
-    expect(stemContainsPosition(st, 32)).toBeFalsy();
-    expect(stemContainsPosition(st, 30)).toBeFalsy();
+    expect(stemContainsPosition(stem, 19)).toBeFalsy();
+    expect(stemContainsPosition(stem, 21)).toBeFalsy();
+    expect(stemContainsPosition(stem, 32)).toBeFalsy();
+    expect(stemContainsPosition(stem, 30)).toBeFalsy();
 
     // further outside
-    expect(stemContainsPosition(st, 6)).toBeFalsy();
-    expect(stemContainsPosition(st, 27)).toBeFalsy();
-    expect(stemContainsPosition(st, 42)).toBeFalsy();
+    expect(stemContainsPosition(stem, 6)).toBeFalsy();
+    expect(stemContainsPosition(stem, 27)).toBeFalsy();
+    expect(stemContainsPosition(stem, 42)).toBeFalsy();
   });
 
   test('a stem with more than one pair', () => {
-    let st = createStem({ bottomPair: [45, 12], numPairs: 3 });
+    let stem = createStem({ bottomPair: [45, 12], numPairs: 3 });
 
     // contained
-    expect(stemContainsPosition(st, 12)).toBeTruthy();
-    expect(stemContainsPosition(st, 13)).toBeTruthy();
-    expect(stemContainsPosition(st, 14)).toBeTruthy();
-    expect(stemContainsPosition(st, 45)).toBeTruthy();
-    expect(stemContainsPosition(st, 44)).toBeTruthy();
-    expect(stemContainsPosition(st, 43)).toBeTruthy();
+    expect(stemContainsPosition(stem, 12)).toBeTruthy();
+    expect(stemContainsPosition(stem, 13)).toBeTruthy();
+    expect(stemContainsPosition(stem, 14)).toBeTruthy();
+    expect(stemContainsPosition(stem, 45)).toBeTruthy();
+    expect(stemContainsPosition(stem, 44)).toBeTruthy();
+    expect(stemContainsPosition(stem, 43)).toBeTruthy();
 
     // just outside edges
-    expect(stemContainsPosition(st, 11)).toBeFalsy();
-    expect(stemContainsPosition(st, 15)).toBeFalsy();
-    expect(stemContainsPosition(st, 46)).toBeFalsy();
-    expect(stemContainsPosition(st, 42)).toBeFalsy();
+    expect(stemContainsPosition(stem, 11)).toBeFalsy();
+    expect(stemContainsPosition(stem, 15)).toBeFalsy();
+    expect(stemContainsPosition(stem, 46)).toBeFalsy();
+    expect(stemContainsPosition(stem, 42)).toBeFalsy();
 
     // further outside
-    expect(stemContainsPosition(st, 3)).toBeFalsy();
-    expect(stemContainsPosition(st, 26)).toBeFalsy();
-    expect(stemContainsPosition(st, 88)).toBeFalsy();
+    expect(stemContainsPosition(stem, 3)).toBeFalsy();
+    expect(stemContainsPosition(stem, 26)).toBeFalsy();
+    expect(stemContainsPosition(stem, 88)).toBeFalsy();
   });
 
   test('invalid positions', () => {
-    let st = createStem({ bottomPair: [6, 30], numPairs: 6 });
-    expect(stemContainsPosition(st, 0)).toBeFalsy(); // zero
-    expect(stemContainsPosition(st, -1)).toBeFalsy(); // negative
+    let stem = createStem({ bottomPair: [6, 30], numPairs: 6 });
+    expect(stemContainsPosition(stem, 0)).toBeFalsy(); // zero
+    expect(stemContainsPosition(stem, -1)).toBeFalsy(); // negative
     // nonfinite
-    expect(stemContainsPosition(st, NaN)).toBeFalsy();
-    expect(stemContainsPosition(st, Infinity)).toBeFalsy();
-    expect(stemContainsPosition(st, -Infinity)).toBeFalsy();
+    expect(stemContainsPosition(stem, NaN)).toBeFalsy();
+    expect(stemContainsPosition(stem, Infinity)).toBeFalsy();
+    expect(stemContainsPosition(stem, -Infinity)).toBeFalsy();
   });
 });
