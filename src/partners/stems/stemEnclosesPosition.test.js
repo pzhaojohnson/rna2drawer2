32b3eea763b2afa446632795ef67dd0de2307b0e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 08:42:10 -0400

    Add test cases for positions further away from the stems being tested

--- src/partners/stems/stemEnclosesPosition.test.js
+++ src/partners/stems/stemEnclosesPosition.test.js
@@ -5,17 +5,20 @@ import { stemEnclosesPosition } from './stemEnclosesPosition';
 describe('stemEnclosesPosition function', () => {
   test('when no positions are enclosed by the stem', () => {
     let stem = createStem({ bottomPair: [9, 12], numPairs: 2 });
-    expect(stemEnclosesPosition(stem, 8)).toBeFalsy(); // before stem
+    expect(stemEnclosesPosition(stem, 3)).toBeFalsy(); // further before stem
+    expect(stemEnclosesPosition(stem, 8)).toBeFalsy(); // immediately before stem
     expect(stemEnclosesPosition(stem, 9)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 10)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 11)).toBeFalsy(); // in downstream side
     expect(stemEnclosesPosition(stem, 12)).toBeFalsy(); // in downstream side
-    expect(stemEnclosesPosition(stem, 13)).toBeFalsy(); // after stem
+    expect(stemEnclosesPosition(stem, 13)).toBeFalsy(); // immediately after stem
+    expect(stemEnclosesPosition(stem, 20)).toBeFalsy(); // further after stem
   });
 
   test('when there are positions enclosed by the stem', () => {
     let stem = createStem({ bottomPair: [13, 6], numPairs: 3 });
-    expect(stemEnclosesPosition(stem, 5)).toBeFalsy(); // before stem
+    expect(stemEnclosesPosition(stem, 2)).toBeFalsy(); // further before stem
+    expect(stemEnclosesPosition(stem, 5)).toBeFalsy(); // immediately before stem
     expect(stemEnclosesPosition(stem, 6)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 7)).toBeFalsy(); // in upstream side
     expect(stemEnclosesPosition(stem, 8)).toBeFalsy(); // in upstream side
@@ -24,6 +27,7 @@ describe('stemEnclosesPosition function', () => {
     expect(stemEnclosesPosition(stem, 11)).toBeFalsy(); // in downstream side
     expect(stemEnclosesPosition(stem, 12)).toBeFalsy(); // in downstream side
     expect(stemEnclosesPosition(stem, 13)).toBeFalsy(); // in downstream side
-    expect(stemEnclosesPosition(stem, 14)).toBeFalsy(); // after stem
+    expect(stemEnclosesPosition(stem, 14)).toBeFalsy(); // immediately after stem
+    expect(stemEnclosesPosition(stem, 18)).toBeFalsy(); // further after stem
   });
 });
