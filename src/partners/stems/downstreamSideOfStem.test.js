78c1875e253f93412a70728e7c79f00c20487213
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-08 07:40:30 -0400

    Rename tests

--- src/partners/stems/downstreamSideOfStem.test.js
+++ src/partners/stems/downstreamSideOfStem.test.js
@@ -3,13 +3,13 @@ import { sortNumbers } from 'Array/sortNumbers';
 import { downstreamSideOfStem } from './downstreamSideOfStem';
 
 describe('downstreamSideOfStem function', () => {
-  test('a stem of size one', () => {
+  test('a stem with just one pair', () => {
     let stem = { position5: 12, position3: 33, size: 1 };
     let side = downstreamSideOfStem(stem);
     expect(side).toStrictEqual([33]);
   });
 
-  test('a stem of size greater than one', () => {
+  test('a stem with more than one pair', () => {
     let stem = { position5: 123, position3: 158, size: 5 };
     let side = downstreamSideOfStem(stem);
     sortNumbers(side);
