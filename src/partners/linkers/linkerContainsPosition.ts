89c74e972f653e38836cf179f98687c236f25f46
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-09 11:06:09 -0400

    Use upstreamBoundingPosition and downstreamBoundingPosition functions in linkerContainsPosition function

--- src/partners/linkers/linkerContainsPosition.ts
+++ src/partners/linkers/linkerContainsPosition.ts
@@ -1,5 +1,10 @@
 import type { Linker } from 'Partners/linkers/Linker';
+import { upstreamBoundingPosition } from 'Partners/linkers/Linker';
+import { downstreamBoundingPosition } from 'Partners/linkers/Linker';
 
 export function linkerContainsPosition(linker: Linker, p: number): boolean {
-  return p > linker.boundingPosition5 && p < linker.boundingPosition3;
+  return (
+    p > upstreamBoundingPosition(linker)
+    && p < downstreamBoundingPosition(linker)
+  );
 }
