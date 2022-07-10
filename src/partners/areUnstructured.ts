fa43bea3378027b154b52842643709ceeef3a308
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-10 13:32:17 -0400

    Make type import explicit

--- src/partners/areUnstructured.ts
+++ src/partners/areUnstructured.ts
@@ -1,4 +1,4 @@
-import { Partners } from 'Partners/Partners';
+import type { Partners } from 'Partners/Partners';
 
 export function areUnstructured(partners: Partners): boolean {
   return partners.every(q => typeof q != 'number');
