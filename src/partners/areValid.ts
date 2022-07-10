55cde3e8274def2e3665993a2be8ba6278ece0b2
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-10 13:56:57 -0400

    Check if partner value is not nullish instead of just if it is a number

--- src/partners/areValid.ts
+++ src/partners/areValid.ts
@@ -4,7 +4,7 @@ export function areValid(partners: Partners): boolean {
   let valid = true;
   partners.forEach((q, i) => {
     let p = i + 1;
-    if (typeof q == 'number') {
+    if (q != undefined) {
       let pairIsValid = (
         Number.isInteger(q)
         && partners[q - 1] == p
