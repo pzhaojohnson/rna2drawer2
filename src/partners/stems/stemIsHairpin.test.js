731bc793f4dbeab969a4dfa51864fe52b368146b
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-13 15:13:12 -0400

    Add describe blocks and reword test names

--- src/partners/stems/stemIsHairpin.test.js
+++ src/partners/stems/stemIsHairpin.test.js
@@ -3,54 +3,58 @@ import { createStem } from 'Partners/stems/Stem';
 import { stemIsHairpin } from './stemIsHairpin';
 
 describe('stemIsHairpin function', () => {
-  test('a hairpin with loop of size zero', () => {
-    let partners = [
-      null, null, null,
-      9, 8, 7,
-      6, 5, 4,
-      null, null,
-    ];
-    let stem = createStem({ bottomPair: [4, 9], numPairs: 3 });
-    expect(stemIsHairpin(partners, stem)).toBeTruthy();
+  describe('when the stem is a hairpin', () => {
+    test('when there are no enclosed positions', () => {
+      let partners = [
+        null, null, null,
+        9, 8, 7,
+        6, 5, 4,
+        null, null,
+      ];
+      let stem = createStem({ bottomPair: [4, 9], numPairs: 3 });
+      expect(stemIsHairpin(partners, stem)).toBeTruthy();
+    });
+
+    test('when there are multiple enclosed positions', () => {
+      let partners = [
+        null, null, null,
+        14, 13, 12,
+        null, null, null, null, null,
+        6, 5, 4,
+        null, null,
+      ];
+      let stem = createStem({ bottomPair: [4, 14], numPairs: 3 });
+      expect(stemIsHairpin(partners, stem)).toBeTruthy();
+    });
   });
 
-  test('a hairpin with loop of size greater than zero', () => {
-    let partners = [
-      null, null, null,
-      14, 13, 12,
-      null, null, null, null, null,
-      6, 5, 4,
-      null, null,
-    ];
-    let stem = createStem({ bottomPair: [4, 14], numPairs: 3 });
-    expect(stemIsHairpin(partners, stem)).toBeTruthy();
-  });
+  describe('when the stem is not a hairpin', () => {
+    test('a leading pseudoknot', () => {
+      let partners = [
+        null, null,
+        12, 11,
+        null, null,
+        18, 17, 16, 15,
+        4, 3,
+        null, null,
+        10, 9, 8, 7,
+      ];
+      let stem = createStem({ bottomPair: [7, 18], numPairs: 4 });
+      expect(stemIsHairpin(partners, stem)).toBeFalsy();
+    });
 
-  test('a hairpin whose leading enclosed positions are paired', () => {
-    let partners = [
-      null, null,
-      12, 11,
-      null, null,
-      18, 17, 16, 15,
-      4, 3,
-      null, null,
-      10, 9, 8, 7,
-    ];
-    let stem = createStem({ bottomPair: [7, 18], numPairs: 4 });
-    expect(stemIsHairpin(partners, stem)).toBeFalsy();
-  });
-
-  test('a hairpin whose trailing enclosed positions are paired', () => {
-    let partners = [
-      null, null,
-      14, 13,
-      null, null,
-      18, 17, 16, 15,
-      null, null,
-      4, 3,
-      10, 9, 8, 7,
-    ];
-    let stem = createStem({ bottomPair: [7, 18], numPairs: 4 });
-    expect(stemIsHairpin(partners, stem)).toBeFalsy();
+    test('a trailing pseudoknot', () => {
+      let partners = [
+        null, null,
+        14, 13,
+        null, null,
+        18, 17, 16, 15,
+        null, null,
+        4, 3,
+        10, 9, 8, 7,
+      ];
+      let stem = createStem({ bottomPair: [7, 18], numPairs: 4 });
+      expect(stemIsHairpin(partners, stem)).toBeFalsy();
+    });
   });
 });
