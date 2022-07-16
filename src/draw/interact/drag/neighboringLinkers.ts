cf26739ea706ebd64ed18e6ede89ba77f36f2ea1
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-16 18:27:47 -0500

    Use upstreamNeighboringLinker and downstreamNeighboringLinker functions in leadingLinker and trailingLinker functions

--- src/draw/interact/drag/neighboringLinkers.ts
+++ src/draw/interact/drag/neighboringLinkers.ts
@@ -1,40 +1,16 @@
 import { Partners } from 'Partners/Partners';
-
 import { Stem } from 'Partners/stems/Stem';
-import { bottomPair } from 'Partners/stems/Stem';
-import { upstreamPartner } from 'Partners/pairs/Pair';
-import { downstreamPartner } from 'Partners/pairs/Pair';
-
 import { Linker } from 'Partners/linkers/Linker';
-import { createLinker } from 'Partners/linkers/Linker';
-import { containingUnpairedRegion as linkerContainingPosition } from 'Partners/containing';
+
+import { upstreamNeighboringLinker } from 'Partners/stems/neighboringLinkers';
+import { downstreamNeighboringLinker } from 'Partners/stems/neighboringLinkers';
 
 // returns the linker immediately before the stem
 export function leadingLinker(partners: Partners, stem: Stem): Linker {
-  let p = upstreamPartner(bottomPair(stem));
-  if (p == 1) {
-    return createLinker({ upstreamBoundingPosition: 0, downstreamBoundingPosition: 1 });
-  } else {
-    let linker = linkerContainingPosition(partners, p - 1);
-    if (linker) {
-      return linker;
-    } else {
-      return createLinker({ upstreamBoundingPosition: p - 1, downstreamBoundingPosition: p });
-    }
-  }
+  return upstreamNeighboringLinker(partners, { stem });
 }
 
 // returns the linker immediately after the stem
 export function trailingLinker(partners: Partners, stem: Stem): Linker {
-  let p = downstreamPartner(bottomPair(stem));
-  if (p == partners.length) {
-    return createLinker({ upstreamBoundingPosition: partners.length, downstreamBoundingPosition: partners.length + 1 });
-  } else {
-    let linker = linkerContainingPosition(partners, p + 1);
-    if (linker) {
-      return linker;
-    } else {
-      return createLinker({ upstreamBoundingPosition: p, downstreamBoundingPosition: p + 1 });
-    }
-  }
+  return downstreamNeighboringLinker(partners, { stem });
 }
