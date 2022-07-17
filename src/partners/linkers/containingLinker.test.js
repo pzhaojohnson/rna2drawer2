6e02b7f93f4f036e996abbd41a129787c06f60fe
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-16 20:47:32 -0500

    Use createLinker function to create linkers

--- src/partners/linkers/containingLinker.test.js
+++ src/partners/linkers/containingLinker.test.js
@@ -1,3 +1,5 @@
+import { createLinker } from 'Partners/linkers/Linker';
+
 import { containingLinker } from './containingLinker';
 
 describe('containingLinker function', () => {
@@ -5,63 +7,63 @@ describe('containingLinker function', () => {
     it('a hairpin loop', () => {
       let partners = [9, 8, null, undefined, null, undefined, undefined, 2, 1];
       expect(containingLinker(partners, { position: 6 })).toStrictEqual(
-        { boundingPosition5: 2, boundingPosition3: 8 }
+        createLinker({ upstreamBoundingPosition: 2, downstreamBoundingPosition: 8 })
       );
     });
 
     it('between two stems', () => {
       let partners = [6, 5, null, null, 2, 1, undefined, null, null, 15, 14, null, null, 11, 10];
       expect(containingLinker(partners, { position: 8 })).toStrictEqual(
-        { boundingPosition5: 6, boundingPosition3: 10 }
+        createLinker({ upstreamBoundingPosition: 6, downstreamBoundingPosition: 10 })
       );
     });
 
     it('position is immediately downstream of a stem', () => {
       let partners = [6, 5, null, null, 2, 1, undefined, null, null, 15, 14, null, null, 11, 10];
       expect(containingLinker(partners, { position: 7 })).toStrictEqual(
-        { boundingPosition5: 6, boundingPosition3: 10 }
+        createLinker({ upstreamBoundingPosition: 6, downstreamBoundingPosition: 10 })
       );
     });
 
     it('position is immediately upstream of a stem', () => {
       let partners = [6, 5, null, null, 2, 1, undefined, null, null, 15, 14, null, null, 11, 10];
       expect(containingLinker(partners, { position: 9 })).toStrictEqual(
-        { boundingPosition5: 6, boundingPosition3: 10 }
+        createLinker({ upstreamBoundingPosition: 6, downstreamBoundingPosition: 10 })
       );
     });
 
     it("unpaired region is at 5' end", () => {
       let partners = [undefined, null, null, undefined, 11, 10, undefined, null, null, 6, 5];
       expect(containingLinker(partners, { position: 3 })).toStrictEqual(
-        { boundingPosition5: 0, boundingPosition3: 5 }
+        createLinker({ upstreamBoundingPosition: 0, downstreamBoundingPosition: 5 })
       );
     });
 
     it("unpaired region is at 3' end", () => {
       let partners = [6, 5, null, null, 2, 1, undefined, undefined, null];
       expect(containingLinker(partners, { position: 8 })).toStrictEqual(
-        { boundingPosition5: 6, boundingPosition3: 10 }
+        createLinker({ upstreamBoundingPosition: 6, downstreamBoundingPosition: 10 })
       );
     });
 
     it('position is the first position', () => {
       let partners = [undefined, null, null, undefined, 11, 10, undefined, null, null, 6, 5];
       expect(containingLinker(partners, { position: 1 })).toStrictEqual(
-        { boundingPosition5: 0, boundingPosition3: 5 }
+        createLinker({ upstreamBoundingPosition: 0, downstreamBoundingPosition: 5 })
       );
     });
 
     it('position is the last position', () => {
       let partners = [6, 5, null, null, 2, 1, undefined, undefined, null];
       expect(containingLinker(partners, { position: partners.length })).toStrictEqual(
-        { boundingPosition5: 6, boundingPosition3: 10 }
+        createLinker({ upstreamBoundingPosition: 6, downstreamBoundingPosition: 10 })
       );
     });
 
     it('unstructured partners', () => {
       let partners = [null, undefined, null, undefined, undefined];
       expect(containingLinker(partners, { position: 2 })).toStrictEqual(
-        { boundingPosition5: 0, boundingPosition3: 6 }
+        createLinker({ upstreamBoundingPosition: 0, downstreamBoundingPosition: 6 })
       );
     });
   });
