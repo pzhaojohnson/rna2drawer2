f451cf86808e404411b13d6fcca3fe624f0de82e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-17 14:29:57 -0500

    Convert if statement conditionals to guard clauses

--- src/partners/stems/containingStem.ts
+++ src/partners/stems/containingStem.ts
@@ -14,20 +14,25 @@ import { downstreamPartner } from 'Partners/pairs/Pair';
  * or undefined if the position is not in a stem.
  */
 export function containingStem(partners: Partners, p: number): Stem | undefined {
-  if (positionIsInRange(partners, p)) {
-    let q = partnerOf(partners, p);
-    if (typeof q == 'number') {
-      let p5 = upstreamPartner([p, q]);
-      let p3 = downstreamPartner([p, q]);
-      while (p5 - 1 > 0 && p3 + 1 <= partners.length && arePaired(partners, p5 - 1, p3 + 1)) {
-        p5--;
-        p3++;
-      }
-      let s = 1;
-      while (p5 + s < p3 - s && arePaired(partners, p5 + s, p3 - s)) {
-        s++;
-      }
-      return { position5: p5, position3: p3, size: s };
-    }
+  if (!positionIsInRange(partners, p)) {
+    return undefined;
   }
+
+  let q = partnerOf(partners, p);
+
+  if (typeof q != 'number') {
+    return undefined;
+  }
+
+  let p5 = upstreamPartner([p, q]);
+  let p3 = downstreamPartner([p, q]);
+  while (p5 - 1 > 0 && p3 + 1 <= partners.length && arePaired(partners, p5 - 1, p3 + 1)) {
+    p5--;
+    p3++;
+  }
+  let s = 1;
+  while (p5 + s < p3 - s && arePaired(partners, p5 + s, p3 - s)) {
+    s++;
+  }
+  return { position5: p5, position3: p3, size: s };
 }
