52fbcb0d3a4edc5e9bfd0746433a2969ca821131
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-18 09:56:53 -0500

    Convert it function calls to test function calls and reword test names

--- src/partners/hasKnots.test.js
+++ src/partners/hasKnots.test.js
@@ -1,22 +1,22 @@
 import { hasKnots } from './hasKnots';
 
 describe('hasKnots function', () => {
-  describe('has no knots', () => {
-    it('empty', () => {
+  describe('when the structure has no knots', () => {
+    test('empty partners', () => {
       expect(hasKnots([])).toBeFalsy();
     });
 
-    it('unstructured', () => {
+    test('unstructured partners', () => {
       let partners = [undefined, null, null, undefined];
       expect(hasKnots(partners)).toBeFalsy();
     });
 
-    it('a knotless structure', () => {
+    test('a stem enclosed by another stem', () => {
       let partners = [15, 14, null, 10, 9, undefined, null, null, 5, 4, undefined, null, null, 2, 1];
       expect(hasKnots(partners)).toBeFalsy();
     });
 
-    it('has unassigned positions', () => {
+    test('when there are unassigned positions', () => {
       let partners = [];
       partners[8 - 1] = 2;
       partners[2 - 1] = 8;
@@ -26,23 +26,23 @@ describe('hasKnots function', () => {
     });
   });
 
-  describe('has knots', () => {
-    it('knot starts upstream of hairpin', () => {
+  describe('when the structure has knots', () => {
+    test('a leading pseudoknot', () => {
       let partners = [null, 12, 11, 10, null, 15, 14, 13, null, 4, 3, 2, 8, 7, 6, undefined];
       expect(hasKnots(partners)).toBeTruthy();
     });
 
-    it('knot ends downstream of hairpin', () => {
+    test('a trailing pseudoknot', () => {
       let partners = [10, 9, 8, 15, 14, 13, null, 3, 2, 1, undefined, null, 6, 5, 4];
       expect(hasKnots(partners)).toBeTruthy();
     });
 
-    it('kissing loops', () => {
+    test('a pseudoknot involving an internal loop', () => {
       let partners = [19, 18, 17, null, 13, 12, 11, null, undefined, null, 7, 6, 5, 21, 20, null, 3, 2, 1, 15, 14]
       expect(hasKnots(partners)).toBeTruthy();
     });
 
-    it('has unassigned positions', () => {
+    test('when there are unassigned positions', () => {
       let partners = [];
       partners[2 - 1] = 9;
       partners[9 - 1] = 2;
