d07841ef1e3f85b052305219e5716ba661a0a56a
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-18 09:43:02 -0500

    Refactor upstreamStack to upstreamPartners

--- src/partners/hasKnots.ts
+++ src/partners/hasKnots.ts
@@ -2,14 +2,14 @@ import type { Partners } from 'Partners/Partners';
 
 export function hasKnots(partners: Partners): boolean {
   let knots = false;
-  let upstreamStack: number[] = [];
+  let upstreamPartners: number[] = [];
   partners.forEach((q, i) => {
     let p = i + 1;
     if (typeof q == 'number') {
       if (p < q) {
-        upstreamStack.push(p);
+        upstreamPartners.push(p);
       } else {
-        if (q != upstreamStack.pop()) {
+        if (q != upstreamPartners.pop()) {
           knots = true;
         }
       }
