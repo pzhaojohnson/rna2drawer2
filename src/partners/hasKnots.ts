ae0dae7f606aca05e5834718f5ce1c0422b525bc
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-18 09:45:22 -0500

    Flatten logic

--- src/partners/hasKnots.ts
+++ src/partners/hasKnots.ts
@@ -5,14 +5,12 @@ export function hasKnots(partners: Partners): boolean {
   let upstreamPartners: number[] = [];
   partners.forEach((q, i) => {
     let p = i + 1;
-    if (typeof q == 'number') {
-      if (p < q) {
-        upstreamPartners.push(p);
-      } else {
-        if (q != upstreamPartners.pop()) {
-          knots = true;
-        }
-      }
+    if (typeof q != 'number') {
+      // nothing to do
+    } else if (p < q) {
+      upstreamPartners.push(p);
+    } else if (q != upstreamPartners.pop()) {
+      knots = true;
     }
   });
   return knots;
