aaa4e6dc4df71be9a272912f2ceebbfb840a2ccb
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-21 22:39:18 -0400

    Use scroll interface of drawing in view module

--- src/draw/view.ts
+++ src/draw/view.ts
@@ -3,20 +3,20 @@ import { Point2D as Point } from 'Math/points/Point';
 
 export function centerOfView(drawing: Drawing): Point {
   return {
-    x: drawing.scrollLeft + (window.innerWidth / 2),
-    y: drawing.scrollTop + (window.innerHeight / 2),
+    x: drawing.scroll.left + (window.innerWidth / 2),
+    y: drawing.scroll.top + (window.innerHeight / 2),
   };
 }
 
 export function centerViewOn(drawing: Drawing, p: Point) {
-  drawing.scrollLeft = p.x - (window.innerWidth / 2);
-  drawing.scrollTop = p.y - (window.innerHeight / 2);
+  drawing.scroll.left = p.x - (window.innerWidth / 2);
+  drawing.scroll.top = p.y - (window.innerHeight / 2);
 }
 
 // centers view on the center coordinates of the drawing
 export function centerView(drawing: Drawing) {
   centerViewOn(drawing, {
-    x: drawing.scrollWidth / 2,
-    y: drawing.scrollHeight / 2,
+    x: drawing.scroll.width / 2,
+    y: drawing.scroll.height / 2,
   });
 }
