62900ce36fec5bf934e06886a0591252be82dd10
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-22 21:12:34 -0400

    Add comments

--- src/draw/bases/numberings/z.ts
+++ src/draw/bases/numberings/z.ts
@@ -2,10 +2,10 @@ import type { BaseNumbering } from 'Draw/bases/numberings/BaseNumbering';
 
 export function bringToFront(bn: BaseNumbering) {
   bn.line.front();
-  bn.text.front();
+  bn.text.front(); // text above line
 }
 
 export function sendToBack(bn: BaseNumbering) {
   bn.text.back();
-  bn.line.back();
+  bn.line.back(); // line below text
 }
