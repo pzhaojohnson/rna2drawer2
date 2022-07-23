c9f94e960d43ca177a52a881af9ec4bf1337c6f4
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-23 09:50:32 -0400

    Add comment

--- src/draw/bases/numberings/isNumbered.ts
+++ src/draw/bases/numberings/isNumbered.ts
@@ -1,5 +1,8 @@
 import type { Base } from 'Draw/bases/Base';
 
+/**
+ * Returns true if the base has numbering and false otherwise.
+ */
 export function isNumbered(b: Base): boolean {
   return b.numbering ? true : false;
 }
