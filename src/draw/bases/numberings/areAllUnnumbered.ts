c2289ee506f2a6791b5b71a6554520a70e4aec4d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-23 10:02:23 -0400

    Convert single line comments to documentation comment

--- src/draw/bases/numberings/areAllUnnumbered.ts
+++ src/draw/bases/numberings/areAllUnnumbered.ts
@@ -1,11 +1,13 @@
 import type { Base } from 'Draw/bases/Base';
 
-// Returns true if none of the given bases have numberings
-// and false otherwise.
-//
-// Currently, returns true for an empty array, though
-// it is not firmly defined what should be returned
-// for an empty array.
+/**
+ * Returns true if none of the given bases have numberings
+ * and false otherwise.
+ *
+ * Currently, returns true for an empty array, though
+ * it is not firmly defined what should be returned
+ * for an empty array.
+ */
 export function areAllUnnumbered(bs: Base[]): boolean {
   return bs.every(b => !b.numbering);
 }
