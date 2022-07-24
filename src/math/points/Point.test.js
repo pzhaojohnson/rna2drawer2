864420710af2f37858107907a357447f1fe628e8
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-24 13:50:25 -0400

    Rework isPoint2D function to be able to accept any value

--- src/math/points/Point.test.js
+++ src/math/points/Point.test.js
@@ -1,38 +1,28 @@
 import { isPoint2D } from './Point';
 import { deepCopyPoint2D } from './Point';
 
-describe('isPoint2D function', () => {
-  it('returns true for 2D points', () => {
-    [
-      { x: 1, y: 2 },
-      { x: 0, y: 0 },
-      { x: -100, y: -50.5 },
-      { x: 211.4, y: 45.6 },
-    ].forEach(p => {
-      expect(isPoint2D(p)).toBeTruthy();
-    });
-  });
+test('isPoint2D function', () => {
+  // actual 2D points
+  expect(isPoint2D({ x: -1, y: 2 })).toBeTruthy();
+  expect(isPoint2D({ x: 58.9, y: -23.7 })).toBeTruthy();
 
-  it('returns false if x or y properties are not numeric', () => {
-    // empty object
-    expect(isPoint2D({})).toBeFalsy();
-    [
-      undefined,
-      null,
-      'asdf',
-      {},
-      true,
-    ].forEach(v => {
-      // x is not a number
-      expect(
-        isPoint2D({ x: v, y: 5 })
-      ).toBeFalsy();
-      // y is not a number
-      expect(
-        isPoint2D({ x: 5, y: v })
-      ).toBeFalsy();
-    });
-  });
+  // non-object values
+  expect(isPoint2D(1)).toBeFalsy();
+  expect(isPoint2D('asdf')).toBeFalsy();
+  expect(isPoint2D(true)).toBeFalsy();
+  expect(isPoint2D(null)).toBeFalsy();
+  expect(isPoint2D(undefined)).toBeFalsy();
+
+  // empty object
+  expect(isPoint2D({})).toBeFalsy();
+
+  // missing a coordinate
+  expect(isPoint2D({ x: 5 })).toBeFalsy();
+  expect(isPoint2D({ y: 8 })).toBeFalsy();
+
+  // one coordinate is the wrong type
+  expect(isPoint2D({ x: '9', y: 1 })).toBeFalsy();
+  expect(isPoint2D({ x: 2, y: '10' })).toBeFalsy();
 });
 
 test('deepCopyPoint2D function', () => {
--- src/math/points/Point.ts
+++ src/math/points/Point.ts
@@ -1,7 +1,12 @@
 export type Point2D = { x: number, y: number };
 
-export function isPoint2D(o: { x?: unknown, y?: unknown }): o is Point2D {
-  return typeof o.x == 'number' && typeof o.y == 'number';
+export function isPoint2D(v: any): v is Point2D {
+  return (
+    typeof v == 'object'
+    && v !== null
+    && typeof v.x == 'number'
+    && typeof v.y == 'number'
+  );
 }
 
 export function deepCopyPoint2D(point: Point2D): Point2D {
