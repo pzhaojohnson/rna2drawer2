d76e02de75c75407e25f41f7976d2a6f4cf1fc38
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:50:31 -0400

    Refactor it function calls to test function calls and reword test names

--- src/math/sum.test.js
+++ src/math/sum.test.js
@@ -1,11 +1,11 @@
 import { sum } from './sum';
 
 describe('sum function', () => {
-  it('empty list', () => {
+  test('an empty array of numbers', () => {
     expect(sum([])).toBe(0);
   });
 
-  it('nonempty lists', () => {
+  test('nonempty arrays of numbers', () => {
     expect(sum([5])).toBe(5);
     expect(sum([-1, 0.5, 11.2, 23])).toBeCloseTo(33.7);
     expect(sum([2, 2, -1, -1, -1, 5, 5.6])).toBeCloseTo(11.6);
