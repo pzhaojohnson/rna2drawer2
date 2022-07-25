b60b8ef8aa30243aa9e948528cfc3ba4f22684f4
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 09:44:35 -0400

    Add describe block

--- src/math/angles/normalizeAngle.test.js
+++ src/math/angles/normalizeAngle.test.js
@@ -1,21 +1,23 @@
 import { normalizeAngle } from './normalizeAngle';
 
-it('below minimum angle', () => {
-  expect(normalizeAngle(-Math.PI, 0)).toBeCloseTo(Math.PI, 6);
-});
+describe('normalizeAngle function', () => {
+  it('below minimum angle', () => {
+    expect(normalizeAngle(-Math.PI, 0)).toBeCloseTo(Math.PI, 6);
+  });
 
-it('equals minimum angle', () => {
-  expect(normalizeAngle(0, 0)).toBeCloseTo(0, 6);
-});
+  it('equals minimum angle', () => {
+    expect(normalizeAngle(0, 0)).toBeCloseTo(0, 6);
+  });
 
-it('above minimum angle', () => {
-  expect(normalizeAngle(4 * Math.PI, 0)).toBeCloseTo(0, 6);
-});
+  it('above minimum angle', () => {
+    expect(normalizeAngle(4 * Math.PI, 0)).toBeCloseTo(0, 6);
+  });
 
-it('already normalized', () => {
-  expect(normalizeAngle(Math.PI, 0)).toBeCloseTo(Math.PI, 6);
-});
+  it('already normalized', () => {
+    expect(normalizeAngle(Math.PI, 0)).toBeCloseTo(Math.PI, 6);
+  });
 
-it('2 * Math.PI above minimum angle', () => {
-  expect(normalizeAngle(2 * Math.PI, 0)).toBeCloseTo(0, 6);
+  it('2 * Math.PI above minimum angle', () => {
+    expect(normalizeAngle(2 * Math.PI, 0)).toBeCloseTo(0, 6);
+  });
 });
