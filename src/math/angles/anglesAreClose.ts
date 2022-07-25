f5c59b38021696b2db4fe31f861f5f76b622cb5e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 11:21:14 -0400

    Use round function in anglesAreClose function

--- src/math/angles/anglesAreClose.ts
+++ src/math/angles/anglesAreClose.ts
@@ -1,4 +1,5 @@
 import { normalizeAngle } from 'Math/angles/normalizeAngle';
+import { round } from 'Math/round';
 
 export type Options = { places: number };
 
@@ -12,7 +13,7 @@ export function anglesAreClose(a1: number, a2: number, options?: Options): boole
   a1 = normalizeAngle(a1);
   a2 = normalizeAngle(a2);
   let diff = Math.abs(a1 - a2);
-  diff = Number.parseFloat(diff.toFixed(places));
-  let pi2 = Number.parseFloat((2 * Math.PI).toFixed(places));
+  diff = round(diff, places);
+  let pi2 = round(2 * Math.PI, places);
   return diff == 0 || diff == pi2;
 }
