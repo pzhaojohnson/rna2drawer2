4144738f6dcf96b192cc6a70c1dd9adb1978c8fa
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:56:56 -0400

    Add comment

--- src/math/round.ts
+++ src/math/round.ts
@@ -1,3 +1,8 @@
+/**
+ * Rounds the number to the specified number of decimal places
+ * or to zero decimal places if the number of decimal places
+ * to round to is not specified.
+ */
 export function round(n: number, places=0): number {
   return Number.parseFloat(n.toFixed(places));
 }
