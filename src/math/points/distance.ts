75e5adcce5ead8b7819ac254ab3604f373301fc5
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 08:14:34 -0400

    Add comment

--- src/math/points/distance.ts
+++ src/math/points/distance.ts
@@ -2,6 +2,9 @@ import { Point2D } from 'Math/points/Point';
 import { subtract2D } from 'Math/points/displacement';
 import { magnitude2D } from 'Math/points/magnitude';
 
+/**
+ * Calculates the distance between the two points.
+ */
 export function distance2D(p1: Point2D, p2: Point2D): number {
   return magnitude2D(subtract2D(p1, p2));
 }
