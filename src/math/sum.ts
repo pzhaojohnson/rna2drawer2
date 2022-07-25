17be34114eae7c9644001a0da810b90890463468
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:53:18 -0400

    Edit comment

--- src/math/sum.ts
+++ src/math/sum.ts
@@ -1,5 +1,8 @@
 /**
  * Calculates the sum of the numbers.
+ *
+ * It is not firmly defined what is returned
+ * for an empty array of numbers.
  */
 export function sum(nums: number[]): number {
   let s = 0;
