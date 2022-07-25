af1b3becb25c93147730c7b23b99dddc4dc9d159
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 09:45:45 -0400

    Refactor it function calls to test function calls and reword test names

--- src/math/angles/normalizeAngle.test.js
+++ src/math/angles/normalizeAngle.test.js
@@ -1,23 +1,23 @@
 import { normalizeAngle } from './normalizeAngle';
 
 describe('normalizeAngle function', () => {
-  it('below minimum angle', () => {
+  test('an angle below the minimum angle', () => {
     expect(normalizeAngle(-Math.PI, 0)).toBeCloseTo(Math.PI, 6);
   });
 
-  it('equals minimum angle', () => {
+  test('an angle equal to the minimum angle', () => {
     expect(normalizeAngle(0, 0)).toBeCloseTo(0, 6);
   });
 
-  it('above minimum angle', () => {
+  test('an angle above the minimum angle', () => {
     expect(normalizeAngle(4 * Math.PI, 0)).toBeCloseTo(0, 6);
   });
 
-  it('already normalized', () => {
+  test('an already normalized angle', () => {
     expect(normalizeAngle(Math.PI, 0)).toBeCloseTo(Math.PI, 6);
   });
 
-  it('2 * Math.PI above minimum angle', () => {
+  test('an angle 2 * Math.PI above the minimum angle', () => {
     expect(normalizeAngle(2 * Math.PI, 0)).toBeCloseTo(0, 6);
   });
 });
