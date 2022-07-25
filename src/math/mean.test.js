ce49dfd94d52d59eefdd184d3f071642c4062586
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:44:43 -0400

    Split unit test into two unit tests

--- src/math/mean.test.js
+++ src/math/mean.test.js
@@ -5,8 +5,11 @@ describe('mean function', () => {
     expect(mean([])).toBe(NaN);
   });
 
-  test('nonempty arrays of numbers', () => {
+  test('an array containing a single number', () => {
     expect(mean([5])).toBeCloseTo(5);
+  });
+
+  test('arrays containing multiple numbers', () => {
     expect(mean([-1, 0.5, 11.2, 23])).toBeCloseTo(33.7 / 4);
     expect(mean([2, 2, -1, -1, -1, 5, 5.6])).toBeCloseTo(11.6 / 7);
   });
