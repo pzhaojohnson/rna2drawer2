81dd2ed4a8ec39e12ce9893d39019632b8a8b8e1
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-24 21:11:13 -0400

    Refactor areClose function to take an options object

--- src/math/areClose.ts
+++ src/math/areClose.ts
@@ -1,4 +1,15 @@
-export function areClose(n1: number, n2: number, places=3): boolean {
+export type Options = { places: number };
+
+/**
+ * Returns true if the two numbers are the same rounding to
+ * the specified number of decimal places and false otherwise.
+ *
+ * Rounds to three decimal places if the number of decimal places
+ * is not specified.
+ */
+export function areClose(n1: number, n2: number, options?: Options): boolean {
+  let places = options?.places ?? 3;
+
   n1 = Number.parseFloat(n1.toFixed(places));
   n2 = Number.parseFloat(n2.toFixed(places));
   return n1 == n2;
