98d586cdf0d90713279449d10edc346ec749ce3c
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:42:43 -0400

    Add describe block

--- src/math/mean.test.js
+++ src/math/mean.test.js
@@ -1,11 +1,13 @@
 import { mean } from './mean';
 
-it('empty list', () => {
-  expect(mean([])).toBe(NaN);
-});
+describe('mean function', () => {
+  it('empty list', () => {
+    expect(mean([])).toBe(NaN);
+  });
 
-it('nonempty lists', () => {
-  expect(mean([5])).toBeCloseTo(5);
-  expect(mean([-1, 0.5, 11.2, 23])).toBeCloseTo(33.7 / 4);
-  expect(mean([2, 2, -1, -1, -1, 5, 5.6])).toBeCloseTo(11.6 / 7);
+  it('nonempty lists', () => {
+    expect(mean([5])).toBeCloseTo(5);
+    expect(mean([-1, 0.5, 11.2, 23])).toBeCloseTo(33.7 / 4);
+    expect(mean([2, 2, -1, -1, -1, 5, 5.6])).toBeCloseTo(11.6 / 7);
+  });
 });
