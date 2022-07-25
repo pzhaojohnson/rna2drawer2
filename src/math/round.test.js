9f0473eddf9d87ea98fdc1c68a8328baa3848c04
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:57:26 -0400

    Add describe block

--- src/math/round.test.js
+++ src/math/round.test.js
@@ -1,40 +1,42 @@
 import { round } from './round';
 
-it('can round up', () => {
-  expect(round(5.627, 2)).toBe(5.63);
-  expect(round(8.88263919, 3)).toBe(8.883);
-  expect(round(0.11669823, 5)).toBe(0.1167);
-});
+describe('round function', () => {
+  it('can round up', () => {
+    expect(round(5.627, 2)).toBe(5.63);
+    expect(round(8.88263919, 3)).toBe(8.883);
+    expect(round(0.11669823, 5)).toBe(0.1167);
+  });
 
-it('can round down', () => {
-  expect(round(10.6218, 1)).toBe(10.6);
-  expect(round(21.00782340792, 6)).toBe(21.007823);
-  expect(round(8.0901026, 3)).toBe(8.09);
-});
+  it('can round down', () => {
+    expect(round(10.6218, 1)).toBe(10.6);
+    expect(round(21.00782340792, 6)).toBe(21.007823);
+    expect(round(8.0901026, 3)).toBe(8.09);
+  });
 
-it('can round to integer', () => {
-  expect(round(3.8)).toBe(4);
-  expect(round(11.0923)).toBe(11);
-});
+  it('can round to integer', () => {
+    expect(round(3.8)).toBe(4);
+    expect(round(11.0923)).toBe(11);
+  });
 
-it('rounds to integer by default', () => {
-  expect(round(1.2553)).toBe(1);
-  expect(round(5)).toBe(5);
-  expect(round(2.9)).toBe(3);
-});
+  it('rounds to integer by default', () => {
+    expect(round(1.2553)).toBe(1);
+    expect(round(5)).toBe(5);
+    expect(round(2.9)).toBe(3);
+  });
 
-it('can round to more places than number has', () => {
-  // 1.528 has fewer than 8 decimal places
-  expect(round(1.526, 8)).toBe(1.526);
-});
+  it('can round to more places than number has', () => {
+    // 1.528 has fewer than 8 decimal places
+    expect(round(1.526, 8)).toBe(1.526);
+  });
 
-it('handles negative numbers', () => {
-  expect(round(-12.03)).toBe(-12);
-  expect(round(-8.239, 2)).toBe(-8.24);
-});
+  it('handles negative numbers', () => {
+    expect(round(-12.03)).toBe(-12);
+    expect(round(-8.239, 2)).toBe(-8.24);
+  });
 
-it('handles nonfinite values', () => {
-  expect(round(NaN)).toBe(NaN);
-  expect(round(Infinity)).toBe(Infinity);
-  expect(round(-Infinity)).toBe(-Infinity);
+  it('handles nonfinite values', () => {
+    expect(round(NaN)).toBe(NaN);
+    expect(round(Infinity)).toBe(Infinity);
+    expect(round(-Infinity)).toBe(-Infinity);
+  });
 });
