57c8fa0c75b5c08a018b81a22c875551a884234e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 08:09:24 -0400

    Add comment

--- src/math/points/midpoint.ts
+++ src/math/points/midpoint.ts
@@ -1,5 +1,8 @@
 import { Point2D } from 'Math/points/Point';
 
+/**
+ * Calculates the point halfway between the two points.
+ */
 export function midpoint2D(p1: Point2D, p2: Point2D): Point2D {
   return {
     x: (p1.x + p2.x) / 2,
