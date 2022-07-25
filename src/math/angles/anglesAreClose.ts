9baf4e1bd8d839d45b2b7e33518d1d840ad0f4b6
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 11:19:54 -0400

    Remove unnecessary rounding of a1 and a2 variables

--- src/math/angles/anglesAreClose.ts
+++ src/math/angles/anglesAreClose.ts
@@ -12,8 +12,6 @@ export function anglesAreClose(a1: number, a2: number, options?: Options): boole
   a1 = normalizeAngle(a1);
   a2 = normalizeAngle(a2);
   let diff = Math.abs(a1 - a2);
-  a1 = Number.parseFloat(a1.toFixed(places));
-  a2 = Number.parseFloat(a2.toFixed(places));
   diff = Number.parseFloat(diff.toFixed(places));
   let pi2 = Number.parseFloat((2 * Math.PI).toFixed(places));
   return diff == 0 || diff == pi2;
