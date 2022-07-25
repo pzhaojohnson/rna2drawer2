83cfa0086caa1be0d8a2bb06137a196311490672
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-24 21:16:25 -0400

    Add comment

--- src/math/distance.ts
+++ src/math/distance.ts
@@ -1,3 +1,6 @@
+/**
+ * Calculates the distance between the two 2D points.
+ */
 export function distance2D(x1: number, y1: number, x2: number, y2: number): number {
   return ((x1 - x2)**2 + (y1 - y2)**2)**0.5;
 }
