d40abf383260a32863acd7126f74ad6c0e7a8a4d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 08:16:24 -0400

    Convert single line comments to documentation comments and edit comments

--- src/math/points/displacement.ts
+++ src/math/points/displacement.ts
@@ -1,11 +1,15 @@
 import { Point2D, Point2D as Vector2D } from 'Math/points/Point';
 
-// subtract p2 from p1
+/**
+ * Subtracts point 2 from point 1.
+ */
 export function subtract2D(p1: Point2D, p2: Point2D): Vector2D {
   return { x: p1.x - p2.x, y: p1.y - p2.y };
 }
 
-// returns the displacement from p1 to p2
+/**
+ * Returns the displacement from point 1 to point 2.
+ */
 export function displacement2D(p1: Point2D, p2: Point2D): Vector2D {
   return subtract2D(p2, p1);
 }
