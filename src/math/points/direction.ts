d0eea4b77b526904bed7b5a6eeb5dc911f2eca1e
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 08:20:35 -0400

    Convert single line comments to documentation comment

--- src/math/points/direction.ts
+++ src/math/points/direction.ts
@@ -1,8 +1,10 @@
 import { Point2D as Vector2D } from 'Math/points/Point';
 
-// returns the angle of the given vector relative to having started from
-// the horizontal vector on the x-axis pointing in the positive direction
-// (due east) and rotating counterclockwise
+/**
+ * Returns the angle of the given vector relative to having started from
+ * the horizontal vector on the x-axis pointing in the positive direction
+ * (due east) and rotating counterclockwise.
+ */
 export function direction2D(v: Vector2D): number {
   return Math.atan2(v.y, v.x);
 }
