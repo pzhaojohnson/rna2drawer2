a7b5470d6c969249ba77acc6cac160d76ccd997b
Philip Johnson <p.zhao.johnson@gmail.com>
2022-07-25 07:43:44 -0400

    Refactor it function calls to test function calls and reword test names

--- src/math/mean.test.js
+++ src/math/mean.test.js
@@ -1,11 +1,11 @@
 import { mean } from './mean';
 
 describe('mean function', () => {
-  it('empty list', () => {
+  test('an empty array of numbers`', () => {
     expect(mean([])).toBe(NaN);
   });
 
-  it('nonempty lists', () => {
+  test('nonempty arrays of numbers', () => {
     expect(mean([5])).toBeCloseTo(5);
     expect(mean([-1, 0.5, 11.2, 23])).toBeCloseTo(33.7 / 4);
     expect(mean([2, 2, -1, -1, -1, 5, 5.6])).toBeCloseTo(11.6 / 7);
