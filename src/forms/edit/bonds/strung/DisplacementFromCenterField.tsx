9a53824151179bf80d38ad30bf5a8a9542d47464
Philip Johnson <p.zhao.johnson@gmail.com>
2022-08-10 09:30:31 -0400

    Add DisplacementFromCenterField component

---
+++ src/forms/edit/bonds/strung/DisplacementFromCenterField.tsx
@@ -0,0 +1,72 @@
+import type { App } from 'App';
+
+import type { Bond } from 'Forms/edit/bonds/strung/Bond';
+import type { StrungElement } from 'Draw/bonds/strung/StrungElement';
+
+import { repositionStrungElementAtIndex } from 'Forms/edit/bonds/strung/repositionStrungElementAtIndex';
+
+import * as React from 'react';
+
+import { NumberPropertyInput } from 'Forms/edit/objects/NumberPropertyInput';
+import { EditEvent } from 'Forms/edit/objects/NumberPropertyInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+export type Props = {
+  /**
+   * A reference to the whole app.
+   */
+  app: App;
+
+  /**
+   * The strung elements to edit.
+   */
+  strungElements: StrungElement[];
+
+  /**
+   * The bonds possessing the strung elements.
+   */
+  bonds: Bond[];
+
+  /**
+   * The index that the strung elements are at in the strung elements
+   * arrays of the bonds possessing the strung elements.
+   */
+  strungElementsIndex: number;
+
+  style?: React.CSSProperties;
+};
+
+export class DisplacementFromCenterField extends React.Component<Props> {
+  onBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
+
+  onEdit(event: EditEvent) {
+    this.props.bonds.forEach(bond => {
+      repositionStrungElementAtIndex({
+        bond,
+        index: this.props.strungElementsIndex,
+      });
+    });
+
+    this.props.app.refresh();
+  }
+
+  render() {
+    return (
+      <FieldLabel style={this.props.style} >
+        <NumberPropertyInput
+          objects={this.props.strungElements}
+          propertyName='displacementFromCenter'
+          places={2}
+          onBeforeEdit={this.onBeforeEdit}
+          onEdit={this.onEdit}
+        />
+        <span style={{ marginLeft: '8px' }} >
+          Displacement From Center
+        </span>
+      </FieldLabel>
+    );
+  }
+}
