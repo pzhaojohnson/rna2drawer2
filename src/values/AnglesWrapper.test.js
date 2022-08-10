3cab730b5c8b7f7c433f1dd1b7d9c59685187aba
Philip Johnson <p.zhao.johnson@gmail.com>
2022-08-09 22:18:42 -0400

    Add normalize method to AnglesWrapper class

--- src/values/AnglesWrapper.test.js
+++ src/values/AnglesWrapper.test.js
@@ -6,4 +6,48 @@ describe('AnglesWrapper class', () => {
     let angles = new AnglesWrapper(anglesArray);
     expect(angles.angles).toBe(anglesArray);
   });
+
+  describe('normalize method', () => {
+    test('when the angle floor is specified', () => {
+      let angles = new AnglesWrapper(
+        [-Math.PI / 3, 26 * Math.PI / 3, 15 * Math.PI / 4]
+      );
+      angles = angles.normalize({ angleFloor: 3 * Math.PI });
+      expect(angles.angles.length).toBe(3);
+      // increased
+      expect(angles.angles[0]).toBeCloseTo(11 * Math.PI / 3);
+      // decreased
+      expect(angles.angles[1]).toBeCloseTo(14 * Math.PI / 3);
+      // stayed the same
+      expect(angles.angles[2]).toBeCloseTo(15 * Math.PI / 4);
+    });
+
+    test('when the angle floor is left unspecified', () => {
+      let angles = new AnglesWrapper(
+        [Math.PI / 4, -11 * Math.PI / 2, 19 * Math.PI / 3]
+      );
+      // should use an angle floor of zero
+      angles = angles.normalize();
+      expect(angles.angles.length).toBe(3);
+      // stayed the same
+      expect(angles.angles[0]).toBeCloseTo(Math.PI / 4);
+      // increased
+      expect(angles.angles[1]).toBeCloseTo(Math.PI / 2);
+      // decreased
+      expect(angles.angles[2]).toBeCloseTo(Math.PI / 3);
+    });
+
+    test('nullish values in angles array', () => {
+      let angles = new AnglesWrapper(
+        [15 * Math.PI / 2, null, undefined]
+      );
+      angles = angles.normalize();
+      expect(angles.angles.length).toBe(3);
+      // normalized non-nullish value
+      expect(angles.angles[0]).toBeCloseTo(3 * Math.PI / 2);
+      // maintained nullish values
+      expect(angles.angles[1]).toBe(null);
+      expect(angles.angles[2]).toBeUndefined();
+    });
+  });
 });
--- src/values/AnglesWrapper.ts
+++ src/values/AnglesWrapper.ts
@@ -1,4 +1,21 @@
+import { normalizeAngle } from 'Math/angles/normalizeAngle';
+
+import { isNullish } from 'Utilities/isNullish';
+
 export type Nullish = null | undefined;
+
+export type NormalizeOptions = {
+  /**
+   * The minimum angle to normalize angles above.
+   *
+   * (Angles will be normalized such that they are greater than or equal
+   * to the angle floor and less than 2 * Math.PI above the angle
+   * floor.)
+   *
+   * Defaults to zero if left unspecified.
+   */
+  angleFloor?: number;
+};
 
 /**
  * Works with angles in radians.
@@ -11,5 +28,22 @@ export class AnglesWrapper {
 
   constructor(angles: (number | Nullish)[]) {
     this.angles = angles;
+  }
+
+  /**
+   * Returns a new angles wrapper of the normalized angles.
+   */
+  normalize(options?: NormalizeOptions): AnglesWrapper {
+    let angleFloor = options?.angleFloor ?? 0;
+
+    return new AnglesWrapper(
+      this.angles.map(angle => {
+        if (isNullish(angle)) {
+          return angle;
+        } else {
+          return normalizeAngle(angle, angleFloor);
+        }
+      })
+    );
   }
 }
