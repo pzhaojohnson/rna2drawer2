efe1b1ae2a34292af6a21efd1c2e860cb15a0c69
Philip Johnson <p.zhao.johnson@gmail.com>
2022-08-10 20:09:17 -0400

    Add StrokeWidthField component

---
+++ src/forms/edit/bonds/strung/StrokeWidthField.tsx
@@ -0,0 +1,82 @@
+import type { App } from 'App';
+
+import type { StrungCircle } from 'Draw/bonds/strung/StrungElement';
+import type { StrungTriangle } from 'Draw/bonds/strung/StrungElement';
+import type { StrungRectangle } from 'Draw/bonds/strung/StrungElement';
+
+import { defaultStrungCircleValues } from 'Draw/bonds/strung/defaults';
+import { defaultStrungTriangleValues } from 'Draw/bonds/strung/defaults';
+import { defaultStrungRectangleValues } from 'Draw/bonds/strung/defaults';
+
+import { svgElementOfStrungElement } from 'Forms/edit/bonds/strung/svgElementOfStrungElement';
+
+import * as React from 'react';
+
+import { NumericAttributeInput } from 'Forms/edit/svg/NumericAttributeInput';
+import { EditEvent } from 'Forms/edit/svg/NumericAttributeInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+const defaultSVGElementAttributes = {
+  'StrungCircle': defaultStrungCircleValues.circle,
+  'StrungTriangle': defaultStrungTriangleValues.path,
+  'StrungRectangle': defaultStrungRectangleValues.path,
+};
+
+export type Props = {
+  /**
+   * A reference to the whole app.
+   */
+  app: App;
+
+  /**
+   * The strung elements to edit.
+   */
+  strungElements: (
+    StrungCircle
+    | StrungTriangle
+    | StrungRectangle
+  )[];
+};
+
+/**
+ * Allows editing of the "stroke-width" attribute of the SVG element of
+ * strung elements.
+ */
+export class StrokeWidthField extends React.Component<Props> {
+  onBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
+
+  onEdit(event: EditEvent) {
+    let newValue = event.newValue;
+    let strungElements = this.props.strungElements;
+    let types = new Set(strungElements.map(ele => ele.type));
+    types.forEach(t => {
+      defaultSVGElementAttributes[t]['stroke-width'] = newValue;
+    });
+
+    this.props.app.pushUndo();
+  }
+
+  render() {
+    let strungElements = this.props.strungElements;
+    let svgElements = strungElements.map(svgElementOfStrungElement);
+
+    return (
+      <FieldLabel>
+        <NumericAttributeInput
+          elements={svgElements}
+          attributeName='stroke-width'
+          minValue={0}
+          places={2}
+          onBeforeEdit={this.onBeforeEdit}
+          onEdit={this.onEdit}
+        />
+        <span style={{ marginLeft: '8px' }} >
+          Line Width
+        </span>
+      </FieldLabel>
+    );
+  }
+}
