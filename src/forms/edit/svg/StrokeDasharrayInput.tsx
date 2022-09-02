f2d103d00d31915d46461e14688588abe2e63899
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-01 21:36:48 -0400

    Add StrokeDasharrayInput component

---
+++ src/forms/edit/svg/StrokeDasharrayInput.tsx
@@ -0,0 +1,173 @@
+import * as SVG from '@svgdotjs/svg.js';
+
+import { ValuesWrapper } from 'Values/ValuesWrapper';
+
+import { isNullish } from 'Values/isNullish';
+import { isBlank } from 'Parse/isBlank';
+import { strokeDasharrayValueEqualsNone as equalsNone } from 'Values/svg/strokeDasharrayValueEqualsNone';
+
+import * as React from 'react';
+
+import { TextInput } from 'Forms/inputs/text/TextInput';
+
+function isNotBlank(s: string): boolean {
+  return !isBlank(s);
+}
+
+function isPositive(n: number): boolean {
+  return n > 0;
+}
+
+/**
+ * A value that the stroke-dasharray attribute of an SVG element can
+ * have.
+ */
+export type StrokeDasharrayValue = unknown;
+
+export type EditEvent = {
+  /**
+   * The new value that was or will be assigned to the stroke-dasharray
+   * attributes of the SVG elements.
+   */
+  newValue: StrokeDasharrayValue;
+
+  /**
+   * The previous value that the stroke-dasharray attributes of the SVG
+   * elements shared.
+   *
+   * Is undefined if not all SVG elements had the same stroke-dasharray
+   * value.
+   */
+  oldValue?: StrokeDasharrayValue;
+};
+
+export type Props = {
+  /**
+   * The SVG elements to edit.
+   */
+  elements?: SVG.Element[];
+
+  /**
+   * Called immediately after changing the stroke-dasharray attributes
+   * of the SVG elements.
+   */
+  onEdit?: (event: EditEvent) => void;
+
+  /**
+   * Called immediately before changing the stroke-dasharray attributes
+   * of the SVG elements.
+   */
+  onBeforeEdit?: (event: EditEvent) => void;
+
+  style?: React.CSSProperties;
+};
+
+/**
+ * Allows editing of the stroke-dasharray attributes of the provided SVG
+ * elements.
+ */
+export class StrokeDasharrayInput extends React.Component<Props> {
+  state: {
+    value: string;
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      value: this.initialValue,
+    };
+  }
+
+  /**
+   * The value that the stroke-dasharray attributes of the SVG elements
+   * currently share.
+   *
+   * Is undefined if not all SVG elements have the same value for their
+   * stroke-dasharray attribute.
+   */
+  get oldValue(): StrokeDasharrayValue | undefined {
+    if (!this.props.elements || this.props.elements.length == 0) {
+      return undefined;
+    }
+
+    let values = new ValuesWrapper<unknown>(
+      this.props.elements.map(ele => ele.attr('stroke-dasharray'))
+    );
+
+    let commonValue = values.commonValue;
+    return !isNullish(commonValue) ? commonValue : undefined;
+  }
+
+  /**
+   * The value that the underlying text input component is initially
+   * rendered with.
+   */
+  get initialValue(): string {
+    let oldValue = this.oldValue;
+    return !isNullish(oldValue) ? String(oldValue) : '';
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
+  }
+
+  /**
+   * The value that the stroke-dasharray attributes of the SVG elements
+   * are to be set to based on the current value of the underlying text
+   * input component.
+   */
+  get newValue(): string {
+    // split by whitespace and commas
+    let dashStrings = this.state.value.split(/[\s|,]/);
+    dashStrings = dashStrings.filter(isNotBlank);
+
+    let dashes = dashStrings.map(Number.parseFloat);
+    dashes = dashes.filter(Number.isFinite);
+    dashes = dashes.filter(isPositive);
+
+    return dashes.join(' ');
+  }
+
+  submit() {
+    if (!this.props.elements || this.props.elements.length == 0) {
+      return;
+    }
+
+    let newValue = this.newValue;
+    let oldValue = this.oldValue;
+
+    if (newValue == oldValue) {
+      return;
+    } else if (equalsNone(newValue)) {
+      return;
+    }
+
+    let editEvent = { newValue, oldValue };
+    if (this.props.onBeforeEdit) {
+      this.props.onBeforeEdit(editEvent);
+    }
+    this.props.elements.forEach(ele => {
+      ele.attr('stroke-dasharray', newValue);
+    });
+    if (this.props.onEdit) {
+      this.props.onEdit(editEvent);
+    }
+  }
+
+  render() {
+    return (
+      <TextInput
+        value={this.state.value}
+        onChange={event => this.handleChange(event)}
+        onBlur={() => this.submit()}
+        onKeyUp={event => {
+          if (event.key.toLowerCase() == 'enter') {
+            this.submit();
+          }
+        }}
+        style={this.props.style}
+      />
+    );
+  }
+}
