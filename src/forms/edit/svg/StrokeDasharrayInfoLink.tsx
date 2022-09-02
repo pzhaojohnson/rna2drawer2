424446cc710983328cbdce6389414acab5b9ba49
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-02 09:39:30 -0400

    Add StrokeDasharrayInfoLink component

---
+++ src/forms/edit/svg/StrokeDasharrayInfoLink.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { InfoLink } from 'Forms/info/InfoLink';
+
+export function StrokeDasharrayInfoLink() {
+  return (
+    <InfoLink
+      href='https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dasharray'
+      title='Learn more about dash arrays.'
+    />
+  );
+}
