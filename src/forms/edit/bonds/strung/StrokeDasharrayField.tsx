4ebe55f7e625277666b68ae8cbf7358533e24acf
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-06 11:35:21 -0400

    Update the default dashed value when editing the stroke-dasharray values for strung elements

--- src/forms/edit/bonds/strung/StrokeDasharrayField.tsx
+++ src/forms/edit/bonds/strung/StrokeDasharrayField.tsx
@@ -1,5 +1,9 @@
 import type { App } from 'App';
 
+import * as SVG from '@svgdotjs/svg.js';
+
+import { strokeDasharrayValueEqualsNone as equalsNone } from 'Values/svg/strokeDasharrayValueEqualsNone';
+
 import type { StrungCircle } from 'Draw/bonds/strung/StrungElement';
 import type { StrungTriangle } from 'Draw/bonds/strung/StrungElement';
 import type { StrungRectangle } from 'Draw/bonds/strung/StrungElement';
@@ -10,6 +14,50 @@ import * as React from 'react';
 
 // the underlying component
 import { StrokeDasharrayField as _StrokeDasharrayField } from 'Forms/edit/svg/StrokeDasharrayField';
+
+import { isString } from 'Values/isString';
+
+/**
+ * A value that the stroke-dasharray attribute of an SVG element can
+ * have.
+ */
+type StrokeDasharrayValue = unknown;
+
+/**
+ * A value that the stroke-dasharray attribute of an SVG element can
+ * have that is a string.
+ */
+type StringStrokeDasharrayValue = string;
+
+/**
+ * Returns the values that the stroke-dasharray attributes of the SVG
+ * elements have.
+ */
+function strokeDasharrayValuesOf
+(
+  svgElements: SVG.Element[],
+): StrokeDasharrayValue[]
+{
+  return svgElements.map(ele => ele.attr('stroke-dasharray'));
+}
+
+/**
+ * Returns the values that the stroke-dasharray attributes of the SVG
+ * elements have that are strings.
+ */
+function stringStrokeDasharrayValuesOf
+(
+  svgElements: SVG.Element[],
+): StringStrokeDasharrayValue[]
+{
+  return strokeDasharrayValuesOf(svgElements).filter(isString);
+}
+
+/**
+ * Should be updated on edit and persist between mountings and
+ * unmountings.
+ */
+let defaultDashedValue = '3 1';
 
 export type Props = {
   /**
@@ -28,11 +76,31 @@ export type Props = {
 };
 
 export class StrokeDasharrayField extends React.Component<Props> {
+  /**
+   * Values cached in response to a before edit event.
+   */
+  _oldValues?: Set<StrokeDasharrayValue>;
+
   handleBeforeEdit() {
+    let svgElements = this.props.strungElements.map(svgElementOfStrungElement);
+    this._oldValues = new Set(strokeDasharrayValuesOf(svgElements));
+
     this.props.app.pushUndo();
   }
 
   handleEdit() {
+    let svgElements = this.props.strungElements.map(svgElementOfStrungElement);
+    // ignore nullish values
+    let newValues = stringStrokeDasharrayValuesOf(svgElements);
+    // remove old values
+    newValues = newValues.filter(v => !this._oldValues?.has(v));
+
+    if (newValues.length > 0 && !equalsNone(newValues[0])) {
+      defaultDashedValue = newValues[0]; // update
+    }
+
+    this._oldValues = undefined; // reset
+
     this.props.app.refresh();
   }
 
@@ -43,7 +111,7 @@ export class StrokeDasharrayField extends React.Component<Props> {
       <_StrokeDasharrayField
         label='Dashed Line'
         elements={svgElements}
-        defaultDashedValue='3 1'
+        defaultDashedValue={defaultDashedValue}
         onBeforeEdit={() => this.handleBeforeEdit()}
         onEdit={() => this.handleEdit()}
         style={{
