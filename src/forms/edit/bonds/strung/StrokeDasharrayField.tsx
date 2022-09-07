8efc8e33726acfb3e1a4414fd8219e5202ba9fec
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-07 09:49:05 -0400

    Make use of edit event object in StrokeDasharrayField component for strung elements

--- src/forms/edit/bonds/strung/StrokeDasharrayField.tsx
+++ src/forms/edit/bonds/strung/StrokeDasharrayField.tsx
@@ -1,7 +1,5 @@
 import type { App } from 'App';
 
-import * as SVG from '@svgdotjs/svg.js';
-
 import { strokeDasharrayValueEqualsNone as equalsNone } from 'Values/svg/strokeDasharrayValueEqualsNone';
 
 import type { StrungCircle } from 'Draw/bonds/strung/StrungElement';
@@ -14,44 +12,7 @@ import * as React from 'react';
 
 // the underlying component
 import { StrokeDasharrayField as _StrokeDasharrayField } from 'Forms/edit/svg/StrokeDasharrayField';
-
-import { isString } from 'Values/isString';
-
-/**
- * A value that the stroke-dasharray attribute of an SVG element can
- * have.
- */
-type StrokeDasharrayValue = unknown;
-
-/**
- * A value that the stroke-dasharray attribute of an SVG element can
- * have that is a string.
- */
-type StringStrokeDasharrayValue = string;
-
-/**
- * Returns the values that the stroke-dasharray attributes of the SVG
- * elements have.
- */
-function strokeDasharrayValuesOf
-(
-  svgElements: SVG.Element[],
-): StrokeDasharrayValue[]
-{
-  return svgElements.map(ele => ele.attr('stroke-dasharray'));
-}
-
-/**
- * Returns the values that the stroke-dasharray attributes of the SVG
- * elements have that are strings.
- */
-function stringStrokeDasharrayValuesOf
-(
-  svgElements: SVG.Element[],
-): StringStrokeDasharrayValue[]
-{
-  return strokeDasharrayValuesOf(svgElements).filter(isString);
-}
+import { EditEvent } from 'Forms/edit/svg/StrokeDasharrayField';
 
 /**
  * Should be updated on edit and persist between mountings and
@@ -76,30 +37,14 @@ export type Props = {
 };
 
 export class StrokeDasharrayField extends React.Component<Props> {
-  /**
-   * Values cached in response to a before edit event.
-   */
-  _oldValues?: Set<StrokeDasharrayValue>;
-
   handleBeforeEdit() {
-    let svgElements = this.props.strungElements.map(svgElementOfStrungElement);
-    this._oldValues = new Set(strokeDasharrayValuesOf(svgElements));
-
     this.props.app.pushUndo();
   }
 
-  handleEdit() {
-    let svgElements = this.props.strungElements.map(svgElementOfStrungElement);
-    // ignore nullish values
-    let newValues = stringStrokeDasharrayValuesOf(svgElements);
-    // remove old values
-    newValues = newValues.filter(v => !this._oldValues?.has(v));
-
-    if (newValues.length > 0 && !equalsNone(newValues[0])) {
-      defaultDashedValue = newValues[0]; // update
+  handleEdit(event: EditEvent) {
+    if (!equalsNone(event.newValue)) {
+      defaultDashedValue = event.newValue;
     }
-
-    this._oldValues = undefined; // reset
 
     this.props.app.refresh();
   }
@@ -113,7 +58,7 @@ export class StrokeDasharrayField extends React.Component<Props> {
         elements={svgElements}
         defaultDashedValue={defaultDashedValue}
         onBeforeEdit={() => this.handleBeforeEdit()}
-        onEdit={() => this.handleEdit()}
+        onEdit={event => this.handleEdit(event)}
         style={{
           marginTop: '8px',
           minHeight: '22px',
