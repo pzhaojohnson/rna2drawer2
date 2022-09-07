fbd0a5456787aaa49edf58df662e11e01bad0bd1
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-07 09:42:20 -0400

    Make use of edit event object in StrokeDasharrayField component for base outlines

--- src/forms/edit/bases/outlines/StrokeDasharrayField.tsx
+++ src/forms/edit/bases/outlines/StrokeDasharrayField.tsx
@@ -8,40 +8,7 @@ import * as React from 'react';
 
 // the underlying component
 import { StrokeDasharrayField as _StrokeDasharrayField } from 'Forms/edit/svg/StrokeDasharrayField';
-
-import { isString } from 'Values/isString';
-
-/**
- * A value that the stroke-dasharray attribute of an SVG element can
- * have.
- */
-type StrokeDasharrayValue = unknown;
-
-/**
- * A value that the stroke-dasharray attribute of an SVG element can
- * have that is a string.
- */
-type StringStrokeDasharrayValue = string;
-
-/**
- * Returns the stroke-dasharray values of the base outline circle
- * elements.
- */
-function valuesOf(outlines: BaseOutline[]): StrokeDasharrayValue[] {
-  return outlines.map(o => o.circle.attr('stroke-dasharray'));
-}
-
-/**
- * Returns the stroke-dasharray values of the base outline circle
- * elements that are strings.
- */
-function stringValuesOf
-(
-  outlines: BaseOutline[],
-): StringStrokeDasharrayValue[]
-{
-  return valuesOf(outlines).filter(isString);
-}
+import { EditEvent } from 'Forms/edit/svg/StrokeDasharrayField';
 
 // should persist between mountings and unmountings
 let defaultDashedValue = '3 1';
@@ -59,25 +26,13 @@ export type Props = {
 };
 
 export class StrokeDasharrayField extends React.Component<Props> {
-  /**
-   * Values cached in response to a before edit event.
-   */
-  _oldValues?: Set<StrokeDasharrayValue>;
-
   handleBeforeEdit() {
-    this._oldValues = new Set(valuesOf(this.props.outlines));
-
     this.props.app.pushUndo();
   }
 
-  handleEdit() {
-    // ignore nullish values
-    let newValues = stringValuesOf(this.props.outlines);
-    // remove old values
-    newValues = newValues.filter(v => !this._oldValues?.has(v));
-
-    if (newValues.length > 0 && !equalsNone(newValues[0])) {
-      defaultDashedValue = newValues[0]; // cache
+  handleEdit(event: EditEvent) {
+    if (!equalsNone(event.newValue)) {
+      defaultDashedValue = event.newValue;
     }
 
     this.props.app.refresh(); // refresh after updating all values
@@ -90,7 +45,7 @@ export class StrokeDasharrayField extends React.Component<Props> {
         elements={this.props.outlines.map(o => o.circle)}
         defaultDashedValue={defaultDashedValue}
         onBeforeEdit={() => this.handleBeforeEdit()}
-        onEdit={() => this.handleEdit()}
+        onEdit={event => this.handleEdit(event)}
         style={{
           marginTop: '10px',
           minHeight: '22px',
