923e32e7444044f8e4fcb7cf5f4a3083e4697dde
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-14 09:36:11 -0400

    Remove unused EnterDotBracketForm component

--- src/forms/new/dot-bracket/EnterDotBracketForm.test.js
+++
@@ -1,9 +0,0 @@
-import { App } from 'App';
-import { NodeSVG } from 'Draw/svg/NodeSVG';
-import * as React from 'react';
-import { EnterDotBracketForm } from './EnterDotBracketForm';
-
-it('renders', () => {
-  let app = new App({ SVG: { SVG: NodeSVG } });
-  app.formContainer.renderForm(close => <EnterDotBracketForm app={app} close={close} />);
-});
