5816f0a1fa6f3c90bec2328424dde296d6aaeb89
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-21 10:12:19 -0400

    Use OpacityAttributeInput component in StrokeOpacityInput component for secondary bonds

--- src/forms/edit/bonds/secondary/StrokeOpacityInput.tsx
+++ src/forms/edit/bonds/secondary/StrokeOpacityInput.tsx
@@ -1,83 +1,60 @@
 import type { App } from 'App';
+
 import { SecondaryBond } from 'Draw/bonds/straight/SecondaryBond';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { strokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { setStrokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { displayablePercentageString } from 'Forms/inputs/numbers/displayablePercentageString';
-import { isBlank } from 'Parse/isBlank';
-import { parsePercentageString } from 'Forms/inputs/numbers/parsePercentageString';
-
 import * as React from 'react';
-import { TextInput } from 'Forms/inputs//text/TextInput';
 
-// returns the line elements of the secondary bonds
-function lines(secondaryBonds: SecondaryBond[]): SVG.Line[] {
-  return secondaryBonds.map(bond => bond.line);
-}
+import { OpacityAttributeInput } from 'Forms/edit/svg/OpacityAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/OpacityAttributeInput';
+
+import { generateHTMLSafeUUID } from 'Utilities/generateHTMLSafeUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing on refresh)
+const id = generateHTMLSafeUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the secondary bonds to edit
+  /**
+   * The secondary bonds to edit.
+   */
   secondaryBonds: SecondaryBond[];
 }
 
 export class StrokeOpacityInput extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
+    let types = new Set(this.props.secondaryBonds.map(sb => sb.type));
 
-    this.state = {
-      value: displayablePercentageString(strokeOpacity(lines(props.secondaryBonds)), { places: 0 }),
-    };
+    // don't make secondary bonds invisible by default
+    if (newValue > 0) {
+      types.forEach(t => {
+        SecondaryBond.recommendedDefaults[t].line['stroke-opacity'] = newValue;
+      });
+    }
+
+    this.props.app.refresh();
   }
 
   render() {
     return (
-      <TextInput
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        style={{ width: '32px', textAlign: 'end' }}
+      <OpacityAttributeInput
+        id={id}
+        elements={this.props.secondaryBonds.map(sb => sb.line)}
+        attributeName='stroke-opacity'
+        places={2}
+        onBeforeEdit={event => this.handleBeforeEdit(event)}
+        onEdit={event => this.handleEdit(event)}
+        style={{ textAlign: 'end' }}
       />
     );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = parsePercentageString(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == strokeOpacity(lines(this.props.secondaryBonds))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setStrokeOpacity(lines(this.props.secondaryBonds), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = strokeOpacity(lines(this.props.secondaryBonds));
-
-    this.props.secondaryBonds.forEach(bond => {
-      SecondaryBond.recommendedDefaults[bond.type].line['stroke-opacity'] = constrainedValue;
-    });
   }
 }
