f5eb279bac499cb5d4cd4099953d041921a00e81
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-22 10:38:07 -0400

    Use NumericAttributeInput component in primary bonds StrokeWidthField component

--- src/forms/edit/bonds/primary/StrokeWidthField.tsx
+++ src/forms/edit/bonds/primary/StrokeWidthField.tsx
@@ -1,87 +1,71 @@
 import type { App } from 'App';
+
 import { PrimaryBond } from 'Draw/bonds/straight/PrimaryBond';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { strokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { setStrokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { numberToDisplayableString as displayableString } from 'Forms/inputs/numbers/numberToDisplayableString';
-import { isBlank } from 'Parse/isBlank';
-
 import * as React from 'react';
-import { TextInputField } from 'Forms/inputs/text/TextInputField';
 
-// returns the line elements of the primary bonds
-function lines(primaryBonds: PrimaryBond[]): SVG.Line[] {
-  return primaryBonds.map(primaryBond => primaryBond.line);
-}
+import { NumericAttributeInput } from 'Forms/edit/svg/NumericAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/NumericAttributeInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+import { generateHTMLSafeUUID } from 'Utilities/generateHTMLSafeUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLSafeUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the primary bonds to edit
+  /**
+   * The primary bonds to edit.
+   */
   primaryBonds: PrimaryBond[];
 }
 
 export class StrokeWidthField extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: displayableString(strokeWidth(lines(props.primaryBonds)), { places: 2 }),
+    // don't make primary bonds too hard to see by default
+    if (newValue >= 0.5) {
+      PrimaryBond.recommendedDefaults.line['stroke-width'] = newValue;
     }
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
+    let style: React.CSSProperties = {
+      marginTop: '14px',
+      alignSelf: 'start',
+      cursor: 'text',
+    };
+
     return (
-      <TextInputField
-        label='Line Width'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        input={{
-          style: { width: '32px' },
-        }}
-        style={{ marginTop: '14px', alignSelf: 'start' }}
-      />
-    );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = Number.parseFloat(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == strokeWidth(lines(this.props.primaryBonds))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setStrokeWidth(lines(this.props.primaryBonds), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = strokeWidth(lines(this.props.primaryBonds));
-
-    PrimaryBond.recommendedDefaults.line['stroke-width'] = (
-      constrainedValue
-      ?? PrimaryBond.recommendedDefaults.line['stroke-width']
+      <FieldLabel style={style} >
+        <NumericAttributeInput
+          id={inputId}
+          elements={this.props.primaryBonds.map(pb => pb.line)}
+          attributeName='stroke-width'
+          minValue={0}
+          places={2}
+          onBeforeEdit={event => this.handleBeforeEdit(event)}
+          onEdit={event => this.handleEdit(event)}
+          style={{ width: '32px' }}
+        />
+        <span style={{ paddingLeft: '8px' }} >
+          Line Width
+        </span>
+      </FieldLabel>
     );
   }
 }
