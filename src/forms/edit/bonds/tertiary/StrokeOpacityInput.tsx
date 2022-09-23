8654ad038254c934d2f2dccede8bfc05670f9006
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-23 08:11:10 -0400

    Use OpacityAttributeInput component in tertiary bonds StrokeOpacityInput component

--- src/forms/edit/bonds/tertiary/StrokeOpacityInput.tsx
+++ src/forms/edit/bonds/tertiary/StrokeOpacityInput.tsx
@@ -1,84 +1,63 @@
 import type { App } from 'App';
+
 import { TertiaryBond } from 'Draw/bonds/curved/TertiaryBond';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { strokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { setStrokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { displayablePercentageString } from 'Forms/inputs/numbers/displayablePercentageString';
-import { isBlank } from 'Parse/isBlank';
-import { parsePercentageString } from 'Forms/inputs/numbers/parsePercentageString';
-
 import * as React from 'react';
-import { TextInput } from 'Forms/inputs/text/TextInput';
 
-// returns the path elements of the tertiary bonds
-function paths(tertiaryBonds: TertiaryBond[]): SVG.Path[] {
-  return tertiaryBonds.map(bond => bond.path);
-}
+import { OpacityAttributeInput } from 'Forms/edit/svg/OpacityAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/OpacityAttributeInput';
+
+import { generateHTMLSafeUUID } from 'Utilities/generateHTMLSafeUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const id = generateHTMLSafeUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the tertiary bonds to edit
+  /**
+   * The tertiary bonds to edit.
+   */
   tertiaryBonds: TertiaryBond[];
 }
 
 export class StrokeOpacityInput extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: displayablePercentageString(strokeOpacity(paths(props.tertiaryBonds)), { places: 0 }),
-    };
+    // don't make tertiary bonds too hard to see by default
+    if (newValue >= 0.25) {
+      TertiaryBond.recommendedDefaults.path['stroke-opacity'] = newValue;
+    }
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
+    let style: React.CSSProperties = {
+      marginRight: '8px',
+      width: '32px',
+      textAlign: 'end',
+    };
+
     return (
-      <TextInput
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        style={{ marginRight: '8px', width: '32px', textAlign: 'end' }}
+      <OpacityAttributeInput
+        id={id}
+        elements={this.props.tertiaryBonds.map(tb => tb.path)}
+        attributeName='stroke-opacity'
+        places={2}
+        onBeforeEdit={event => this.handleBeforeEdit(event)}
+        onEdit={event => this.handleEdit(event)}
+        style={style}
       />
-    );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = parsePercentageString(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == strokeOpacity(paths(this.props.tertiaryBonds))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setStrokeOpacity(paths(this.props.tertiaryBonds), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = strokeOpacity(paths(this.props.tertiaryBonds));
-
-    TertiaryBond.recommendedDefaults.path['stroke-opacity'] = (
-      constrainedValue
-      ?? TertiaryBond.recommendedDefaults.path['stroke-opacity']
     );
   }
 }
