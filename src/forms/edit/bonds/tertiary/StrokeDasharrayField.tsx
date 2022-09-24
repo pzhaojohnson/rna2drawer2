14dc95bc1cb9deab33251d775946fd9a331f81ae
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-24 10:13:01 -0400

    Specify input component ID in tertiary bonds StrokeDasharrayField component

--- src/forms/edit/bonds/tertiary/StrokeDasharrayField.tsx
+++ src/forms/edit/bonds/tertiary/StrokeDasharrayField.tsx
@@ -10,11 +10,17 @@ import * as React from 'react';
 import { StrokeDasharrayField as _StrokeDasharrayField } from 'Forms/edit/svg/StrokeDasharrayField';
 import { EditEvent } from 'Forms/edit/svg/StrokeDasharrayField';
 
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
+
 /**
  * A value that the stroke-dasharray attribute of an SVG element can
  * have that is a string.
  */
 type StringStrokeDasharrayValue = string;
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLCompatibleUUID();
 
 /**
  * To be remembered between mountings and unmountings.
@@ -69,6 +75,7 @@ export class StrokeDasharrayField extends React.Component<Props> {
         defaultDashedValue={this.defaultDashedValue}
         onBeforeEdit={() => this.handleBeforeEdit()}
         onEdit={event => this.handleEdit(event)}
+        input={{ id: inputId }}
         style={{
           marginTop: '10px',
           minHeight: '22px',
