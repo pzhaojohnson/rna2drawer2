2e33d46689df0f65c773edb6d03c04aa5d0e133c
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-25 13:49:02 -0400

    Use NumericAttributeInput component in base numberings LineWidthField component

--- src/forms/edit/bases/numberings/LineWidthField.tsx
+++ src/forms/edit/bases/numberings/LineWidthField.tsx
@@ -1,87 +1,68 @@
 import type { App } from 'App';
+
 import { BaseNumbering } from 'Draw/bases/numberings/BaseNumbering';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { strokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { setStrokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { numberToDisplayableString as displayableString } from 'Forms/inputs/numbers/numberToDisplayableString';
-import { isBlank } from 'Parse/isBlank';
-
 import * as React from 'react';
-import { TextInputField } from 'Forms/inputs/text/TextInputField';
 
-// returns the line elements of the base numberings
-function lines(baseNumberings: BaseNumbering[]): SVG.Line[] {
-  return baseNumberings.map(bn => bn.line);
-}
+import { NumericAttributeInput } from 'Forms/edit/svg/NumericAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/NumericAttributeInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLCompatibleUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the base numberings to edit
+  /**
+   * The base numberings to edit.
+   */
   baseNumberings: BaseNumbering[];
 }
 
 export class LineWidthField extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: displayableString(strokeWidth(lines(props.baseNumberings)), { places: 2 }),
-    };
+    BaseNumbering.recommendedDefaults.line['stroke-width'] = newValue;
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
+    let style: React.CSSProperties = {
+      marginTop: '16px',
+      alignSelf: 'start',
+      cursor: 'text',
+    };
+
     return (
-      <TextInputField
-        label='Line Width'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        input={{
-          style: { width: '32px' },
-        }}
-        style={{ marginTop: '16px', alignSelf: 'start' }}
-      />
-    );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = Number.parseFloat(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == strokeWidth(lines(this.props.baseNumberings))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setStrokeWidth(lines(this.props.baseNumberings), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = strokeWidth(lines(this.props.baseNumberings));
-
-    BaseNumbering.recommendedDefaults.line['stroke-width'] = (
-      constrainedValue
-      ?? BaseNumbering.recommendedDefaults.line['stroke-width']
+      <FieldLabel style={style} >
+        <NumericAttributeInput
+          id={inputId}
+          elements={this.props.baseNumberings.map(bn => bn.line)}
+          attributeName='stroke-width'
+          minValue={0}
+          places={2}
+          onBeforeEdit={event => this.handleBeforeEdit(event)}
+          onEdit={event => this.handleEdit(event)}
+          style={{ width: '32px' }}
+        />
+        <span style={{ paddingLeft: '8px' }} >
+          Line Width
+        </span>
+      </FieldLabel>
     );
   }
 }
