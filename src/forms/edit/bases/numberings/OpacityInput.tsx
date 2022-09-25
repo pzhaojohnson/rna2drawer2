76fa9db81d3d205f5a395899345076b7f7678b5d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-25 13:09:39 -0400

    Use OpacityAttributeInput component in base numberings OpacityInput component

--- src/forms/edit/bases/numberings/OpacityInput.tsx
+++ src/forms/edit/bases/numberings/OpacityInput.tsx
@@ -1,103 +1,72 @@
 import type { App } from 'App';
+
 import { BaseNumbering } from 'Draw/bases/numberings/BaseNumbering';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { fillOpacity } from 'Forms/inputs/svg/fillOpacity/fillOpacity';
-import { setFillOpacity } from 'Forms/inputs/svg/fillOpacity/fillOpacity';
-import { strokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { setStrokeOpacity } from 'Forms/inputs/svg/strokeOpacity/strokeOpacity';
-import { displayablePercentageString } from 'Forms/inputs/numbers/displayablePercentageString';
-import { isBlank } from 'Parse/isBlank';
-import { parsePercentageString } from 'Forms/inputs/numbers/parsePercentageString';
-
 import * as React from 'react';
-import textFieldStyles from 'Forms/inputs/text/TextField.css';
 
-// returns the text elements of the base numberings
-function texts(baseNumberings: BaseNumbering[]): SVG.Text[] {
-  return baseNumberings.map(bn => bn.text);
-}
+import { OpacityAttributeInput } from 'Forms/edit/svg/OpacityAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/OpacityAttributeInput';
 
-// returns the line elements of the base numberings
-function lines(baseNumberings: BaseNumbering[]): SVG.Line[] {
-  return baseNumberings.map(bn => bn.line);
-}
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const id = generateHTMLCompatibleUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the base numberings to edit
+  /**
+   * The base numberings to edit.
+   */
   baseNumberings: BaseNumbering[];
 }
 
 export class OpacityInput extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      // assumes that texts fill-opacity is the same as lines stroke-opacity
-      value: displayablePercentageString(fillOpacity(texts(props.baseNumberings)), { places: 0 }),
-    };
+    // make text and line elements opacities the same
+    this.props.baseNumberings.forEach(bn => {
+      bn.text.attr('fill-opacity', newValue);
+      bn.line.attr('stroke-opacity', newValue);
+    });
+
+    // don't make base numberings too hard to see by default
+    if (newValue >= 0.25) {
+      BaseNumbering.recommendedDefaults.text['fill-opacity'] = newValue;
+      BaseNumbering.recommendedDefaults.line['stroke-opacity'] = newValue;
+    }
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
+    // line elements stroke opacities should be updated on edit
+    let elements = this.props.baseNumberings.map(bn => bn.text);
+    const attributeName = 'fill-opacity';
+
     return (
-      <input
-        type='text'
-        className={textFieldStyles.input}
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
+      <OpacityAttributeInput
+        id={id}
+        elements={elements}
+        attributeName={attributeName}
+        places={2}
+        onBeforeEdit={event => this.handleBeforeEdit(event)}
+        onEdit={event => this.handleEdit(event)}
+        style={{
+          marginRight: '8px',
+          width: '32px',
+          textAlign: 'end',
         }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        style={{ marginRight: '8px', width: '32px', textAlign: 'end' }}
       />
-    );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = parsePercentageString(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == fillOpacity(texts(this.props.baseNumberings))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setFillOpacity(texts(this.props.baseNumberings), value);
-    setStrokeOpacity(lines(this.props.baseNumberings), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = fillOpacity(texts(this.props.baseNumberings));
-
-    BaseNumbering.recommendedDefaults.text['fill-opacity'] = (
-      constrainedValue
-      ?? BaseNumbering.recommendedDefaults.text['fill-opacity']
-    );
-
-    // may be different from the value that was specified
-    constrainedValue = strokeOpacity(lines(this.props.baseNumberings));
-
-    BaseNumbering.recommendedDefaults.line['stroke-opacity'] = (
-      constrainedValue
-      ?? BaseNumbering.recommendedDefaults.line['stroke-opacity']
     );
   }
 }
