1a75b846c044cdfd81fa53058a5e3c916bef692d
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-25 09:37:42 -0400

    Use NumericAttributeInput component in base outlines RadiusField component

--- src/forms/edit/bases/outlines/RadiusField.tsx
+++ src/forms/edit/bases/outlines/RadiusField.tsx
@@ -1,110 +1,71 @@
-import * as React from 'react';
-import { TextInputField } from 'Forms/inputs/text/TextInputField';
 import type { App } from 'App';
-import { CircleBaseAnnotation } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
-import { interpretNumber } from 'Draw/svg/interpretNumber';
-import { round } from 'Math/round';
+
+import { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
+
+import * as React from 'react';
+
+import { NumericAttributeInput } from 'Forms/edit/svg/NumericAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/NumericAttributeInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLCompatibleUUID();
 
 export type Props = {
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the outlines to edit
-  outlines: CircleBaseAnnotation[];
-}
-
-type Value = string;
-
-type State = {
-  value: Value;
-}
-
-// returns an empty string value for an empty outlines array
-// or if not all outlines have the same radius
-function currRadius(outlines: CircleBaseAnnotation[]): Value {
-  let rs = new Set<Value>();
-  outlines.forEach(o => {
-    let r = o.circle.attr('r');
-    let n = interpretNumber(r);
-    if (n) {
-      let pxs = n.convert('px').valueOf();
-      pxs = round(pxs, 2);
-      rs.add(pxs.toString());
-    }
-  });
-  if (rs.size == 1) {
-    return rs.values().next().value;
-  } else {
-    return '';
-  }
-}
-
-function isBlank(v: Value): boolean {
-  return v.trim().length == 0;
-}
-
-function areEqual(v1: Value, v2: Value): boolean {
-  return Number.parseFloat(v1) == Number.parseFloat(v2);
-}
-
-function constrainRadius(r: number): number {
-  if (!Number.isFinite(r)) {
-    return 8;
-  } else if (r < 0) {
-    return 0;
-  } else {
-    return r;
-  }
+  /**
+   * The base outlines to edit.
+   */
+  outlines: BaseOutline[];
 }
 
 export class RadiusField extends React.Component<Props> {
-  state: State;
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: currRadius(props.outlines),
-    };
+    // don't make base outlines too hard to see by default
+    if (newValue >= 3) {
+      BaseOutline.recommendedDefaults.circle['r'] = newValue;
+    }
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
-    return (
-      <TextInputField
-        label='Radius'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        input={{
-          style: { width: '32px' },
-        }}
-        style={{ marginTop: '10px', alignSelf: 'start' }}
-      />
-    );
-  }
+    let style: React.CSSProperties = {
+      marginTop: '10px',
+      alignSelf: 'start',
+      cursor: 'text',
+    };
 
-  submit() {
-    if (!isBlank(this.state.value)) {
-      let r = Number.parseFloat(this.state.value);
-      if (Number.isFinite(r)) {
-        if (!areEqual(this.state.value, currRadius(this.props.outlines))) {
-          this.props.app.pushUndo();
-          r = constrainRadius(r);
-          this.props.outlines.forEach(o => {
-            o.circle.attr({ 'r': r });
-          });
-          CircleBaseAnnotation.recommendedDefaults.circle['r'] = r;
-          this.props.app.refresh();
-        }
-      }
-    }
+    return (
+      <FieldLabel style={style} >
+        <NumericAttributeInput
+          id={inputId}
+          elements={this.props.outlines.map(o => o.circle)}
+          attributeName='r'
+          minValue={0}
+          places={2}
+          onBeforeEdit={event => this.handleBeforeEdit(event)}
+          onEdit={event => this.handleEdit(event)}
+          style={{ width: '32px' }}
+        />
+        <span style={{ paddingLeft: '8px' }} >
+          Radius
+        </span>
+      </FieldLabel>
+    );
   }
 }
