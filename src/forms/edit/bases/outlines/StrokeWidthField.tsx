75045a7e683e86b32a13f30b0dec3ab37bd587e2
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-25 11:15:47 -0400

    Use NumericAttributeInput component in base outlines StrokeWidthField component

--- src/forms/edit/bases/outlines/StrokeWidthField.tsx
+++ src/forms/edit/bases/outlines/StrokeWidthField.tsx
@@ -1,87 +1,68 @@
 import type { App } from 'App';
-import { CircleBaseAnnotation } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { strokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { setStrokeWidth } from 'Forms/inputs/svg/strokeWidth/strokeWidth';
-import { numberToDisplayableString as displayableString } from 'Forms/inputs/numbers/numberToDisplayableString';
-import { isBlank } from 'Parse/isBlank';
+import { CircleBaseAnnotation as BaseOutline } from 'Draw/bases/annotate/circle/CircleBaseAnnotation';
 
 import * as React from 'react';
-import { TextInputField } from 'Forms/inputs/text/TextInputField';
 
-// returns the circle elements of the outlines
-function circles(outlines: CircleBaseAnnotation[]): SVG.Circle[] {
-  return outlines.map(outline => outline.circle);
-}
+import { NumericAttributeInput } from 'Forms/edit/svg/NumericAttributeInput';
+import type { EditEvent } from 'Forms/edit/svg/NumericAttributeInput';
+
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
+
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
+
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLCompatibleUUID();
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the outlines to edit
-  outlines: CircleBaseAnnotation[];
+  /**
+   * The base outlines to edit.
+   */
+  outlines: BaseOutline[];
 }
 
 export class StrokeWidthField extends React.Component<Props> {
-  state: {
-    value: string;
-  };
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: displayableString(strokeWidth(circles(props.outlines)), { places: 2 }),
-    };
+    BaseOutline.recommendedDefaults.circle['stroke-width'] = newValue;
+
+    this.props.app.refresh(); // refresh after updating all values
   }
 
   render() {
+    let style: React.CSSProperties = {
+      marginTop: '10px',
+      alignSelf: 'start',
+      cursor: 'text',
+    };
+
     return (
-      <TextInputField
-        label='Line Width'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        input={{
-          style: { width: '32px' },
-        }}
-        style={{ marginTop: '10px', alignSelf: 'start' }}
-      />
-    );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let value = Number.parseFloat(this.state.value);
-    if (!Number.isFinite(value)) {
-      return;
-    } else if (value == strokeWidth(circles(this.props.outlines))) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-    setStrokeWidth(circles(this.props.outlines), value);
-
-    // may be different from the value that was specified
-    let constrainedValue = strokeWidth(circles(this.props.outlines));
-
-    CircleBaseAnnotation.recommendedDefaults.circle['stroke-width'] = (
-      constrainedValue
-      ?? CircleBaseAnnotation.recommendedDefaults.circle['stroke-width']
+      <FieldLabel style={style} >
+        <NumericAttributeInput
+          id={inputId}
+          elements={this.props.outlines.map(o => o.circle)}
+          attributeName='stroke-width'
+          minValue={0}
+          places={2}
+          onBeforeEdit={event => this.handleBeforeEdit(event)}
+          onEdit={event => this.handleEdit(event)}
+          style={{ width: '32px' }}
+        />
+        <span style={{ paddingLeft: '8px' }} >
+          Line Width
+        </span>
+      </FieldLabel>
     );
   }
 }
