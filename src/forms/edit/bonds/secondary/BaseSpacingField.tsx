bccaa9b48acca47a4c1abd8baf80970c51320ffc
Philip Johnson <p.zhao.johnson@gmail.com>
2022-09-26 07:58:07 -0400

    Use NumberPropertyInput component in BaseSpacingField component

--- src/forms/edit/bonds/secondary/BaseSpacingField.tsx
+++ src/forms/edit/bonds/secondary/BaseSpacingField.tsx
@@ -1,105 +1,107 @@
-import * as React from 'react';
-import { TextInputField } from 'Forms/inputs/text/TextInputField';
 import type { App } from 'App';
-import { round } from 'Math/round';
+
+import type { StrictDrawing } from 'Draw/strict/StrictDrawing';
 
 import { isDot } from 'Draw/bonds/straight/dotify';
-import { isSquare } from 'Draw/bonds/straight/dotify';
 import { dotify } from 'Draw/bonds/straight/dotify';
+
+import { isSquare } from 'Draw/bonds/straight/dotify';
 import { squarify } from 'Draw/bonds/straight/dotify';
 
-export type Props = {
-  app: App;
-}
+import * as React from 'react';
 
-type Value = string;
+import { NumberPropertyInput } from 'Forms/edit/objects/NumberPropertyInput';
+import type { EditEvent } from 'Forms/edit/objects/NumberPropertyInput';
 
-type State = {
-  value: Value;
-}
+import { FieldLabel } from 'Forms/inputs/labels/FieldLabel';
 
-function isBlank(v: Value): boolean {
-  return v.trim().length == 0;
-}
+import { generateHTMLCompatibleUUID } from 'Utilities/generateHTMLCompatibleUUID';
 
-function constrainBaseSpacing(bs: number): number {
-  if (!Number.isFinite(bs)) {
-    return 0;
-  } else if (bs < 0) {
-    return 0;
-  } else {
-    return bs;
+/**
+ * Meant to help make getting and setting the base-pair bond length of
+ * the drawing easier.
+ */
+class DrawingWrapper {
+  readonly drawing: StrictDrawing;
+
+  constructor(drawing: StrictDrawing) {
+    this.drawing = drawing;
+  }
+
+  get basePairBondLength(): number {
+    return this.drawing.generalLayoutProps.basePairBondLength;
+  }
+
+  set basePairBondLength(basePairBondLength: number) {
+    // remember which secondary bonds were dots and squares
+    let dotSecondaryBonds = this.drawing.secondaryBonds.filter(isDot);
+    let squareSecondaryBonds = this.drawing.secondaryBonds.filter(isSquare);
+
+    // update the base-pair bond length
+    this.drawing.generalLayoutProps.basePairBondLength = basePairBondLength;
+    this.drawing.updateLayout();
+
+    // make sure are still dots and squares
+    dotSecondaryBonds.forEach(dotify);
+    squareSecondaryBonds.forEach(squarify);
   }
 }
 
+// should be stable across mountings and unmountings
+// (to facilitate refocusing when the app is refreshed)
+const inputId = generateHTMLCompatibleUUID();
+
+export type Props = {
+  /**
+   * A reference to the whole app.
+   */
+  app: App;
+};
+
 export class BaseSpacingField extends React.Component<Props> {
-  state: State;
+  get drawing() {
+    return new DrawingWrapper(this.props.app.drawing);
+  }
 
-  constructor(props: Props) {
-    super(props);
+  handleBeforeEdit(event: EditEvent) {
+    this.props.app.pushUndo();
+  }
 
-    let generalLayoutProps = props.app.strictDrawing.generalLayoutProps;
-    let bs = generalLayoutProps.basePairBondLength;
-    bs = round(bs, 2);
+  handleEdit(event: EditEvent) {
+    let newValue = event.newValue;
 
-    this.state = {
-      value: bs.toString(),
-    };
+    // update the drawing
+    this.drawing.basePairBondLength = newValue;
+
+    this.props.app.refresh(); // refresh after updating everything else
   }
 
   render() {
+    // the drawing itself should be updated on edit
+    let objects = [{ basePairBondLength: this.drawing.basePairBondLength }];
+
+    let style: React.CSSProperties = {
+      margin: '10px 0 0 10px',
+      alignSelf: 'start',
+      cursor: 'text',
+    };
+
     return (
-      <TextInputField
-        label='Base Spacing'
-        value={this.state.value}
-        onChange={event => this.setState({ value: event.target.value })}
-        onBlur={() => {
-          this.submit();
-          this.props.app.refresh();
-        }}
-        onKeyUp={event => {
-          if (event.key.toLowerCase() == 'enter') {
-            this.submit();
-            this.props.app.refresh();
-          }
-        }}
-        input={{
-          style: { width: '6ch' },
-        }}
-        style={{ margin: '10px 0 0 10px', alignSelf: 'start' }}
-      />
+      <FieldLabel style={style} >
+        <NumberPropertyInput
+          id={inputId}
+          objects={objects}
+          propertyName='basePairBondLength'
+          minValue={0}
+          places={2}
+          onBeforeEdit={event => this.handleBeforeEdit(event)}
+          onEdit={event => this.handleEdit(event)}
+          style={{ width: '6ch' }}
+        />
+        <span style={{ paddingLeft: '8px' }} >
+          Base Spacing
+        </span>
+      </FieldLabel>
     );
-  }
-
-  submit() {
-    if (isBlank(this.state.value)) {
-      return;
-    }
-
-    let bs = Number.parseFloat(this.state.value);
-    if (!Number.isFinite(bs)) {
-      return;
-    }
-
-    let generalLayoutProps = this.props.app.strictDrawing.generalLayoutProps;
-    if (bs == generalLayoutProps.basePairBondLength) {
-      return;
-    }
-
-    this.props.app.pushUndo();
-
-    let secondaryBonds = this.props.app.strictDrawing.drawing.secondaryBonds;
-    let dotSecondaryBonds = secondaryBonds.filter(bond => isDot(bond));
-    let squareSecondaryBonds = secondaryBonds.filter(bond => isSquare(bond));
-
-    bs = constrainBaseSpacing(bs);
-    bs = round(bs, 2);
-    generalLayoutProps.basePairBondLength = bs;
-    this.props.app.strictDrawing.updateLayout();
-
-    dotSecondaryBonds.forEach(bond => dotify(bond));
-    squareSecondaryBonds.forEach(bond => squarify(bond));
-
-    this.props.app.refresh();
   }
 }
