c95691f33e5a2fd96be2e7fe1aa6b5738c29cceb
Philip Johnson <p.zhao.johnson@gmail.com>
2022-10-01 12:46:27 -0400

    Add Bond type definition

---
+++ src/draw/bonds/Bond.ts
@@ -0,0 +1,4 @@
+import type { StraightBond } from 'Draw/bonds/straight/StraightBond';
+import type { QuadraticBezierBond } from 'Draw/bonds/curved/QuadraticBezierBond';
+
+export type Bond = StraightBond | QuadraticBezierBond;
