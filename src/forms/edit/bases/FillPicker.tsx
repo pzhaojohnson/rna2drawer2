34eb147e1ba72f002d1a9c440d53127b8c7a66fe
Philip Johnson <p.zhao.johnson@gmail.com>
2022-10-06 11:13:16 -0400

    Use ColorAttributePicker component in bases FillPicker component

--- src/forms/edit/bases/FillPicker.tsx
+++ src/forms/edit/bases/FillPicker.tsx
@@ -1,44 +1,34 @@
 import type { App } from 'App';
+
 import type { Base } from 'Draw/bases/Base';
 
-import * as SVG from '@svgdotjs/svg.js';
-import { fill } from 'Forms/inputs/svg/fill/fill';
-import { fillEquals } from 'Forms/inputs/svg/fill/fill';
-import { setFill } from 'Forms/inputs/svg/fill/fill';
-
 import * as React from 'react';
-import { ColorPicker } from 'Forms/inputs/color/ColorPicker';
 
-// returns the text elements of the bases
-function texts(bases: Base[]): SVG.Text[] {
-  return bases.map(base => base.text);
-}
+import { ColorAttributePicker } from 'Forms/edit/svg/ColorAttributePicker';
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the bases to edit
+  /**
+   * The bases to edit.
+   */
   bases: Base[];
 }
 
 export function FillPicker(props: Props) {
   return (
-    <ColorPicker
-      value={fill(texts(props.bases))}
-      onClose={event => {
-        if (!event.target.value) {
-          return;
-        } else if (fillEquals(texts(props.bases), event.target.value.color)) {
-          return;
-        }
-
+    <ColorAttributePicker
+      elements={props.bases.map(b => b.text)}
+      attributeName='fill'
+      onBeforeEdit={() => {
         props.app.pushUndo();
-        setFill(texts(props.bases), event.target.value.color);
+      }}
+      onEdit={() => {
         props.app.refresh();
       }}
-      disableAlpha={true}
     />
   );
 }
