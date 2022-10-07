31b4e299609f5bb707552565e2d2d4e40c6240f0
Philip Johnson <p.zhao.johnson@gmail.com>
2022-10-07 08:01:19 -0400

    Use ColorAttributePicker component in tertiary bonds StrokePicker component

--- src/forms/edit/bonds/tertiary/StrokePicker.tsx
+++ src/forms/edit/bonds/tertiary/StrokePicker.tsx
@@ -1,45 +1,52 @@
 import type { App } from 'App';
+
 import { TertiaryBond } from 'Draw/bonds/curved/TertiaryBond';
 
 import * as SVG from '@svgdotjs/svg.js';
-import { stroke } from 'Forms/inputs/svg/stroke/stroke';
-import { strokeEquals } from 'Forms/inputs/svg/stroke/stroke';
-import { setStroke } from 'Forms/inputs/svg/stroke/stroke';
 
 import * as React from 'react';
-import { ColorPicker } from 'Forms/inputs/color/ColorPicker';
 
-// returns the path elements of the tertiary bonds
-function paths(tertiaryBonds: TertiaryBond[]): SVG.Path[] {
-  return tertiaryBonds.map(bond => bond.path);
+import { ColorAttributePicker } from 'Forms/edit/svg/ColorAttributePicker';
+
+import { colorValuesAreEqual } from 'Draw/svg/colorValuesAreEqual';
+
+/**
+ * Returns true if the color is white.
+ */
+function isWhite(color: SVG.Color): boolean {
+  return colorValuesAreEqual(color.toHex(), '#ffffff');
 }
 
 export type Props = {
-
-  // a reference to the whole app
+  /**
+   * A reference to the whole app.
+   */
   app: App;
 
-  // the tertiary bonds to edit
+  /**
+   * The tertiary bonds to edit.
+   */
   tertiaryBonds: TertiaryBond[];
 }
 
 export function StrokePicker(props: Props) {
   return (
-    <ColorPicker
-      value={stroke(paths(props.tertiaryBonds))}
-      onClose={event => {
-        if (!event.target.value) {
-          return;
-        } else if (strokeEquals(paths(props.tertiaryBonds), event.target.value.color)) {
-          return;
+    <ColorAttributePicker
+      elements={props.tertiaryBonds.map(tb => tb.path)}
+      attributeName='stroke'
+      onBeforeEdit={() => {
+        props.app.pushUndo();
+      }}
+      onEdit={event => {
+        let newValue = event.newValue;
+
+        // don't make the same color as the background by default
+        if (!isWhite(newValue)) {
+          TertiaryBond.recommendedDefaults.path['stroke'] = newValue.toHex();
         }
 
-        props.app.pushUndo();
-        setStroke(paths(props.tertiaryBonds), event.target.value.color);
-        TertiaryBond.recommendedDefaults.path['stroke'] = event.target.value.color.toHex();
-        props.app.refresh();
+        props.app.refresh(); // refresh after updating all values
       }}
-      disableAlpha={true}
     />
   );
 }
