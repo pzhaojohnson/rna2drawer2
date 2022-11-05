1810fc519c50996420fd7dfa517fd98855f2e255
Philip Johnson <p.zhao.johnson@gmail.com>
2022-11-04 23:12:39 -0400

    Reorganize attributes in RightCaret component

--- src/menu/Dropright.tsx
+++ src/menu/Dropright.tsx
@@ -4,13 +4,16 @@ import styles from './Dropright.css';
 function RightCaret() {
   return (
     <svg
-      viewBox="0 0 3.89 6.72" xmlns="http://www.w3.org/2000/svg" overflow="hidden"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 3.89 6.72"
+      overflow="hidden"
       style={{ height: '12px' }}
     >
       <path
-        d="M 0.53 0.53 L 3.36 3.36 L 0.53 6.19" strokeWidth="0.75"
-        stroke="#38383c" fillOpacity="0"
+        d="M 0.53 0.53 L 3.36 3.36 L 0.53 6.19"
+        stroke="#38383c" strokeWidth="0.75"
         strokeLinecap="round" strokeLinejoin="round"
+        fillOpacity="0"
       />
     </svg>
   );
