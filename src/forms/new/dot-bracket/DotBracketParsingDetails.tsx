0f13b6eb69b74da2d4d6520a5fe5515d079171c0
Philip Johnson <p.zhao.johnson@gmail.com>
2022-11-17 12:37:12 -0500

    Reword specified by to indicated by

--- src/forms/new/dot-bracket/DotBracketParsingDetails.tsx
+++ src/forms/new/dot-bracket/DotBracketParsingDetails.tsx
@@ -15,7 +15,7 @@ export function DotBracketParsingDetails(): React.ReactElement {
           Matching parentheses "( )" indicate base-pairs in the secondary structure.
         </p>
         <p style={{ marginTop: '8px' }} >
-          {'Pseudoknot base-pairs are specified by "[ ]", "{ }" and "< >".'}
+          {'Pseudoknot base-pairs are indicated by "[ ]", "{ }" and "< >".'}
         </p>
         <p style={{ marginTop: '8px' }} >
           All other characters and whitespace are ignored.
