ca5da40fcbd7550c443f9fd540e6f8f5bfc86d22
Philip Johnson <p.zhao.johnson@gmail.com>
2022-12-09 12:03:48 -0500

    Darken ExampleSelect examples label text

--- src/forms/new/dot-bracket/ExampleSelect.tsx
+++ src/forms/new/dot-bracket/ExampleSelect.tsx
@@ -23,7 +23,7 @@ export function ExampleSelect(props: Props): React.ReactElement {
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }} >
       <p
         className='unselectable'
-        style={{ fontSize: '14px', fontWeight: 500, fontStyle: 'italic', color: 'rgba(68, 68, 75, 1)' }}
+        style={{ fontSize: '14px', fontWeight: 500, fontStyle: 'italic', color: 'hsl(240, 5%, 25%)' }}
       >
         Examples...
       </p>
